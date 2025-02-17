728299634125d3c56aed083d910aab3da7052f04
Semyon1104 <sema_titov_04@mail.ru>
2025-02-17 22:18:27 +0300

    fix clang

--- tasks/seq/titov_s_ImageFilter_HorizGaussian3x3/func_tests/main.cpp
+++ tasks/seq/titov_s_ImageFilter_HorizGaussian3x3/func_tests/main.cpp
@@ -8,54 +8,6 @@
 #include "core/task/include/task.hpp"
 #include "seq/titov_s_ImageFilter_HorizGaussian3x3/include/ops_seq.hpp"
 
-TEST(titov_s_image_filter_horiz_gaussian3x3_seq, test_10_diag1) {
-  constexpr size_t kWidth = 10;
-  constexpr size_t kHeight = 10;
-  std::vector<double> input_image(kWidth * kHeight, 0.0);
-  std::vector<double> output_image(kWidth * kHeight, 0.0);
-  std::vector<double> expected_output(kWidth * kHeight, 0.0);
-  std::vector<int> kernel = {1, 2, 1};
-
-  for (size_t i = 0; i < kHeight; ++i) {
-    input_image[(i * kWidth) + i] = 1;
-  }
-
-  for (size_t i = 0; i < kHeight; ++i) {
-    for (size_t j = 0; j < kWidth; ++j) {
-      bool isMainDiag = (i == j);
-      bool isCorner = (j == 0 && i == 0) || (j == kWidth - 1 && i == kHeight - 1);
-      bool isAdjacent = (j == i + 1) || (j == i - 1);
-
-      if (isMainDiag || isCorner) {
-        expected_output[i * kWidth + j] = 0.5;
-      } else if (isAdjacent) {
-        expected_output[i * kWidth + j] = 0.25;
-      }
-    }
-  }
-
-  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_image.data()));
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(kernel.data()));
-  task_data_seq->inputs_count.emplace_back(input_image.size());
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(output_image.data()));
-  task_data_seq->outputs_count.emplace_back(output_image.size());
-
-  titov_s_image_filter_horiz_gaussian3x3_seq::ImageFilterSequential image_filter_sequential(task_data_seq);
-
-  ASSERT_EQ(image_filter_sequential.Validation(), true);
-
-  image_filter_sequential.PreProcessing();
-  image_filter_sequential.Run();
-  image_filter_sequential.PostProcessing();
-
-  for (size_t i = 0; i < kHeight; ++i) {
-    for (size_t j = 0; j < kWidth; ++j) {
-      ASSERT_NEAR(output_image[((i * kWidth)) + j], expected_output[((i * kWidth)) + j], 1e-5);
-    }
-  }
-}
-
 TEST(titov_s_image_filter_horiz_gaussian3x3_seq, test_10_1) {
   constexpr size_t kWidth = 10;
   constexpr size_t kHeight = 10;
