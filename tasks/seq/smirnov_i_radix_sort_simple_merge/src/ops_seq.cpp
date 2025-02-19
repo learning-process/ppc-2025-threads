2bf14f901c80844219402815437b33bf04a09902
IljaSmirnov <ilsmirnov693n@gmail.com>
2025-02-19 11:56:42 +0300

    add lab

---
+++ tasks/seq/smirnov_i_radix_sort_simple_merge/src/ops_seq.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <numeric>
+#include <vector>
+
+#include "seq/smirnov_i_radix_sort_simple_merge/include/ops_seq.hpp"
+
+bool smirnov_i_radix_sort_simple_merge_seq::TestTaskSequential::PreProcessingImpl() {
+  unsigned int input_size = task_data->inputs_count[0];
+  auto* in_ptr = reinterpret_cast<int*>(task_data->inputs[0]);
+  mas = std::vector<int>(in_ptr, in_ptr + input_size);
+
+  unsigned int output_size = task_data->outputs_count[0];
+  output_ = std::vector<int>(output_size, 0);
+
+  return true;
+}
+
+bool smirnov_i_radix_sort_simple_merge_seq::TestTaskSequential::ValidationImpl() {
+  return task_data->inputs_count[0] == task_data->outputs_count[0];
+}
+
+bool smirnov_i_radix_sort_simple_merge_seq::TestTaskSequential::RunImpl() {
+  int longest = *std::max_element(mas.begin(), mas.end());
+  int len = std::ceil(std::log10(longest + 1));
+  std::vector<int> sorting(mas.size());
+
+  for (int j = 0; j < len; j++) {
+    std::vector<int> counting(10, 0);
+    for (int i = 0; i < mas.size(); i++) {
+      counting[mas[i] / int(pow(10, j)) % 10]++;
+    }
+    std::partial_sum(counting.begin(), counting.end(), counting.begin());
+    for (int i = mas.size() - 1; i >= 0; i--) {
+      int pos = counting[mas[i] / int(pow(10, j)) % 10] - 1;
+      sorting[pos] = mas[i];
+      counting[mas[i] / int(pow(10, j)) % 10]--;
+    }
+    std::swap(mas, sorting);
+  }
+  output_ = mas;
+  return true;
+}
+
+bool smirnov_i_radix_sort_simple_merge_seq::TestTaskSequential::PostProcessingImpl() {
+  for (size_t i = 0; i < output_.size(); i++) {
+    reinterpret_cast<int*>(task_data->outputs[0])[i] = output_[i];
+  }
+  return true;
+}
