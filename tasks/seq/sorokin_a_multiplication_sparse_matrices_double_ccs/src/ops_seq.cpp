ee219bc8e315a793e867e87321f8b4653210e205
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2025-02-19 12:45:18 +0300

    fix

--- tasks/seq/sorokin_a_multiplication_sparse_matrices_double_ccs/src/ops_seq.cpp
+++ tasks/seq/sorokin_a_multiplication_sparse_matrices_double_ccs/src/ops_seq.cpp
@@ -11,8 +11,8 @@ void MultiplyCCS(const std::vector<double> &a_values, const std::vector<int> &a_
                  const std::vector<int> &a_col_ptr, const std::vector<double> &b_values,
                  const std::vector<int> &b_row_indices, int k, const std::vector<int> &b_col_ptr,
                  std::vector<double> &c_values, std::vector<int> &c_row_indices, int n, std::vector<int> &c_col_ptr) {
-  if (static_cast<int>(a_col_ptr.size()) < m + 1 || static_cast<int>(b_col_ptr.size()) < k + 1) {
-    throw std::invalid_argument("Invalid column pointer size");
+  if (static_cast<int>(a_values.size()) > m * k || static_cast<int>(b_values.size()) > k * n) {
+    throw std::invalid_argument("Invalid val pointer size");
   }
   c_values.clear();
   c_row_indices.clear();
