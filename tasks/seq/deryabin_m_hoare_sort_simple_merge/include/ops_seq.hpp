72c7fff80f49c680185bf06573c5f903a39d2fe8
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-20 00:59:58 +0300

    Update ops_seq.hpp

--- tasks/seq/deryabin_m_hoare_sort_simple_merge/include/ops_seq.hpp
+++ tasks/seq/deryabin_m_hoare_sort_simple_merge/include/ops_seq.hpp
@@ -1,1 +1,26 @@
+bool deryabin_m_hoare_sort_simple_merge_seq::HoareSortTaskSequential
 
+#pragma once
+
+#include <utility>
+
+#include "core/task/include/task.hpp"
+
+namespace deryabin_m_hoare_sort_simple_merge_seq {
+
+class HoareSortTaskSequential : public ppc::core::Task {
+ public:
+  explicit HoareSortTaskSequential(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
+  bool PreProcessingImpl() override;
+  bool ValidationImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+ private:
+  std::vector<double> input_matrix_A_; // входной массив
+  size_type dimension_; // его размер
+  size_type X_; // позиция опорного элемента
+  size_type min_chunk_size_; // размер частей на которые будет разбиваться исходный массив
+  size_type chunk_count_; // число таких частей
+};
+}  // namespace deryabin_m_hoare_sort_simple_merge_seq
