d6a431feb19886974960de7d33f8e3f5a8fb91ad
Anton <132440944+lolipolovkovith@users.noreply.github.com>
2025-02-21 21:19:07 +0300

    fix review

--- tasks/seq/malyshev_a_increase_contrast_by_histogram/src/ops_seq.cpp
+++ tasks/seq/malyshev_a_increase_contrast_by_histogram/src/ops_seq.cpp
@@ -2,7 +2,6 @@
 
 #include <algorithm>
 #include <cmath>
-#include <cstddef>
 #include <cstdint>
 #include <vector>
 
@@ -18,28 +17,20 @@ bool malyshev_a_increase_contrast_by_histogram_seq::TestTaskSequential::Validati
 }
 
 bool malyshev_a_increase_contrast_by_histogram_seq::TestTaskSequential::RunImpl() {
-  uint8_t min = 255;
-  uint8_t max = 0;
-
-  for (size_t i = 0; i < data_.size(); i++) {
-    min = std::min(min, data_[i]);
-    max = std::max(max, data_[i]);
-  }
+  auto [temp_min, temp_max] = std::ranges::minmax_element(data_);
+  uint8_t min = *temp_min;
+  uint8_t max = *temp_max;
 
   if (min == max) {
     return true;
   }
 
-  for (size_t i = 0; i < data_.size(); i++) {
-    data_[i] = (data_[i] - min) * 255 / (max - min);
-  }
+  std::ranges::for_each(data_, [&min, &max](uint8_t& pixel) { pixel = (pixel - min) * 255 / (max - min); });
   return true;
 }
 
 bool malyshev_a_increase_contrast_by_histogram_seq::TestTaskSequential::PostProcessingImpl() {
-  for (size_t i = 0; i < data_.size(); ++i) {
-    task_data->outputs[0][i] = data_[i];
-  }
+  std::ranges::copy(data_, task_data->outputs[0]);
 
   return true;
 }
