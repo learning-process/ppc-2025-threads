3729ec4fb64336ac94e11d96f9251b908b6709cc
Polina Sidorina <polishas206@gmail.com>
2025-02-21 22:14:28 +0300

    1.2

--- tasks/seq/sidorina_p_gradient_method/src/ops_seq.cpp
+++ tasks/seq/sidorina_p_gradient_method/src/ops_seq.cpp
@@ -21,7 +21,7 @@ bool sidorina_p_gradient_method_seq::GradientMethod::PreProcessingImpl() {
 }
 
 bool sidorina_p_gradient_method_seq::GradientMethod::ValidationImpl() {
-  if (*reinterpret_cast<int*>(task_data->inputs[0]) <= 0 || *reinterpret_cast<int*>(task_data->inputs[1]) < 0 ||
+  if (*reinterpret_cast<int*>(task_data->inputs[0]) <= 0 ||
       static_cast<int>(task_data->inputs_count[2]) <= 0 || static_cast<int>(task_data->inputs_count[3]) <= 0 ||
       static_cast<int>(task_data->inputs_count[4]) <= 0)
     return false;
