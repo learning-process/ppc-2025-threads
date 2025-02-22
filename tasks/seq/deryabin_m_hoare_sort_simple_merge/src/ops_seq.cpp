54b49395fb308388676e2a774fc2ad319081b8e4
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-22 11:51:00 +0300

    Update ops_seq.cpp

--- tasks/seq/deryabin_m_hoare_sort_simple_merge/src/ops_seq.cpp
+++ tasks/seq/deryabin_m_hoare_sort_simple_merge/src/ops_seq.cpp
@@ -7,7 +7,7 @@
 #include <vector>
 
 bool deryabin_m_hoare_sort_simple_merge_seq::HoareSortTaskSequential::PreProcessingImpl() {
-  input_array_A_ = reinterpret_cast<double**>(task_data->outputs[0])[0];
+  input_array_A_ = reinterpret_cast<std::vector<double> *>(task_data->inputs[0])[0];
   dimension_ = task_data->inputs_count[0];
   chunk_count_ = task_data->inputs_count[1];
   min_chunk_size_ = dimension_ / chunk_count_;
@@ -118,6 +118,6 @@ bool deryabin_m_hoare_sort_simple_merge_seq::HoareSortTaskSequential::RunImpl() 
 }
 
 bool deryabin_m_hoare_sort_simple_merge_seq::HoareSortTaskSequential::PostProcessingImpl() {
-  reinterpret_cast<double**>(task_data->outputs[0])[0] = input_array_A_;
+  reinterpret_cast<std::vector<double> *>(task_data->outputs[0])[0]; = input_array_A_;
   return true;
 }
