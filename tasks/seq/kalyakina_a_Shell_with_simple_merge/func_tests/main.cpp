ddcb8c7edebd18ef4da1b00ae52d99cefbfe655f
kalyakinaa <nastyakalyakinann@gmail.com>
2025-02-22 17:24:30 +0300

    Shell sort with simple merge

---
+++ tasks/seq/kalyakina_a_Shell_with_simple_merge/func_tests/main.cpp
@@ -0,0 +1,136 @@
+#include <gtest/gtest.h>
+
+#include <cstddef>
+#include <cstdint>
+#include <fstream>
+#include <memory>
+#include <random>
+#include <string>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+#include "core/util/include/util.hpp"
+#include "seq/kalyakina_a_Shell_with_simple_merge/include/ops_seq.hpp"
+
+std::vector<int> CreateReverseSortedVector(unsigned int size, const int left) {
+  std::vector<int> result;
+  while (size--) {
+    result.push_back(left + size);
+  }
+  return result;
+}
+
+bool IsSorted(const std::vector<int> vec) {
+  if (vec.size() < 2) {
+    return true;
+  }
+  for (unsigned int i = 1; i < vec.size(); i++) {
+    if (vec[i - 1] > vec[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void TestOfFunction(std::vector<int>& in) {
+  std::vector<int> out(in.size());
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSequential = std::make_shared<ppc::core::TaskData>();
+
+  taskDataSequential->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
+  taskDataSequential->inputs_count.emplace_back(in.size());
+  taskDataSequential->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+  taskDataSequential->outputs_count.emplace_back(out.size());
+
+  kalyakina_a_Shell_with_simple_merge_seq::ShellSortSequential TaskSequential(taskDataSequential);
+
+  ASSERT_EQ(TaskSequential.Validation(), true);
+  TaskSequential.PreProcessing();
+  TaskSequential.Run();
+  TaskSequential.PostProcessing();
+
+  ASSERT_TRUE(std::is_sorted(out.begin(), out.end()));
+}
+
+TEST(kalyakina_a_Shell_with_simple_merge_seq, test_of_Validation1) {
+  std::vector<int> in = {2, 9, 5, 1, 4, 8, 3};
+  std::vector<int> out(in.size());
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSequential = std::make_shared<ppc::core::TaskData>();
+
+  taskDataSequential->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
+  taskDataSequential->inputs_count.emplace_back(0);
+  taskDataSequential->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+  taskDataSequential->outputs_count.emplace_back(out.size());
+
+  kalyakina_a_Shell_with_simple_merge_seq::ShellSortSequential TaskSequential(taskDataSequential);
+
+  ASSERT_EQ(TaskSequential.Validation(), false);
+}
+
+TEST(kalyakina_a_Shell_with_simple_merge_seq, test_of_Validation2) {
+  std::vector<int> in = {2, 9, 5, 1, 4, 8, 3};
+  std::vector<int> out(in.size());
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSequential = std::make_shared<ppc::core::TaskData>();
+
+  taskDataSequential->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
+  taskDataSequential->inputs_count.emplace_back(in.size());
+  taskDataSequential->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+  taskDataSequential->outputs_count.emplace_back(0);
+
+  kalyakina_a_Shell_with_simple_merge_seq::ShellSortSequential TaskSequential(taskDataSequential);
+
+  ASSERT_EQ(TaskSequential.Validation(), false);
+}
+
+TEST(kalyakina_a_Shell_with_simple_merge_seq, test_of_Validation3) {
+  std::vector<int> in = {2, 9, 5, 1, 4, 8, 3};
+  std::vector<int> out(in.size());
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSequential = std::make_shared<ppc::core::TaskData>();
+
+  taskDataSequential->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
+  taskDataSequential->inputs_count.emplace_back(in.size());
+  taskDataSequential->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+  taskDataSequential->outputs_count.emplace_back(out.size() + 1);
+
+  kalyakina_a_Shell_with_simple_merge_seq::ShellSortSequential TaskSequential(taskDataSequential);
+
+  ASSERT_EQ(TaskSequential.Validation(), false);
+}
+
+TEST(kalyakina_a_Shell_with_simple_merge_seq, small_fixed_vector) {
+  std::vector<int> in = {2, 9, 5, 1, 4, 8, 3, 2};
+  TestOfFunction(in);
+}
+
+TEST(kalyakina_a_Shell_with_simple_merge_seq, medium_fixed_vector) {
+  std::vector<int> in = {2,  9,  5,  1,  4,  8,  3,  11, 34, 12, 6,  29,  13,   7,     56,     32,      88,
+                         90, 94, 78, 54, 47, 37, 77, 22, 44, 55, 66, 123, 1234, 12345, 123456, 1234567, 0};
+  TestOfFunction(in);
+}
+
+TEST(kalyakina_a_Shell_with_simple_merge_seq, reverse_sorted_vector_50) {
+  std::vector<int> in = CreateReverseSortedVector(50, -10);
+  TestOfFunction(in);
+}
+
+TEST(kalyakina_a_Shell_with_simple_merge_seq, reverse_sorted_vector_100) {
+  std::vector<int> in = CreateReverseSortedVector(100, -10);
+  TestOfFunction(in);
+}
+
+TEST(kalyakina_a_Shell_with_simple_merge_seq, reverse_sorted_vector_1000) {
+  std::vector<int> in = CreateReverseSortedVector(1000, -10);
+  TestOfFunction(in);
+}
+
+TEST(kalyakina_a_Shell_with_simple_merge_seq, reverse_sorted_vector_10000) {
+  std::vector<int> in = CreateReverseSortedVector(10000, -10);
+  TestOfFunction(in);
+}
---
+++ tasks/seq/kalyakina_a_Shell_with_simple_merge/src/ops_seq.cpp
@@ -0,0 +1,64 @@
+#include "seq/kalyakina_a_Shell_with_simple_merge/include/ops_seq.hpp"
+
+#include <cmath>
+#include <cstddef>
+#include <vector>
+
+std::vector<unsigned int> kalyakina_a_Shell_with_simple_merge_seq::ShellSortSequential::CalculationOfGapLengths(
+    const unsigned int &size) {
+  std::vector<unsigned int> result;
+  unsigned int local_res = 1;
+  for (unsigned int i = 1; (local_res * 3 <= size) || (local_res == 1); i++) {
+    result.push_back(local_res);
+    if (i % 2) {
+      local_res = 8 * pow(2, i) - 6 * pow(2, (i + 1) / 2) + 1;
+    } else {
+      local_res = 9 * pow(2, i) - 9 * pow(2, i / 2) + 1;
+    }
+  }
+  return result;
+}
+
+void kalyakina_a_Shell_with_simple_merge_seq::ShellSortSequential::ShellSort(std::vector<int> &vec) {
+  for (int k = Sedgwick_sequence.size() - 1; k >= 0; k--) {
+    unsigned int gap = Sedgwick_sequence[k];
+    for (unsigned int i = 0; i < gap; i++) {
+      for (unsigned int j = 0; gap * j + i < vec.size(); j++) {
+        unsigned int index = j;
+        int tmp = vec[index];
+        while ((index >= gap) && (tmp < vec[index - gap])) {
+          vec[index] = vec[index - gap];
+          index -= gap;
+        }
+        vec[index] = tmp;
+      }
+    }
+  }
+}
+
+bool kalyakina_a_Shell_with_simple_merge_seq::ShellSortSequential::PreProcessingImpl() {
+  input_ = std::vector<int>(task_data->inputs_count[0]);
+  auto *in_ptr = reinterpret_cast<int *>(task_data->inputs[0]);
+  std::copy(in_ptr, in_ptr + task_data->inputs_count[0], input_.begin());
+
+  Sedgwick_sequence = CalculationOfGapLengths(input_.size());
+
+  return true;
+}
+
+bool kalyakina_a_Shell_with_simple_merge_seq::ShellSortSequential::ValidationImpl() {
+  return (task_data->inputs_count[0] > 0) && (task_data->outputs_count[0] > 0) &&
+         (task_data->inputs_count[0] == task_data->outputs_count[0]);
+}
+
+bool kalyakina_a_Shell_with_simple_merge_seq::ShellSortSequential::RunImpl() {
+  ShellSort(input_);
+  return true;
+}
+
+bool kalyakina_a_Shell_with_simple_merge_seq::ShellSortSequential::PostProcessingImpl() {
+  for (size_t i = 0; i < input_.size(); i++) {
+    reinterpret_cast<int *>(task_data->outputs[0])[i] = input_[i];
+  }
+  return true;
+}
