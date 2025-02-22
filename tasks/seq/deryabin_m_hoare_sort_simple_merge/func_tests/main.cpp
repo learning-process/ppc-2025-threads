582fcb5c5fc0b088c0afef656ee820ff543df16c
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-22 16:31:24 +0300

    Update main.cpp

--- tasks/seq/deryabin_m_hoare_sort_simple_merge/func_tests/main.cpp
+++ tasks/seq/deryabin_m_hoare_sort_simple_merge/func_tests/main.cpp
@@ -44,7 +44,7 @@ TEST(deryabin_m_hoare_sort_simple_merge_seq, test_random_array) {
   std::uniform_real_distribution<> distribution(-100, 100);
   std::vector<double> input_array(100, distribution(gen));
   std::vector<std::vector<double>> in_array(1, input_array);
-  size_t chunk_count = 10;
+  size_t chunk_count = 20;
   std::vector<double> output_array(100, 0);
   std::vector<std::vector<double>> out_array(1, output_array);
   std::vector<double> true_solution(input_array);
@@ -75,7 +75,7 @@ TEST(deryabin_m_hoare_sort_simple_merge_seq, test_shuffle_array) {
   std::ranges::generate(input_array.begin(), input_array.end(), [value = 0]() mutable { return value++; });
   std::shuffle(input_array.begin(), input_array.end(), gen);
   std::vector<std::vector<double>> in_array(1, input_array);
-  size_t chunk_count = 10;
+  size_t chunk_count = 20;
   std::vector<double> output_array(100, 0);
   std::vector<std::vector<double>> out_array(1, output_array);
   std::vector<double> true_solution(input_array);
@@ -167,7 +167,7 @@ TEST(deryabin_m_hoare_sort_simple_merge_seq, test_partially_sorted_array) {
   const auto half = input_array.size() / 2U;
   std::shuffle(input_array.begin() + (long)half, input_array.end(), gen);
   std::vector<std::vector<double>> in_array(1, input_array);
-  size_t chunk_count = 10;
+  size_t chunk_count = 20;
   std::vector<double> output_array(100, 0);
   std::vector<std::vector<double>> out_array(1, output_array);
   std::vector<double> true_solution(input_array);
