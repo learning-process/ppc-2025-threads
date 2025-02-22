9d54dbde4c05bb3ded0f415da3c95d52303e2d8e
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2025-02-22 13:50:15 +0300

    Update main.cpp

--- tasks/seq/vavilov_v_cannon/perf_tests/main.cpp
+++ tasks/seq/vavilov_v_cannon/perf_tests/main.cpp
@@ -11,73 +11,73 @@
 #include "seq/vavilov_v_cannon/include/ops_seq.hpp"
 
 TEST(vavilov_v_cannon_seq, test_pipeline_run) {
-	constexpr int N = 500;
-	std::vector<double> A(N * N, 1.0);
-	std::vector<double> B(N * N, 1.0);
-	std::vector<double> C(N * N, 0.0);
-	std::vector<double> expected_output(N * N, N);
+  constexpr int N = 500;
+  std::vector<double> A(N * N, 1.0);
+  std::vector<double> B(N * N, 1.0);
+  std::vector<double> C(N * N, 0.0);
+  std::vector<double> expected_output(N * N, N);
 
-	auto task_data_seq = std::make_shared<ppc::core::TaskData>();
-	task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
-	task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(B.data()));
-	task_data_seq->inputs_count.emplace_back(A.size());
-	task_data_seq->inputs_count.emplace_back(B.size());
-	task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(C.data()));
-	task_data_seq->outputs_count.emplace_back(C.size());
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(B.data()));
+  task_data_seq->inputs_count.emplace_back(A.size());
+  task_data_seq->inputs_count.emplace_back(B.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(C.data()));
+  task_data_seq->outputs_count.emplace_back(C.size());
 
-	auto task_seq = std::make_shared<vavilov_v_cannon_seq::CannonSequential>(task_data_seq);
+  auto task_seq = std::make_shared<vavilov_v_cannon_seq::CannonSequential>(task_data_seq);
 
-	auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-	perf_attr->num_running = 10;
-	const auto t0 = std::chrono::high_resolution_clock::now();
-	perf_attr->current_timer = [&] {
-		auto current_time_point = std::chrono::high_resolution_clock::now();
-		auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
-		return static_cast<double>(duration) * 1e-9;
-	};
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perf_attr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
 
-	auto perf_results = std::make_shared<ppc::core::PerfResults>();
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
-	auto perf_analyzer = std::make_shared<ppc::core::Perf>(task_seq);
-	perf_analyzer->PipelineRun(perf_attr, perf_results);
-	ppc::core::Perf::PrintPerfStatistic(perf_results);
-	for (size_t i = 0; i < N * N; i++) {
-		ASSERT_EQ(expected_output[i], C[i]);
-	}
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task_seq);
+  perf_analyzer->PipelineRun(perf_attr, perf_results);
+  ppc::core::Perf::PrintPerfStatistic(perf_results);
+  for (size_t i = 0; i < N * N; i++) {
+    ASSERT_EQ(expected_output[i], C[i]);
+  }
 }
 
 TEST(vavilov_v_cannon_seq, test_task_run) {
-	constexpr int N = 500;
-	std::vector<double> A(N * N, 1.0);
-	std::vector<double> B(N * N, 1.0);
-	std::vector<double> C(N * N, 0.0);
-	std::vector<double> expected_output(N * N, N);
+  constexpr int N = 500;
+  std::vector<double> A(N * N, 1.0);
+  std::vector<double> B(N * N, 1.0);
+  std::vector<double> C(N * N, 0.0);
+  std::vector<double> expected_output(N * N, N);
 
-	auto task_data_seq = std::make_shared<ppc::core::TaskData>();
-	task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
-	task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(B.data()));
-	task_data_seq->inputs_count.emplace_back(A.size());
-	task_data_seq->inputs_count.emplace_back(B.size());
-	task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(C.data()));
-	task_data_seq->outputs_count.emplace_back(C.size());
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(B.data()));
+  task_data_seq->inputs_count.emplace_back(A.size());
+  task_data_seq->inputs_count.emplace_back(B.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(C.data()));
+  task_data_seq->outputs_count.emplace_back(C.size());
 
-	auto task_seq = std::make_shared<vavilov_v_cannon_seq::CannonSequential>(task_data_seq);
+  auto task_seq = std::make_shared<vavilov_v_cannon_seq::CannonSequential>(task_data_seq);
 
-	auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-	perf_attr->num_running = 10;
-	const auto t0 = std::chrono::high_resolution_clock::now();
-	perf_attr->current_timer = [&] {
-		auto current_time_point = std::chrono::high_resolution_clock::now();
-		auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
-		return static_cast<double>(duration) * 1e-9;
-	};
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perf_attr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
 
-	auto perf_results = std::make_shared<ppc::core::PerfResults>();
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
-	auto perf_analyzer = std::make_shared<ppc::core::Perf>(task_seq);
-	perf_analyzer->TaskRun(perf_attr, perf_results);
-	ppc::core::Perf::PrintPerfStatistic(perf_results);
-	for (size_t i = 0; i < N * N; i++) {
-		ASSERT_EQ(expected_output[i], C[i]);
-	}
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task_seq);
+  perf_analyzer->TaskRun(perf_attr, perf_results);
+  ppc::core::Perf::PrintPerfStatistic(perf_results);
+  for (size_t i = 0; i < N * N; i++) {
+    ASSERT_EQ(expected_output[i], C[i]);
+  }
 }
