19a3295d5da028b8673edef51398e160962c5dc2
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2025-02-22 14:03:53 +0300

    Update ops_seq.hpp

--- tasks/seq/vavilov_v_cannon/include/ops_seq.hpp
+++ tasks/seq/vavilov_v_cannon/include/ops_seq.hpp
@@ -8,27 +8,25 @@
 
 namespace vavilov_v_cannon_seq {
 
-    class CannonSequential : public ppc::core::Task {
-    public:
-        explicit CannonSequential(std::shared_ptr<ppc::core::TaskData> task_data)
-            : Task(std::move(task_data)) {}
+class CannonSequential : public ppc::core::Task {
+ public:
+  explicit CannonSequential(std::shared_ptr<ppc::core::TaskData> task_data) : Task(std::move(task_data)) {}
 
-        bool PreProcessingImpl() override;
-        bool ValidationImpl() override;
-        bool RunImpl() override;
-        bool PostProcessingImpl() override;
+  bool PreProcessingImpl() override;
+  bool ValidationImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
 
-    private:
-        unsigned int N;
-        unsigned int block_size;
-        unsigned int num_blocks;
-        std::vector<double> A_;
-        std::vector<double> B_;
-        std::vector<double> C_;
+ private:
+  unsigned int N;
+  unsigned int block_size;
+  unsigned int num_blocks;
+  std::vector<double> A_;
+  std::vector<double> B_;
+  std::vector<double> C_;
 
-        void InitialShift();
-        void BlockMultiply();
-        void ShiftBlocks();
-    };
-
+  void InitialShift();
+  void BlockMultiply();
+  void ShiftBlocks();
+ };
 }  // namespace vavilov_v_cannon_seq
