b40b920433f89ad936b067bc160691297d576342
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2025-02-24 22:46:38 +0300

    Update ops_seq.cpp

--- tasks/seq/vavilov_v_cannon/src/ops_seq.cpp
+++ tasks/seq/vavilov_v_cannon/src/ops_seq.cpp
@@ -19,8 +19,8 @@ bool vavilov_v_cannon_seq::CannonSequential::PreProcessingImpl() {
 }
 
 bool vavilov_v_cannon_seq::CannonSequential::ValidationImpl() {
-  int n = task_data->inputs_count[0];
-  int sqrt_n = static_cast<unsigned int>(std::sqrt(n));
+  unsigned int n = task_data->inputs_count[0];
+  unsigned int sqrt_n = static_cast<unsigned int>(std::sqrt(n));
 
   return n == task_data->inputs_count[1] && task_data->outputs_count[0] == task_data->inputs_count[0] &&
          sqrt_n * sqrt_n == n;
