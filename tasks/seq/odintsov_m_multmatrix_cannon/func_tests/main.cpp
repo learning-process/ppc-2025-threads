8a7e347e14b9ee6ad659f4937fc250eb002836c9
odincovm <odincovmisa16@gmail.com>
2025-02-24 10:01:47 +0300

    Added a validation test and a case where the block size is 1

--- tasks/seq/odintsov_m_multmatrix_cannon/func_tests/main.cpp
+++ tasks/seq/odintsov_m_multmatrix_cannon/func_tests/main.cpp
@@ -81,3 +81,47 @@ TEST(odintsov_m_mulmatrix_cannon_seq, test_matrix_900) {
   test_task_sequential.PostProcessing();
   EXPECT_EQ(out, matrix_c);
 }
+TEST(odintsov_m_mulmatrix_cannon_seq, test_sz_block_1) {
+  // Create data
+  std::vector<double> matrix_a(9, 1);
+  std::vector<double> matrix_b(9, 1);
+  std::vector<double> out(9, 0);
+  std::vector<double> matrix_c(9,3);
+
+  // Create task_data
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_a.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_b.data()));
+  task_data_seq->inputs_count.emplace_back(matrix_a.size());
+  task_data_seq->inputs_count.emplace_back(matrix_a.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+
+  // Create Task
+  odintsov_m_mulmatrix_cannon_seq::MulMatrixCannonSequential test_task_sequential(task_data_seq);
+  ASSERT_EQ(test_task_sequential.Validation(), true);
+  test_task_sequential.PreProcessing();
+
+  test_task_sequential.Run();
+  test_task_sequential.PostProcessing();
+  EXPECT_EQ(out, matrix_c);
+}
+TEST(odintsov_m_mulmatrix_cannon_seq, test_validation) {
+  // Create data
+  std::vector<double> matrix_a(12, 1);
+  std::vector<double> matrix_b(12, 1);
+  std::vector<double> out(12, 0);
+  std::vector<double> matrix_c(12, 3);
+
+  // Create task_data
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_a.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_b.data()));
+  task_data_seq->inputs_count.emplace_back(matrix_a.size());
+  task_data_seq->inputs_count.emplace_back(matrix_a.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+
+  // Create Task
+  odintsov_m_mulmatrix_cannon_seq::MulMatrixCannonSequential test_task_sequential(task_data_seq);
+  ASSERT_EQ(test_task_sequential.Validation(), false);
+ 
+}
