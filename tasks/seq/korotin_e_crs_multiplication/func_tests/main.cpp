a4706951d43cdeac03fb1e96f74ed969823394ba
korotin_e <kev1974glv@yandex.ru>
2025-02-24 16:12:08 +0300

    cogn comlex

--- tasks/seq/korotin_e_crs_multiplication/func_tests/main.cpp
+++ tasks/seq/korotin_e_crs_multiplication/func_tests/main.cpp
@@ -11,6 +11,9 @@
 namespace korotin_e_crs_multiplication_seq {
 
 std::vector<double> GetRandomMatrix(unsigned int m, unsigned int n) {
+  if (m * n == 0) {
+    throw std::invalid_argument("Can't creaate matrix with 0 rows or columns");
+  }
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<double> distrib(-100.0, 100.0);
@@ -25,6 +28,9 @@ std::vector<double> GetRandomMatrix(unsigned int m, unsigned int n) {
 
 void MakeCRS(std::vector<unsigned int> &r_i, std::vector<unsigned int> &col, std::vector<double> &val,
              const std::vector<double> &src, unsigned int m, unsigned int n) {
+  if (m * n != src.size()) {
+    throw std::invalid_argument("Size of the source matrix does not match the specified");
+  }
   r_i = std::vector<unsigned int>(m + 1, 0);
   col.clear();
   val.clear();
@@ -122,15 +128,11 @@ TEST(korotin_e_crs_multiplication_seq, test_rndcrs_stat_zeroes) {
   std::vector<unsigned int> b_col;
   a = korotin_e_crs_multiplication_seq::GetRandomMatrix(m, n);
   b = korotin_e_crs_multiplication_seq::GetRandomMatrix(n, p);
-  for (unsigned int i = 0; i < m * n; i++) {
-    if (i % 2 == 0) {
-      a[i] = 0;
-    }
+  for (unsigned int i = 0; i < (m * n) / 2; i++) {
+    a[i * 2] = 0;
   }
-  for (unsigned int i = 0; i < n * p; i++) {
-    if (i % 2 == 1) {
-      b[i] = 0;
-    }
+  for (unsigned int i = 0; i < (n * p) / 2; i++) {
+    b[(i * 2) + 1] = 0;
   }
 
   korotin_e_crs_multiplication_seq::MakeCRS(a_ri, a_col, a_val, a, m, n);
@@ -151,20 +153,6 @@ TEST(korotin_e_crs_multiplication_seq, test_rndcrs_stat_zeroes) {
   task_data_seq->inputs_count.emplace_back(b_col.size());
   task_data_seq->inputs_count.emplace_back(b_val.size());
 
-  std::vector<unsigned int> out_ri(a_ri.size(), 0);
-  std::vector<unsigned int> out_col(m * p);
-  std::vector<double> out_val(m * p);
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_ri.data()));
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_col.data()));
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_val.data()));
-  task_data_seq->outputs_count.emplace_back(out_ri.size());
-
-  korotin_e_crs_multiplication_seq::CrsMultiplicationSequential test_task_sequential(task_data_seq);
-  ASSERT_EQ(test_task_sequential.Validation(), true);
-  test_task_sequential.PreProcessing();
-  test_task_sequential.Run();
-  test_task_sequential.PostProcessing();
-
   std::vector<double> c(m * p, 0);
   std::vector<double> c_val;
   std::vector<unsigned int> c_ri;
@@ -176,8 +164,22 @@ TEST(korotin_e_crs_multiplication_seq, test_rndcrs_stat_zeroes) {
       }
     }
   }
-
   korotin_e_crs_multiplication_seq::MakeCRS(c_ri, c_col, c_val, c, m, p);
+
+  std::vector<unsigned int> out_ri(a_ri.size(), 0);
+  std::vector<unsigned int> out_col(c_col.size());
+  std::vector<double> out_val(c_val.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_ri.data()));
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_col.data()));
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_val.data()));
+  task_data_seq->outputs_count.emplace_back(out_ri.size());
+
+  korotin_e_crs_multiplication_seq::CrsMultiplicationSequential test_task_sequential(task_data_seq);
+  ASSERT_EQ(test_task_sequential.Validation(), true);
+  test_task_sequential.PreProcessing();
+  test_task_sequential.Run();
+  test_task_sequential.PostProcessing();
+
   ASSERT_EQ(c_ri, out_ri);
   ASSERT_EQ(c_col, out_col);
   ASSERT_EQ(c_val, out_val);
@@ -226,20 +228,6 @@ TEST(korotin_e_crs_multiplication_seq, test_rndcrs) {
   task_data_seq->inputs_count.emplace_back(b_col.size());
   task_data_seq->inputs_count.emplace_back(b_val.size());
 
-  std::vector<unsigned int> out_ri(a_ri.size(), 0);
-  std::vector<unsigned int> out_col(m * p);
-  std::vector<double> out_val(m * p);
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_ri.data()));
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_col.data()));
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_val.data()));
-  task_data_seq->outputs_count.emplace_back(out_ri.size());
-
-  korotin_e_crs_multiplication_seq::CrsMultiplicationSequential test_task_sequential(task_data_seq);
-  ASSERT_EQ(test_task_sequential.Validation(), true);
-  test_task_sequential.PreProcessing();
-  test_task_sequential.Run();
-  test_task_sequential.PostProcessing();
-
   std::vector<double> c(m * p, 0);
   std::vector<double> c_val;
   std::vector<unsigned int> c_ri;
@@ -251,8 +239,22 @@ TEST(korotin_e_crs_multiplication_seq, test_rndcrs) {
       }
     }
   }
-
   korotin_e_crs_multiplication_seq::MakeCRS(c_ri, c_col, c_val, c, m, p);
+
+  std::vector<unsigned int> out_ri(a_ri.size(), 0);
+  std::vector<unsigned int> out_col(c_col.size());
+  std::vector<double> out_val(c_val.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_ri.data()));
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_col.data()));
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_val.data()));
+  task_data_seq->outputs_count.emplace_back(out_ri.size());
+
+  korotin_e_crs_multiplication_seq::CrsMultiplicationSequential test_task_sequential(task_data_seq);
+  ASSERT_EQ(test_task_sequential.Validation(), true);
+  test_task_sequential.PreProcessing();
+  test_task_sequential.Run();
+  test_task_sequential.PostProcessing();
+
   ASSERT_EQ(c_ri, out_ri);
   ASSERT_EQ(c_col, out_col);
   ASSERT_EQ(c_val, out_val);
