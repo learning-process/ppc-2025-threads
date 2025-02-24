ba1b8949c6084c2a28856340fe89431cae4d01fc
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-02-24 05:36:39 +0300

    Update ops_seq.cpp

--- tasks/seq/deryabin_m_hoare_sort_simple_merge/src/ops_seq.cpp
+++ tasks/seq/deryabin_m_hoare_sort_simple_merge/src/ops_seq.cpp
@@ -89,6 +89,7 @@ bool deryabin_m_hoare_sort_simple_merge_seq::HoareSortTaskSequential::RunImpl() 
     for (size_t j = 0; j < chunk_count; j++) {
       if (i == (size_t)(log((double)chunk_count_) / std::numbers::ln2) - 1) {
         MergeTwoParts(input_array_A_, 0, dimension_ - 1);
+        chunk_count--;
       } else {
         MergeTwoParts(input_array_A_, 2 * j * min_chunk_size_ * (i + 1), (2 * (j + 1) * min_chunk_size_ * (i + 1)) - 1);
         chunk_count--;
