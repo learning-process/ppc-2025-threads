f27bd734e46694a3b421abaea51b28d16a7dedf8
KAPUSHA228 <shura_tsatsyn_2003@mail.ru>
2025-02-25 15:11:34 +0300

    formatfix

--- tasks/seq/tsatsyn_a_radix_sort_simple_merge_seq/include/ops_seq.hpp
+++ tasks/seq/tsatsyn_a_radix_sort_simple_merge_seq/include/ops_seq.hpp
@@ -9,7 +9,7 @@ namespace tsatsyn_a_radix_sort_simple_merge_seq {
 std::vector<double> GetRandomVector(int sz, int a, int b);
 std::pair<std::vector<uint64_t>, std::vector<uint64_t>> ParseOrigin(std::vector<double> &input_data);
 int CalculateBits(const std::vector<uint64_t> &data, bool is_pozitive);
-  class TestTaskSequential : public ppc::core::Task {
+class TestTaskSequential : public ppc::core::Task {
  public:
   explicit TestTaskSequential(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
   bool PreProcessingImpl() override;
