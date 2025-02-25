7e81bb800f53c4f54b4bf25bcd20c67abd01e982
Александра Вершинина <xx.vershinina.xx@yandex.ru>
2025-02-26 00:26:41 +0300

    com 6

--- tasks/seq/vershinina_a_hoare_sort/perf_tests/main.cpp
+++ tasks/seq/vershinina_a_hoare_sort/perf_tests/main.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <chrono>
+#include <cstddef>
 #include <cstdint>
 #include <memory>
 #include <random>
@@ -11,7 +12,7 @@
 #include "core/task/include/task.hpp"
 #include "seq/vershinina_a_hoare_sort/include/ops_seq.hpp"
 namespace {
-std::vector<int> GetRandomVector(size_t len) {
+std::vector<int> GetRandomVector(int len) {
   std::random_device dev;
   std::mt19937 gen(dev());
   std::uniform_int_distribution<> distr(0, 100);
