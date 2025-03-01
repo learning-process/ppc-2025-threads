c172c0acf24976df6966ab788626d0278b0b570e
NikKazzzzzz <127418135+NikKazzzzzz@users.noreply.github.com>
2025-03-01 17:37:55 +0300

    Adapt tests

--- tasks/tbb/kazunin_n_montecarlo/func_tests/main.cpp
+++ tasks/tbb/kazunin_n_montecarlo/func_tests/main.cpp
@@ -17,8 +17,9 @@ using std::sin;
 
 namespace {
 template <std::size_t N, typename F>
-void MonteCarloTest(F f, std::size_t precision, std::array<std::pair<double, double>, N> limits, double ref) {
+void MonteCarloTest(F f, std::size_t precision, std::array<std::pair<double, double>, N> limits) {
   double out = 0.0;
+  double ref = 0.0;
 
   auto task_data = std::make_shared<ppc::core::TaskData>();
   task_data->inputs = {reinterpret_cast<uint8_t *>(&precision), reinterpret_cast<uint8_t *>(&limits)};
@@ -32,7 +33,14 @@ void MonteCarloTest(F f, std::size_t precision, std::array<std::pair<double, dou
   task_tbb.PreProcessing();
   task_tbb.Run();
   task_tbb.PostProcessing();
-  EXPECT_NEAR(out, ref, 0.2);
+
+  task_data->outputs = {reinterpret_cast<uint8_t *>(&ref)};
+  kazunin_n_montecarlo_seq::MonteCarloSeq<N, F> task_seq(task_data, f);
+  ASSERT_TRUE(task_seq.Validation());
+  task_seq.PreProcessing();
+  task_seq.Run();
+  task_seq.PostProcessing();
+  EXPECT_NEAR(out, ref, 0.15);
 }
 template <std::size_t N, typename F>
 void InvalidMonteCarloTest(F f, std::size_t precision, std::array<std::pair<double, double>, N> limits) {
@@ -45,8 +53,8 @@ void InvalidMonteCarloTest(F f, std::size_t precision, std::array<std::pair<doub
   task_data->outputs_count = {1};
 
   // Create Task
-  kazunin_n_montecarlo_tbb::MonteCarloTbb<N, F> task_tbb(task_data, f);
-  EXPECT_FALSE(task_tbb.Validation());
+  kazunin_n_montecarlo_tbb::MonteCarloTbb<N, F> task(task_data, f);
+  EXPECT_FALSE(task.Validation());
 }
 }  // namespace
 
@@ -72,7 +80,7 @@ TEST(kazunin_n_montecarlo_tbb, sin_prod_2d) {
         return std::accumulate(args.begin(), args.end(), 1.0,
                                [](const double acc, double component) { return acc * sin(component); });
       },
-      5000, {{{0.0, 1.0}, {-1.0, 0.0}}}, -0.211);
+      5000, {{{0.0, 1.0}, {-1.0, 0.0}}});
 }
 
 TEST(kazunin_n_montecarlo_tbb, sin_sum_2d) {
@@ -82,7 +90,7 @@ TEST(kazunin_n_montecarlo_tbb, sin_sum_2d) {
         return std::accumulate(args.begin(), args.end(), 0.0,
                                [](const double acc, double component) { return acc + sin(component); });
       },
-      5000, {{{0.0, 1.0}, {-1.0, 0.0}}}, 0.0044);
+      5000, {{{0.0, 1.0}, {-1.0, 0.0}}});
 }
 
 TEST(kazunin_n_montecarlo_tbb, sin_prod_3d) {
@@ -92,7 +100,7 @@ TEST(kazunin_n_montecarlo_tbb, sin_prod_3d) {
         return std::accumulate(args.begin(), args.end(), 1.0,
                                [](const double acc, double component) { return acc * sin(component); });
       },
-      5000, {{{0.0, 1.0}, {-1.0, 0.0}, {-1.0, 0.0}}}, 0.0985);
+      5000, {{{0.0, 1.0}, {-1.0, 0.0}, {-1.0, 0.0}}});
 }
 
 TEST(kazunin_n_montecarlo_tbb, sin_sum_3d) {
@@ -102,7 +110,7 @@ TEST(kazunin_n_montecarlo_tbb, sin_sum_3d) {
         return std::accumulate(args.begin(), args.end(), 0.0,
                                [](const double acc, double component) { return acc + sin(component); });
       },
-      5000, {{{0.0, 1.0}, {-1.0, 0.0}, {-1.0, 0.0}}}, -0.4464);
+      5000, {{{0.0, 1.0}, {-1.0, 0.0}, {-1.0, 0.0}}});
 }
 
 TEST(kazunin_n_montecarlo_tbb, product_3d) {
@@ -111,7 +119,7 @@ TEST(kazunin_n_montecarlo_tbb, product_3d) {
       [](const std::array<double, n> &args) {
         return std::accumulate(args.begin(), args.end(), 1.0, std::multiplies<>());
       },
-      5000, {{{0.0, 1.0}, {0.0, 1.0}, {0.0, 1.0}}}, 1.0 / 8.0);
+      5000, {{{0.0, 1.0}, {0.0, 1.0}, {0.0, 1.0}}});
 }
 
 TEST(kazunin_n_montecarlo_tbb, sum_3d) {
@@ -120,7 +128,7 @@ TEST(kazunin_n_montecarlo_tbb, sum_3d) {
       [](const std::array<double, n> &args) {
         return std::accumulate(args.begin(), args.end(), 1.0, std::multiplies<>());
       },
-      5000, {{{0.0, 1.0}, {0.0, 1.0}, {0.0, 1.0}}}, 1.0 / 8.0);
+      5000, {{{0.0, 1.0}, {0.0, 1.0}, {0.0, 1.0}}});
 }
 
 TEST(kazunin_n_montecarlo_tbb, sin_prod_2d_degenerate) {
@@ -130,7 +138,7 @@ TEST(kazunin_n_montecarlo_tbb, sin_prod_2d_degenerate) {
         return std::accumulate(args.begin(), args.end(), 1.0,
                                [](const double acc, double component) { return acc * sin(component); });
       },
-      5000, {{{0.0, 0.0}, {0.0, 0.0}}}, 0);
+      5000, {{{0.0, 0.0}, {0.0, 0.0}}});
 }
 
 TEST(kazunin_n_montecarlo_tbb, sin_sum_2d_degenerate) {
@@ -140,7 +148,7 @@ TEST(kazunin_n_montecarlo_tbb, sin_sum_2d_degenerate) {
         return std::accumulate(args.begin(), args.end(), 0.0,
                                [](const double acc, double component) { return acc + sin(component); });
       },
-      5000, {{{0.0, 0.0}, {0.0, 0.0}}}, 0);
+      5000, {{{0.0, 0.0}, {0.0, 0.0}}});
 }
 
 TEST(kazunin_n_montecarlo_tbb, sin_prod_3d_degenerate) {
@@ -150,7 +158,7 @@ TEST(kazunin_n_montecarlo_tbb, sin_prod_3d_degenerate) {
         return std::accumulate(args.begin(), args.end(), 1.0,
                                [](const double acc, double component) { return acc * sin(component); });
       },
-      5000, {{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}}}, 0);
+      5000, {{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}}});
 }
 
 TEST(kazunin_n_montecarlo_tbb, sin_sum_3d_degenerate) {
@@ -160,7 +168,7 @@ TEST(kazunin_n_montecarlo_tbb, sin_sum_3d_degenerate) {
         return std::accumulate(args.begin(), args.end(), 0.0,
                                [](const double acc, double component) { return acc + sin(component); });
       },
-      5000, {{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}}}, 0);
+      5000, {{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}}});
 }
 
 TEST(kazunin_n_montecarlo_tbb, product_3d_degenerate) {
@@ -169,7 +177,7 @@ TEST(kazunin_n_montecarlo_tbb, product_3d_degenerate) {
       [](const std::array<double, n> &args) {
         return std::accumulate(args.begin(), args.end(), 1.0, std::multiplies<>());
       },
-      5000, {{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}}}, 0);
+      5000, {{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}}});
 }
 
 TEST(kazunin_n_montecarlo_tbb, sum_3d_degenerate) {
@@ -178,7 +186,7 @@ TEST(kazunin_n_montecarlo_tbb, sum_3d_degenerate) {
       [](const std::array<double, n> &args) {
         return std::accumulate(args.begin(), args.end(), 1.0, std::multiplies<>());
       },
-      5000, {{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}}}, 0);
+      5000, {{{0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}}});
 }
 
 TEST(kazunin_n_montecarlo_tbb, sum_3d_coinciding_bounds) {
@@ -187,5 +195,5 @@ TEST(kazunin_n_montecarlo_tbb, sum_3d_coinciding_bounds) {
       [](const std::array<double, n> &args) {
         return std::accumulate(args.begin(), args.end(), 1.0, std::multiplies<>());
       },
-      5000, {{{1.0, 1.0}, {5.0, 5.0}, {9.0, 9.0}}}, 0);
+      5000, {{{1.0, 1.0}, {5.0, 5.0}, {9.0, 9.0}}});
 }
