9f0caf9f4e413260371617f171db1df8d087ccf9
ermilovad <darya.ermilova04@inbox.ru>
2025-03-02 16:54:02 +0300

    v1.4

--- tasks/seq/ermilova_d_shell_sort_batcher_even-odd_merger/func_tests/main.cpp
+++ tasks/seq/ermilova_d_shell_sort_batcher_even-odd_merger/func_tests/main.cpp
@@ -25,51 +25,6 @@ std::vector<int> GetRandomVector(int size, int upper_border, int lower_border) {
 }
 }  // namespace
 
-TEST(ermilova_d_shell_sort_batcher_even_odd_merger_seq, Can_create_vector) {
-  const int size_test = 10;
-  const int upper_border_test = 100;
-  const int lower_border_test = -100;
-  EXPECT_NO_THROW(GetRandomVector(size_test, upper_border_test, lower_border_test));
-}
-
-TEST(ermilova_d_shell_sort_batcher_even_odd_merger_seq, Cant_create_incorrect_vector) {
-  const int size_test = -10;
-  const int upper_border_test = 100;
-  const int lower_border_test = -100;
-  EXPECT_ANY_THROW(GetRandomVector(size_test, upper_border_test, lower_border_test));
-}
-
-//TEST(ermilova_d_shell_sort_batcher_even_odd_merger_seq, vec_0) {
-//  // Create data
-//  const int upper_border_test = 1000;
-//  const int lower_border_test = -1000;
-//  const int size = 1;
-//
-//  bool is_resersed = false;
-//
-//  std::vector<int> in = {};
-//  std::vector<int> out(in.size(), 0);
-//
-//  std::vector<int> ref = in;
-//  std::ranges::sort(ref);
-//
-//  // Create task_data
-//  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
-//  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-//  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&is_resersed));
-//  task_data_seq->inputs_count.emplace_back(in.size());
-//  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-//  task_data_seq->outputs_count.emplace_back(out.size());
-//
-//  // Create Task
-//  ermilova_d_shell_sort_batcher_even_odd_merger_seq::TestTaskSequential test_task_sequential(task_data_seq);
-//  ASSERT_EQ(test_task_sequential.Validation(), true);
-//  test_task_sequential.PreProcessing();
-//  test_task_sequential.Run();
-//  test_task_sequential.PostProcessing();
-//  EXPECT_EQ(ref, out);
-//}
-
 TEST(ermilova_d_shell_sort_batcher_even_odd_merger_seq, vec_1) {
   // Create data
   const int upper_border_test = 1000;
