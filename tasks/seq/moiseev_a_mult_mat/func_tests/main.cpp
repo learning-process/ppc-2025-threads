d7f447389f8ccd49b3e7358ceb5f33e2f23b5d7a
MoiseevArt <amoiseev_00@mail.ru>
2025-03-02 11:49:56 +0000

    fix 1.0

--- tasks/seq/moiseev_a_mult_mat/func_tests/main.cpp
+++ tasks/seq/moiseev_a_mult_mat/func_tests/main.cpp
@@ -116,3 +116,53 @@ TEST(moiseev_a_mult_mat_seq, test_zero_matrix) {
 
   EXPECT_EQ(c, b);
 }
+
+TEST(moiseev_a_mult_mat_seq, test_known_result) {
+  constexpr size_t kSize = 2;
+  std::vector<double> a = {1, 2, 3, 4};
+  std::vector<double> b = {5, 6, 7, 8};
+  std::vector<double> expected_c = {19, 22, 43, 50};
+
+  std::vector<double> c(kSize * kSize, 0.0);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(a.data()));
+  task_data_seq->inputs_count.emplace_back(a.size());
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(b.data()));
+  task_data_seq->inputs_count.emplace_back(b.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(c.data()));
+  task_data_seq->outputs_count.emplace_back(c.size());
+
+  moiseev_a_mult_mat_seq::MultMatSequential test_task_sequential(task_data_seq);
+  ASSERT_TRUE(test_task_sequential.Validation());
+  test_task_sequential.PreProcessing();
+  test_task_sequential.Run();
+  test_task_sequential.PostProcessing();
+
+  EXPECT_EQ(c, expected_c);
+}
+
+TEST(moiseev_a_mult_mat_seq, test_repeated_values) {
+  constexpr size_t kSize = 2;
+  std::vector<double> a = {2, 2, 2, 2};
+  std::vector<double> b = {3, 3, 3, 3};
+  std::vector<double> expected_c = {12, 12, 12, 12};
+
+  std::vector<double> c(kSize * kSize, 0.0);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(a.data()));
+  task_data_seq->inputs_count.emplace_back(a.size());
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(b.data()));
+  task_data_seq->inputs_count.emplace_back(b.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(c.data()));
+  task_data_seq->outputs_count.emplace_back(c.size());
+
+  moiseev_a_mult_mat_seq::MultMatSequential test_task_sequential(task_data_seq);
+  ASSERT_TRUE(test_task_sequential.Validation());
+  test_task_sequential.PreProcessing();
+  test_task_sequential.Run();
+  test_task_sequential.PostProcessing();
+
+  EXPECT_EQ(c, expected_c);
+}
