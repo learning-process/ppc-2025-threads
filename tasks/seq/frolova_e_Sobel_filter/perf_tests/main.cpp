d5298e174a00cff087cc09e4bcfb1e14b8cdd617
ElizavetaFrolova <liza1827frol@gmail.com>
2025-03-04 01:29:37 +0300

    commit static_cast9

--- tasks/seq/frolova_e_Sobel_filter/perf_tests/main.cpp
+++ tasks/seq/frolova_e_Sobel_filter/perf_tests/main.cpp
@@ -93,6 +93,5 @@ TEST(frolova_e_Sobel_filter_seq, test_task_run) {
   auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_sequential);
   perf_analyzer->TaskRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
-  size_t exp_size = 4000000;
-  EXPECT_TRUE(exp_size == res.size());
+  ASSERT_EQ(reference.size(), res.size());
 }
