0b61ccd4775310109b5cf8df6de0d5ab33d0cc8d
Kirius257 <holin.kirill@yandex.ru>
2025-03-03 19:40:58 +0300

    ubuntu-test: fix time

--- tasks/seq/kholin_k_multidimensional_integrals_rectangle/func_tests/main.cpp
+++ tasks/seq/kholin_k_multidimensional_integrals_rectangle/func_tests/main.cpp
@@ -227,7 +227,7 @@ TEST(kholin_k_multidimensional_integrals_rectangle_seq, double_integral_two_var)
   auto f = [](const std::vector<double> &f_values) { return (f_values[0] + f_values[1]); };
   std::vector<double> in_lower_limits{0, 0};
   std::vector<double> in_upper_limits{3, 3};
-  double n = 1000.0;
+  double n = 256.0;
   std::vector<double> out_i(1, 0.0);
 
   auto *f_object = new std::function<double(const std::vector<double> &)>(f);
@@ -303,7 +303,7 @@ TEST(kholin_k_multidimensional_integrals_rectangle_seq, triple_integral_three_va
   auto f = [](const std::vector<double> &f_values) { return f_values[0] + f_values[1] + f_values[2]; };
   std::vector<double> in_lower_limits{0, 0, 0};
   std::vector<double> in_upper_limits{2, 2, 2};
-  double n = 100.0;
+  double n = 20.0;
   std::vector<double> out_i(1, 0.0);
 
   auto *f_object = new std::function<double(const std::vector<double> &)>(f);
