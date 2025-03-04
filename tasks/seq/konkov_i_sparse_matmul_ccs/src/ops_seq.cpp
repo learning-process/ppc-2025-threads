637880e4ece5aed7396ca9f526548d0305a223b3
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-04 18:02:08 +0300

    del comments

--- tasks/seq/konkov_i_sparse_matmul_ccs/src/ops_seq.cpp
+++ tasks/seq/konkov_i_sparse_matmul_ccs/src/ops_seq.cpp
@@ -7,7 +7,6 @@ namespace konkov_i_sparse_matmul_ccs {
 SparseMatmulTask::SparseMatmulTask(ppc::core::TaskDataPtr task_data) : ppc::core::Task(task_data) {}
 
 bool SparseMatmulTask::ValidationImpl() {
-  // Проверка корректности входных данных
   if (A_values.size() == 0 || B_values.size() == 0) {
     std::cerr << "Error: Empty matrix data\n";
     return false;
@@ -20,27 +19,21 @@ bool SparseMatmulTask::ValidationImpl() {
 }
 
 bool SparseMatmulTask::PreProcessingImpl() {
-  // Подготовка данных для умножения
-  // Например, можем создать дополнительные структуры для столбцов и значений
   return true;
 }
 
 bool SparseMatmulTask::RunImpl() {
-  // Реализация алгоритма умножения разреженных матриц
   C_values.resize(rowsA * colsB, 0.0);
 
-  // Процесс умножения
   for (size_t i = 0; i < A_values.size(); ++i) {
     for (size_t j = 0; j < B_values.size(); ++j) {
-      // Алгоритм умножения разреженных матриц в формате CCS
-      C_values[i] += A_values[i] * B_values[j];  // Пример простого умножения
+      C_values[i] += A_values[i] * B_values[j];
     }
   }
   return true;
 }
 
 bool SparseMatmulTask::PostProcessingImpl() {
-  // Завершение работы задачи, обработка результата умножения
   std::cout << "Result matrix C:\n";
   for (const auto& value : C_values) {
     std::cout << value << " ";
@@ -49,4 +42,4 @@ bool SparseMatmulTask::PostProcessingImpl() {
   return true;
 }
 
-}  // namespace seq
+}  // namespace konkov_i_sparse_matmul_ccs
