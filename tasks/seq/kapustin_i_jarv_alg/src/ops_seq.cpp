6a79491a5c1ecff17557c49a80a4bf54e22a8d65
Ivan <ivankapustin60@gmail.com>
2025-03-05 13:00:45 +0300

    fix incl 2

--- tasks/seq/kapustin_i_jarv_alg/src/ops_seq.cpp
+++ tasks/seq/kapustin_i_jarv_alg/src/ops_seq.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cmath>
 #include <utility>
+#include <cstddef>
 #include <vector>
 
 int kapustin_i_jarv_alg_seq::TestTaskSequential::Orientation(const std::pair<int, int>& p, const std::pair<int, int>& q,
