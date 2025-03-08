3bf88ac0ea6bc39e0227aae1446958e1e4aedb50
IljaSmirnov <ilsmirnov693n@gmail.com>
2025-03-08 18:22:51 +0300

    update

--- tasks/omp/smirnov_i_radix_sort_simple_merge/src/ops_omp.cpp
+++ tasks/omp/smirnov_i_radix_sort_simple_merge/src/ops_omp.cpp
@@ -83,9 +83,6 @@ bool smirnov_i_radix_sort_simple_merge_omp::TestTaskOpenMP::RunImpl() {
       local_mas.push_back(mas_[i]);
     }
     radix_sort(local_mas);
-    if (!local_mas.empty()) {
-      printf("first %d %d \n", num, local_mas[0]);
-    }
 #pragma omp critical
     {
       if (!local_mas.empty()) A.push_back(local_mas);
@@ -105,7 +102,6 @@ bool smirnov_i_radix_sort_simple_merge_omp::TestTaskOpenMP::RunImpl() {
       }
       if (!mas1.empty() && !mas2.empty()) {
         merge_mas = merge(mas1, mas2);
-        printf("thir d%d %ld %ld %ld\n", num, mas1.size(), mas2.size(), merge_mas.size());
       }
 
       if (!merge_mas.empty()) {
@@ -129,7 +125,6 @@ bool smirnov_i_radix_sort_simple_merge_omp::TestTaskOpenMP::RunImpl() {
       if (static_cast<int>(A.size()) == 1) {
         sort_res.resize(A[0].size());
         std::copy(A[0].begin(), A[0].end(), sort_res.begin());
-        printf("second %d %d %d %d\n", num, A[0][0], A[0][1], A[0][2]);
       }
     }
     printf("second %d %d %d %d\n", num, sort_res[0], sort_res[1], sort_res[2]);
