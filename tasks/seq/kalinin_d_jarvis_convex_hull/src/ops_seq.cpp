19d0e6beae043bbf77f4701d2782083e94a9bffd
kalinin_plush <125553054+Dmit-bit-m@users.noreply.github.com>
2025-03-09 21:05:12 +0300

    fix v6

--- tasks/seq/kalinin_d_jarvis_convex_hull/src/ops_seq.cpp
+++ tasks/seq/kalinin_d_jarvis_convex_hull/src/ops_seq.cpp
@@ -16,8 +16,8 @@ std::vector<kalinin_d_jarvis_convex_hull_seq::Point> kalinin_d_jarvis_convex_hul
   kalinin_d_jarvis_convex_hull_seq::Point p0 = points[0];
   for (const auto& p : points) {
     if (p.x < p0.x || (p.x == p0.x && p.y < p0.y)) {
-      p0 = p
-    };
+      p0 = p;
+    }
   }
   std::vector<kalinin_d_jarvis_convex_hull_seq::Point> convex_hull = {p0};
   kalinin_d_jarvis_convex_hull_seq::Point prev_point = p0;
@@ -44,16 +44,16 @@ std::vector<kalinin_d_jarvis_convex_hull_seq::Point> kalinin_d_jarvis_convex_hul
     if (next_point == p0) {
       break;
     }
-    convexHull.push_back(next_point);
+    convex_hull.push_back(next_point);
     prev_point = next_point;
   }
 
-  return convexHull;
+  return convex_hull;
 }
 
 bool kalinin_d_jarvis_convex_hull_seq::TestTaskSequential::PreProcessingImpl() {
   // Init value for input and output
-  points.resize(task_data->inputs_count[0]);
+  points_.resize(task_data->inputs_count[0]);
   auto* tmp_ptr_a = reinterpret_cast<kalinin_d_jarvis_convex_hull_seq::Point*>(task_data->inputs[0]);
   std::copy_n(tmp_ptr_a, task_data->inputs_count[0], points.begin());
   return true;
@@ -64,17 +64,17 @@ bool kalinin_d_jarvis_convex_hull_seq::TestTaskSequential::ValidationImpl() {
     return false;
   }
 
-  std::ranges::sort(points.begin(), points.end());
+  std::ranges::sort(points.begin(), points_.end());
   return std::ranges::unique(points.begin(), points.end()) == points.end();
 }
 
 bool kalinin_d_jarvis_convex_hull_seq::TestTaskSequential::RunImpl() {
-  convexHullPoints = kalinin_d_jarvis_convex_hull_seq::Jarvis(points);
+  convexHullPoints_ = kalinin_d_jarvis_convex_hull_seq::Jarvis(points_);
   return true;
 }
 
 bool kalinin_d_jarvis_convex_hull_seq::TestTaskSequential::PostProcessingImpl() {
   auto* output_ptr = reinterpret_cast<kalinin_d_jarvis_convex_hull_seq::Point*>(task_data->outputs[0]);
-  std::copy_n(convexHullPoints.begin(), convexHullPoints.size(), output_ptr);
+  std::copy_n(convexHullPoints_.begin(), convexHullPoints_.size(), output_ptr);
   return true;
 }
