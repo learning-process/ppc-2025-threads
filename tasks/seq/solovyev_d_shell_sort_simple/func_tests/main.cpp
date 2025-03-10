9e7bad5d60ec4365af7c526b1fe054b5ca87d7f5
0xG00SE <61384845+DSolo03@users.noreply.github.com>
2025-03-10 11:13:44 +0300

    more tests

--- tasks/seq/solovyev_d_shell_sort_simple/func_tests/main.cpp
+++ tasks/seq/solovyev_d_shell_sort_simple/func_tests/main.cpp
@@ -32,6 +32,48 @@ bool IsSorted(std::vector<int> data) {
 }
 }  // namespace
 
+TEST(solovyev_d_shell_sort_simple_seq, sort_empty) {
+  // Create data
+  std::vector<int> in = {};
+  std::vector<int> out(in.size(), 0);
+
+  // Create task_data
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_seq->inputs_count.emplace_back(in.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_seq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  solovyev_d_shell_sort_simple_seq::TaskSequential task_sequential(task_data_seq);
+  ASSERT_EQ(task_sequential.Validation(), true);
+  task_sequential.PreProcessing();
+  task_sequential.Run();
+  task_sequential.PostProcessing();
+  ASSERT_TRUE(IsSorted(out));
+}
+
+TEST(solovyev_d_shell_sort_simple_seq, sort_10_negative) {
+  // Create data
+  std::vector<int> in = {1, 5, -7, 3, 7, -3, 8, 4, -1, 6};
+  std::vector<int> out(in.size(), 0);
+
+  // Create task_data
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_seq->inputs_count.emplace_back(in.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_seq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  solovyev_d_shell_sort_simple_seq::TaskSequential task_sequential(task_data_seq);
+  ASSERT_EQ(task_sequential.Validation(), true);
+  task_sequential.PreProcessing();
+  task_sequential.Run();
+  task_sequential.PostProcessing();
+  ASSERT_TRUE(IsSorted(out));
+}
+
 TEST(solovyev_d_shell_sort_simple_seq, sort_10) {
   // Create data
   std::vector<int> in = {1, 5, 7, 3, 7, 3, 8, 4, 1, 6};
@@ -56,6 +98,28 @@ TEST(solovyev_d_shell_sort_simple_seq, sort_10) {
 TEST(solovyev_d_shell_sort_simple_seq, sort_20) {
   // Create data
   std::vector<int> in = {1, 5, 7, 3, 7, 3, 8, 4, 1, 6, 4, 6, 7, 3, 12, 21, 65, 43, 1, 54, 34, 76};
+  std::vector<int> out(in.size(), 0);
+
+  // Create task_data
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_seq->inputs_count.emplace_back(in.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_seq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  solovyev_d_shell_sort_simple_seq::TaskSequential task_sequential(task_data_seq);
+  ASSERT_EQ(task_sequential.Validation(), true);
+  task_sequential.PreProcessing();
+  task_sequential.Run();
+  task_sequential.PostProcessing();
+  ASSERT_TRUE(IsSorted(out));
+}
+
+TEST(solovyev_d_shell_sort_simple_seq, sort_30_negative) {
+  // Create data
+  std::vector<int> in = {1,  5,  7, 3,  7,  3,  -8,   4,  1,   6,   4,  6, 7,   3, -12,  21,
+                         -65, -43, 1, 54, 34, 76, -345, 21, 765, 346, 34, 1, 434, 8, 343, -88};
   std::vector<int> out(in.size(), 0);
 
   // Create task_data
