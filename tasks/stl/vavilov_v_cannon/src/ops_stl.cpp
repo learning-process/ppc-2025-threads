748600f9dcf942d337c9f0b1c7116fe2d5f63fd9
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2025-03-15 00:16:12 +0300

    Update ops_stl.cpp

--- tasks/stl/vavilov_v_cannon/src/ops_stl.cpp
+++ tasks/stl/vavilov_v_cannon/src/ops_stl.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <mutex>
 #include <thread>
 #include <vector>
 
@@ -24,10 +25,38 @@ bool vavilov_v_cannon_stl::CannonSTL::ValidationImpl() {
          task_data->outputs_count[0] == task_data->inputs_count[0];
 }
 
+void vavilov_v_cannon_stl::CannonSTL::InitialShift() {
+  std::vector<double> a_tmp = A_;
+  std::vector<double> b_tmp = B_;
+  std::vector<std::thread> threads;
+
+  auto shift_work = [&](int bi) {
+    for (int bj = 0; bj < num_blocks_; ++bj) {
+      int src_row = (bi + bj) % num_blocks_;
+      int src_col = (bj + bi) % num_blocks_;
+      for (int i = 0; i < block_size_; ++i) {
+        for (int j = 0; j < block_size_; ++j) {
+          B_[(bi * block_size_ + i) * N_ + (bj * block_size_ + j)] =
+              b_tmp[(src_row * block_size_ + i) * N_ + (bj * block_size_ + j)];
+          A_[(bi * block_size_ + i) * N_ + (bj * block_size_ + j)] =
+              a_tmp[(bi * block_size_ + i) * N_ + (src_col * block_size_ + j)];
+        }
+      }
+    }
+  };
+
+  for (int bi = 0; bi < num_blocks_; ++bi) {
+    threads.emplace_back(shift_work, bi);
+  }
+  for (auto &thread : threads) {
+    thread.join();
+  }
+}
+
 void vavilov_v_cannon_stl::CannonSTL::BlockMultiply() {
   std::vector<std::vector<double>> C_temp(num_blocks_ * num_blocks_, std::vector<double>(block_size_ * block_size_, 0));
   std::vector<std::thread> threads;
-
+  
   auto multiply_work = [&](int bi, int bj) {
     std::vector<double> &C_local = C_temp[bi * num_blocks_ + bj];
     for (int bk = 0; bk < num_blocks_; ++bk) {
@@ -63,6 +92,42 @@ void vavilov_v_cannon_stl::CannonSTL::BlockMultiply() {
         }
       }
     }
+  }
+}
+
+  for (int bi = 0; bi < num_blocks_; ++bi) {
+    threads.emplace_back(multiply_work, bi);
+  }
+  for (auto &thread : threads) {
+    thread.join();
+  }
+}
+
+void vavilov_v_cannon_stl::CannonSTL::ShiftBlocks() {
+  std::vector<double> a_tmp = A_;
+  std::vector<double> b_tmp = B_;
+  std::vector<std::thread> threads;
+
+  auto shift_work = [&](int bi) {
+    for (int bj = 0; bj < num_blocks_; ++bj) {
+      int src_row = (bi + 1) % num_blocks_;
+      int src_col = (bj + 1) % num_blocks_;
+      for (int i = 0; i < block_size_; ++i) {
+        for (int j = 0; j < block_size_; ++j) {
+          B_[(bi * block_size_ + i) * N_ + (bj * block_size_ + j)] =
+              b_tmp[(src_row * block_size_ + i) * N_ + (bj * block_size_ + j)];
+          A_[(bi * block_size_ + i) * N_ + (bj * block_size_ + j)] =
+              a_tmp[(bi * block_size_ + i) * N_ + (src_col * block_size_ + j)];
+        }
+      }
+    }
+  };
+
+  for (int bi = 0; bi < num_blocks_; ++bi) {
+    threads.emplace_back(shift_work, bi);
+  }
+  for (auto &thread : threads) {
+    thread.join();
   }
 }
 
