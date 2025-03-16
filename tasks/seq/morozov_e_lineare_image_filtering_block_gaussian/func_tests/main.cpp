37f98dc948985049a4c2b6075277f9ff2ee42b28
Egor1dzeN <egormorozov123450@gmail.com>
2025-03-16 15:25:55 +0300

    minor fix

--- tasks/seq/morozov_e_lineare_image_filtering_block_gaussian/func_tests/main.cpp
+++ tasks/seq/morozov_e_lineare_image_filtering_block_gaussian/func_tests/main.cpp
@@ -7,8 +7,8 @@
 
 #include "core/task/include/task.hpp"
 #include "seq/morozov_e_lineare_image_filtering_block_gaussian/include/ops_seq.hpp"
-namespace morozov_e_lineare_image_filtering_block_gaussian {
-std::vector<double> static GenerateRandomVector(int n, int m) {
+namespace {
+std::vector<double> GenerateRandomVector(int n, int m) {
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<> distrib(0, 100);
@@ -23,7 +23,7 @@ std::vector<double> static GenerateRandomVector(int n, int m) {
   }
   return vector;
 }
-}  // namespace morozov_e_lineare_image_filtering_block_gaussian
+}  // namespace
 
 TEST(morozov_e_lineare_image_filtering_block_gaussian, empty_image_test) {
   int n = 0;
@@ -294,7 +294,7 @@ TEST(morozov_e_lineare_image_filtering_block_gaussian, random_test1) {
   int n = 3;
   int m = 3;
   std::vector image_res(n * m, 0.0);
-  std::vector<double> image = morozov_e_lineare_image_filtering_block_gaussian::GenerateRandomVector(n, m);
+  std::vector<double> image = GenerateRandomVector(n, m);
   // Create task_data
   auto task_data_seq = std::make_shared<ppc::core::TaskData>();
   task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(image.data()));
