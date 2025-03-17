a7109e495830e3b413be52c0e589f55b55c012f5
andrewmeow <91527374+andrewmeow@users.noreply.github.com>
2025-03-17 13:41:22 +0300

    seq realization

---
+++ tasks/seq/zolotareva_a_SLE_gradient_method/include/ops_seq.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace zolotareva_a_SLE_gradient_method_seq {
+
+class TestTaskSequential : public ppc::core::Task {
+ public:
+  explicit TestTaskSequential(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
+  bool PreProcessingImpl() override;
+  bool ValidationImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+  inline static void ConjugateGradient(const std::vector<double>& A, const std::vector<double>& b,
+                                        std::vector<double>& x, int N);
+  inline static void DotProduct(double& sum, const std::vector<double>& vec1, const std::vector<double>& vec2, int n);
+  inline static void MatrixVectorMult(const std::vector<double>& matrix, const std::vector<double>& vector,
+                                        std::vector<double>& result, int n);
+  inline static bool IsPositiveAndSimm(const double* A, int n);
+
+ private:
+  std::vector<double> A_;
+  std::vector<double> b_;
+  std::vector<double> x_;
+  int n_{0};
+};
+
+}  // namespace zolotareva_a_SLE_gradient_method_seq
---
+++ tasks/seq/zolotareva_a_SLE_gradient_method/perf_tests/main.cpp
@@ -0,0 +1,74 @@
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "core/perf/include/perf.hpp"
+#include "core/task/include/task.hpp"
+#include "seq/zolotareva_a_SLE_gradient_method/include/ops_seq.hpp"
+
+TEST(sequential_zolotareva_a_SLE_gradient_method_seq, test_pipeline_run) {
+  const uint32_t n = 1000;
+  std::vector<double> A(n * n, 0);
+  std::vector<double> b(n, 0);
+  std::vector<double> x(n);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.push_back(reinterpret_cast<uint8_t *>(A.data()));
+  task_data_seq->inputs.push_back(reinterpret_cast<uint8_t *>(b.data()));
+  task_data_seq->inputs_count.push_back(n * n);
+  task_data_seq->inputs_count.push_back(n);
+  task_data_seq->outputs.push_back(reinterpret_cast<uint8_t *>(x.data()));
+  task_data_seq->outputs_count.push_back(x.size());
+
+  auto test_task_sequential = std::make_shared<zolotareva_a_SLE_gradient_method_seq::TestTaskSequential>(task_data_seq);
+
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perf_attr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_sequential);
+  perf_analyzer->PipelineRun(perf_attr, perf_results);
+  ppc::core::Perf::PrintPerfStatistic(perf_results);
+
+  ASSERT_EQ(n, task_data_seq->inputs_count[1]);
+}
+
+TEST(sequential_zolotareva_a_SLE_gradient_method_seq, test_task_run) {
+  const uint32_t n = 1000;
+  std::vector<double> A(n * n, 0);
+  std::vector<double> b(n, 0);
+  std::vector<double> x(n);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.push_back(reinterpret_cast<uint8_t *>(A.data()));
+  task_data_seq->inputs.push_back(reinterpret_cast<uint8_t *>(b.data()));
+  task_data_seq->inputs_count.push_back(n * n);
+  task_data_seq->inputs_count.push_back(n);
+  task_data_seq->outputs.push_back(reinterpret_cast<uint8_t *>(x.data()));
+  task_data_seq->outputs_count.push_back(x.size());
+
+  auto test_task_sequential = std::make_shared<zolotareva_a_SLE_gradient_method_seq::TestTaskSequential>(task_data_seq);
+
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perf_attr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_sequential);
+  perf_analyzer->TaskRun(perf_attr, perf_results);
+  ppc::core::Perf::PrintPerfStatistic(perf_results);
+  ASSERT_EQ(n, task_data_seq->inputs_count[1]);
+}
