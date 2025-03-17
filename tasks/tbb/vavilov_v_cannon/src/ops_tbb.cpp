3af632650631b061aa25fd50add2f99c3e470d51
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2025-03-18 00:02:13 +0300

    Update ops_tbb.cpp

--- tasks/tbb/vavilov_v_cannon/src/ops_tbb.cpp
+++ tasks/tbb/vavilov_v_cannon/src/ops_tbb.cpp
@@ -51,7 +51,7 @@ void vavilov_v_cannon_tbb::CannonTBB::InitialShift() {
 }
 
 void vavilov_v_cannon_tbb::CannonTBB::BlockMultiply() {
-  std::vector<double> local_C_;
+  std::vector<std::vector<double>> local_C_;
   local_C_.resize(num_blocks_, std::vector<double>(N_ * block_size_, 0.0));
   for (auto& vec : local_C_) {
     std::fill(vec.begin(), vec.end(), 0.0);
