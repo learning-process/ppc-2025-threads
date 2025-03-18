f004aa6037c0d98c8fd539f7ed02a6163b5b8855
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2025-03-19 01:22:29 +0300

    Update main.cpp

--- tasks/stl/vavilov_v_cannon/perf_tests/main.cpp
+++ tasks/stl/vavilov_v_cannon/perf_tests/main.cpp
@@ -10,7 +10,7 @@
 #include "stl/vavilov_v_cannon/include/ops_stl.hpp"
 
 TEST(vavilov_v_cannon_stl, test_pipeline_run) {
-  constexpr int kN = 625;
+  constexpr int kN = 900;
   std::vector<double> a(kN * kN, 1.0);
   std::vector<double> b(kN * kN, 1.0);
   std::vector<double> c(kN * kN, 0.0);
@@ -46,7 +46,7 @@ TEST(vavilov_v_cannon_stl, test_pipeline_run) {
 }
 
 TEST(vavilov_v_cannon_stl, test_task_run) {
-  constexpr int kN = 625;
+  constexpr int kN = 900;
   std::vector<double> a(kN * kN, 1.0);
   std::vector<double> b(kN * kN, 1.0);
   std::vector<double> c(kN * kN, 0.0);
