1acab57e13c1ac0e617a4a18e4bea36e27119ae4
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-03-18 21:48:10 +0300

    Update ops_tbb.cpp

--- tasks/tbb/deryabin_m_hoare_sort_simple_merge/src/ops_tbb.cpp
+++ tasks/tbb/deryabin_m_hoare_sort_simple_merge/src/ops_tbb.cpp
@@ -34,13 +34,7 @@ void deryabin_m_hoare_sort_simple_merge_tbb::HoaraSort(std::vector<double>& a, s
     }
   } while (i < j);
   tbb::parallel_invoke(
-  if (i < last) {
-  [&a]() { HoaraSort(a, i + 1, last); },
-  }
-  if (first < j) {
-  [&a]() { HoaraSort(a, first, j); }
-  }
-  );
+      if (i < last) { [&a]() { HoaraSort(a, i + 1, last); }, } if (first < j){[&a]() { HoaraSort(a, first, j); }});
 }
 
 void deryabin_m_hoare_sort_simple_merge_tbb::MergeTwoParts(std::vector<double>& a, size_t left, size_t right,
