a899d9d96b7e7f60625179549b8877799304cb9a
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-03-19 07:41:07 +0300

    Update main.cpp

--- tasks/seq/deryabin_m_hoare_sort_simple_merge/func_tests/main.cpp
+++ tasks/seq/deryabin_m_hoare_sort_simple_merge/func_tests/main.cpp
@@ -192,6 +192,36 @@ TEST(deryabin_m_hoare_sort_simple_merge_seq, test_random_array_large_pieces) {
   ASSERT_EQ(true_solution, out_array[0]);
 }
 
+TEST(deryabin_m_hoare_sort_simple_merge_seq, test_random_array_large_pieces_reversed_order) {
+  // Create data
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::vector<double> input_array(800);
+  std::ranges::generate(input_array.begin(), input_array.end(), [value = 800]() mutable { return value--; });
+  std::vector<std::vector<double>> in_array(1, input_array);
+  size_t chunk_count = 2;
+  std::vector<double> output_array(800);
+  std::vector<std::vector<double>> out_array(1, output_array);
+  std::vector<double> true_solution(input_array);
+  std::ranges::sort(true_solution.begin(), true_solution.end());
+
+  // Create TaskData
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in_array.data()));
+  task_data_seq->inputs_count.emplace_back(input_array.size());
+  task_data_seq->inputs_count.emplace_back(chunk_count);
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out_array.data()));
+  task_data_seq->outputs_count.emplace_back(output_array.size());
+
+  // Create Task
+  deryabin_m_hoare_sort_simple_merge_seq::HoareSortTaskSequential hoare_sort_task_sequential(task_data_seq);
+  ASSERT_EQ(hoare_sort_task_sequential.Validation(), true);
+  hoare_sort_task_sequential.PreProcessing();
+  hoare_sort_task_sequential.Run();
+  hoare_sort_task_sequential.PostProcessing();
+  ASSERT_EQ(true_solution, out_array[0]);
+}
+
 TEST(deryabin_m_hoare_sort_simple_merge_seq, test_partially_sorted_array) {
   // Create data
   std::random_device rd;
