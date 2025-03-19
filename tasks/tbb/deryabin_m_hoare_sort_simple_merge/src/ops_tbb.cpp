ced46dc0140b394aef637b2970975b3cbedd67d4
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-03-20 01:33:52 +0300

    Update ops_tbb.cpp

--- tasks/tbb/deryabin_m_hoare_sort_simple_merge/src/ops_tbb.cpp
+++ tasks/tbb/deryabin_m_hoare_sort_simple_merge/src/ops_tbb.cpp
@@ -122,10 +122,11 @@ bool deryabin_m_hoare_sort_simple_merge_tbb::HoareSortTaskTBB::RunImpl() {
   oneapi::tbb::parallel_for(0, (int)chunk_count, 1, [=, this](int count) {
     HoaraSort(input_array_A_, (short)count * (short)min_chunk_size_, ((count + 1) * (short)min_chunk_size_) - 1);
   });
-  oneapi::tbb::parallel_for(0, (int)(log((double)chunk_count_) / std::numbers::ln2), 1, [=, this](int i) {
-    for (size_t j = 0; j < chunk_count; j++) {
+  auto num_of_lvls = (int)(log((double)chunk_count_) / std::numbers::ln2);
+  oneapi::tbb::parallel_for(0, num_of_lvls, 1, [=, this](int i) {
+    for (size_t j = 0; j < (size_t)chunk_count; j++) {
       MergeTwoParts(input_array_A_, j * min_chunk_size_ << ((size_t)i + 1),
-                    ((j + 1) * min_chunk_size_ << ((size_t)i + 1)) - 1);
+                    ((j + 1) * min_chunk_size_ << ((size_t)i + 1)) - 1, dimension_);
       chunk_count--;
     }
   });
