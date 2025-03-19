8316b3d346f0f6f9e9b9159e8912af18b5599982
IljaSmirnov <ilsmirnov693n@gmail.com>
2025-03-19 12:39:53 +0300

    fixes

--- tasks/seq/smirnov_i_radix_sort_simple_merge/func_tests/main.cpp
+++ tasks/seq/smirnov_i_radix_sort_simple_merge/func_tests/main.cpp
@@ -120,6 +120,34 @@ TEST(smirnov_i_radix_sort_simple_merge_seq, test_256_elem_sorted) {
   test_task_sequential.PostProcessing();
   EXPECT_EQ(exp_out, out);
 }
+TEST(smirnov_i_radix_sort_simple_merge_seq, test_reverse_order) {
+  constexpr size_t kCount = 100;
+
+  // Create data
+  std::vector<int> in(kCount, 0);
+  std::vector<int> exp_out(kCount, 0);
+  std::vector<int> out(kCount);
+  for (size_t i = 0; i < kCount; i++) {
+    in[kCount - 1 - i] = static_cast<int>(i);
+  }
+  for (size_t i = 0; i < kCount; i++) {
+    exp_out[i] = static_cast<int>(i);
+  }
+  // Create task_data
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_seq->inputs_count.emplace_back(in.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_seq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  smirnov_i_radix_sort_simple_merge_seq::TestTaskSequential test_task_sequential(task_data_seq);
+  ASSERT_EQ(test_task_sequential.Validation(), true);
+  test_task_sequential.PreProcessing();
+  test_task_sequential.Run();
+  test_task_sequential.PostProcessing();
+  EXPECT_EQ(exp_out, out);
+}
 TEST(smirnov_i_radix_sort_simple_merge_seq, test_771_elem) {
   constexpr size_t kCount = 771;
 
