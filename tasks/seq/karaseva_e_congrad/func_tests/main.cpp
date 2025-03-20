61a2ff2ec495aa0c78c3305f7f914718cf8fa04e
Katerina Karaseva <k.karaseva2010@mail.ru>
2025-03-20 17:04:48 +0300

    error correction

--- tasks/seq/karaseva_e_congrad/func_tests/main.cpp
+++ tasks/seq/karaseva_e_congrad/func_tests/main.cpp
@@ -48,7 +48,7 @@ std::vector<double> MultiplyMatrixVector(const std::vector<double>& a_matrix, co
   return result;
 }
 
-}  // namespace
+}
 
 TEST(karaseva_e_congrad_seq, test_identity_50) {
   constexpr size_t kN = 50;
@@ -129,5 +129,96 @@ TEST(karaseva_e_congrad_seq, test_random_spd_small) {
   // Check that the computed solution x is close to the true solution x_true with an accuracy of 1e-6
   for (size_t i = 0; i < kN; ++i) {
     EXPECT_NEAR(x[i], x_true[i], 1e-6);
+  }
+}
+
+TEST(karaseva_e_congrad_seq, test_small_system_size_1) {
+  constexpr size_t kN = 1;
+
+  std::vector<double> a_matrix = {5.0};
+  std::vector<double> b = {10.0};
+  std::vector<double> x(kN, 0.0);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.push_back(reinterpret_cast<uint8_t*>(a_matrix.data()));
+  task_data_seq->inputs_count.push_back(kN * kN);
+  task_data_seq->inputs.push_back(reinterpret_cast<uint8_t*>(b.data()));
+  task_data_seq->inputs_count.push_back(kN);
+  task_data_seq->outputs.push_back(reinterpret_cast<uint8_t*>(x.data()));
+  task_data_seq->outputs_count.push_back(kN);
+
+  karaseva_e_congrad_seq::TestTaskSequential test_task(task_data_seq);
+  ASSERT_TRUE(test_task.Validation());
+  test_task.PreProcessing();
+  test_task.Run();
+  test_task.PostProcessing();
+
+  EXPECT_NEAR(x[0], 2.0, 1e-10);
+}
+
+TEST(karaseva_e_congrad_seq, test_validation_invalid_matrix) {
+  constexpr size_t kRows = 2, kCols = 3;
+  std::vector<double> a_matrix(kRows * kCols, 1.0);
+  std::vector<double> b(kRows, 1.0);
+  std::vector<double> x(kRows, 0.0);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.push_back(reinterpret_cast<uint8_t*>(a_matrix.data()));
+  task_data_seq->inputs_count.push_back(kRows * kCols);
+  task_data_seq->inputs.push_back(reinterpret_cast<uint8_t*>(b.data()));
+  task_data_seq->inputs_count.push_back(kRows);
+  task_data_seq->outputs.push_back(reinterpret_cast<uint8_t*>(x.data()));
+  task_data_seq->outputs_count.push_back(kRows);
+
+  karaseva_e_congrad_seq::TestTaskSequential test_task(task_data_seq);
+  ASSERT_FALSE(test_task.Validation());
+}
+
+TEST(karaseva_e_congrad_seq, test_validation_invalid_output) {
+  constexpr size_t kN = 2;
+  std::vector<double> a_matrix(kN * kN, 1.0);
+  std::vector<double> b(kN, 1.0);
+  std::vector<double> x(3, 0.0);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.push_back(reinterpret_cast<uint8_t*>(a_matrix.data()));
+  task_data_seq->inputs_count.push_back(kN * kN);
+  task_data_seq->inputs.push_back(reinterpret_cast<uint8_t*>(b.data()));
+  task_data_seq->inputs_count.push_back(kN);
+  task_data_seq->outputs.push_back(reinterpret_cast<uint8_t*>(x.data()));
+  task_data_seq->outputs_count.push_back(3);
+
+  karaseva_e_congrad_seq::TestTaskSequential test_task(task_data_seq);
+  ASSERT_FALSE(test_task.Validation());
+}
+
+TEST(karaseva_e_congrad_seq, test_diagonal_matrix_100) {
+  constexpr size_t kN = 100;
+  std::vector<double> a_matrix(kN * kN, 0.0);
+  for (size_t i = 0; i < kN; ++i) {
+    a_matrix[i * kN + i] = static_cast<double>(i + 1);
+  }
+  std::vector<double> b(kN);
+  for (size_t i = 0; i < kN; ++i) {
+    b[i] = static_cast<double>(i + 1);
+  }
+  std::vector<double> x(kN, 0.0);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.push_back(reinterpret_cast<uint8_t*>(a_matrix.data()));
+  task_data_seq->inputs_count.push_back(kN * kN);
+  task_data_seq->inputs.push_back(reinterpret_cast<uint8_t*>(b.data()));
+  task_data_seq->inputs_count.push_back(kN);
+  task_data_seq->outputs.push_back(reinterpret_cast<uint8_t*>(x.data()));
+  task_data_seq->outputs_count.push_back(kN);
+
+  karaseva_e_congrad_seq::TestTaskSequential test_task(task_data_seq);
+  ASSERT_TRUE(test_task.Validation());
+  test_task.PreProcessing();
+  test_task.Run();
+  test_task.PostProcessing();
+
+  for (size_t i = 0; i < kN; ++i) {
+    EXPECT_NEAR(x[i], 1.0, 1e-9);
   }
 }
