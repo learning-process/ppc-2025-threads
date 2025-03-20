71c5cc4fdd7cb9a451751268e7582eb38789841c
MaximChizhov <maxim070205@gmail.com>
2025-03-20 16:44:39 +0300

    combine test

--- tasks/seq/chizhov_m_trapezoid_method/func_tests/main.cpp
+++ tasks/seq/chizhov_m_trapezoid_method/func_tests/main.cpp
@@ -289,6 +289,41 @@ TEST(chizhov_m_trapezoid_method_seq, exp_two_variables) {
   delete f_object;
 }
 
+TEST(chizhov_m_trapezoid_method_seq, combine_exp_sin_cos) {
+  int div = 90;
+  int dim = 2;
+  std::vector<double> limits = {0.0, 3.0, 0.0, 3.0};
+
+  std::vector<double> res(1, 0);
+  auto f = [](const std::vector<double> &f_val) { return std::exp(-f_val[0]) * std::sin(f_val[0]) * std::cos(f_val[1]); };
+  auto *f_object = new std::function<double(const std::vector<double> &)>(f);
+
+  std::shared_ptr<ppc::core::TaskData> task_data_seq = std::make_shared<ppc::core::TaskData>();
+
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&div));
+  task_data_seq->inputs_count.emplace_back(sizeof(div));
+
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&dim));
+  task_data_seq->inputs_count.emplace_back(sizeof(dim));
+
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(limits.data()));
+  task_data_seq->inputs_count.emplace_back(limits.size());
+
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(f_object));
+
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(res.data()));
+  task_data_seq->outputs_count.emplace_back(res.size() * sizeof(double));
+
+  chizhov_m_trapezoid_method_seq::TestTaskSequential test_task_sequential(task_data_seq);
+
+  ASSERT_TRUE(test_task_sequential.ValidationImpl());
+  test_task_sequential.PreProcessingImpl();
+  test_task_sequential.RunImpl();
+  test_task_sequential.PostProcessingImpl();
+  ASSERT_NEAR(res[0], 0.073, 0.1);
+  delete f_object;
+}
+
 TEST(chizhov_m_trapezoid_method_seq, invalid_value_dim) {
   int div = 10;
   int dim = -2;
