30f2ca10eef69785c1247f5b3698a00749156c05
Александра Вершинина <xx.vershinina.xx@yandex.ru>
2025-03-20 16:54:32 +0300

    com reverse order

--- tasks/seq/vershinina_a_hoare_sort_seq/func_tests/main.cpp
+++ tasks/seq/vershinina_a_hoare_sort_seq/func_tests/main.cpp
@@ -41,6 +41,24 @@ TEST(vershinina_a_hoare_sort_seq, Test_empty) {
   ASSERT_TRUE(std::ranges::is_sorted(out));
 }
 
+TEST(vershinina_a_hoare_sort_seq, Test_not_random_reverse_order) {
+  std::vector<int> in{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+  std::vector<int> out(10);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_seq->inputs_count.emplace_back(in.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_seq->outputs_count.emplace_back(out.size());
+
+  vershinina_a_hoare_sort_seq::TestTaskSequential test_task_sequential(task_data_seq);
+  ASSERT_EQ(test_task_sequential.Validation(), true);
+  test_task_sequential.PreProcessing();
+  test_task_sequential.Run();
+  test_task_sequential.PostProcessing();
+  ASSERT_TRUE(std::ranges::is_sorted(out));
+}
+
 TEST(vershinina_a_hoare_sort_seq, Test_not_random_len_10) {
   std::vector<int> in{56, 39, 11, 98, 5, 73, 40, 51, 83, 22};
   std::vector<int> out(10);
