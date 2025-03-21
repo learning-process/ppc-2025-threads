52ae2fcedd20db2a51c9e91d3d0653a6ad38c797
LizochkaSpring <saysolovey@gmail.com>
2025-03-21 23:06:05 +0300

    fix format

--- tasks/tbb/filateva_e_simpson/src/ops_tbb.cpp
+++ tasks/tbb/filateva_e_simpson/src/ops_tbb.cpp
@@ -5,10 +5,9 @@
 #include <tbb/tbb.h>
 
 #include <cmath>
+#include <core/util/include/util.hpp>
 #include <cstddef>
 #include <vector>
-
-#include <core/util/include/util.hpp>
 
 bool filateva_e_simpson_tbb::Simpson::PreProcessingImpl() {
   mer_ = task_data->inputs_count[0];
@@ -54,40 +53,32 @@ bool filateva_e_simpson_tbb::Simpson::RunImpl() {
 
   oneapi::tbb::task_arena arena(num_threads);
 
-  local_res = arena.execute([&]{
+  local_res = arena.execute([&] {
     return res_ = oneapi::tbb::parallel_reduce(
-      tbb::blocked_range<long>(0, total_steps), 0.0,
-      [&](const tbb::blocked_range<long>& r, double local_res) {
-        for (long i = r.begin(); i != r.end(); ++i) {
-          unsigned long temp = i;
-          std::vector<double> param(mer_);
-          double weight = 1.0;
-
-          for (size_t m = 0; m < mer_; m++) {
-            size_t shag_i = temp % (steps_ + 1);
-            temp /= (steps_ + 1);
-
-            param[m] = a_[m] + h[m] * static_cast<double>(shag_i);
-
-            if (shag_i == 0 || shag_i == steps_) {
-              weight *= 1.0;
-            } else if (shag_i % 2 == 1) {
-              weight *= 4.0;
-            } else {
-              weight *= 2.0;
-            }
-          }
-
-          local_res += weight * f_(param);
-        }
-        return local_res;
-      },
-      [](double a, double b) {
-        return a + b;
-      }
-    );
+               tbb::blocked_range<long>(0, total_steps), 0.0,
+               [&](const tbb::blocked_range<long> &r, double local_res) {
+                 for (long i = r.begin(); i != r.end(); ++i) {
+                   unsigned long temp = i;
+                   std::vector<double> param(mer_);
+                   double weight = 1.0;
+                   for (size_t m = 0; m < mer_; m++) {
+                     size_t shag_i = temp % (steps_ + 1);
+                     temp /= (steps_ + 1);
+                     param[m] = a_[m] + h[m] * static_cast<double>(shag_i);
+                     if (shag_i == 0 || shag_i == steps_) {
+                       weight *= 1.0;
+                     } else if (shag_i % 2 == 1) {
+                       weight *= 4.0;
+                     } else {
+                       weight *= 2.0;
+                     }
+                   }
+                   local_res += weight * f_(param);
+                 }
+                 return local_res;
+               },
+               [](double a, double b) { return a + b; });
   });
-
 
   for (size_t i = 0; i < mer_; i++) {
     res_ *= (h[i] / 3.0);
