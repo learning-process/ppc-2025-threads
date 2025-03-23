719eadba10aedabc99fa67c3e5a005fe0d672a81
Matthewsoul124 <132999776+Matthewsoul124@users.noreply.github.com>
2025-03-23 12:42:43 +0300

    fix5

--- tasks/seq/fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq/func_tests/main.cpp
+++ tasks/seq/fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq/func_tests/main.cpp
@@ -52,33 +52,12 @@ TEST(fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq, test_small_array_wit
   EXPECT_EQ(output, expected_output);
 }
 
-TEST(fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq, test_empty_array) {
-  std::vector<int> input = {};
-  std::vector<int> expected_output = {};
-
-  std::vector<int> output(input.size(), 0);
-
-  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
-  task_data_seq->inputs_count.emplace_back(input.size());
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
-  task_data_seq->outputs_count.emplace_back(output.size());
-
-  fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq::TestTaskSequential test_task_sequential(task_data_seq);
-  ASSERT_EQ(test_task_sequential.Validation(), true);
-  test_task_sequential.PreProcessing();
-  test_task_sequential.Run();
-  test_task_sequential.PostProcessing();
-
-  EXPECT_EQ(output, expected_output);
-}
-
 TEST(fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq, test_random_sequence) {
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<> distrib(-1000, 1000);
 
-  const size_t size = 1000;
+  const size_t size = 100;
   std::vector<int> input(size);
   for (auto &num : input) {
     num = distrib(gen);
