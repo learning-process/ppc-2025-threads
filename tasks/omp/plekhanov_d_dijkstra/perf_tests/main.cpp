742721336a32ac6bfee6f22acf89384e5da79392
Danie <dxppi@vk.com>
2025-03-23 17:15:13 +0300

    new

--- tasks/omp/plekhanov_d_dijkstra/perf_tests/main.cpp
+++ tasks/omp/plekhanov_d_dijkstra/perf_tests/main.cpp
@@ -53,7 +53,7 @@ static std::vector<int> CalculateExpectedResult(                       // NOLINT
 }  // namespace plekhanov_d_dijkstra_omp
 
 TEST(plekhanov_d_dijkstra_omp, test_pipeline_run) {
-  constexpr size_t kNumVertices = 2000;
+  constexpr size_t kNumVertices = 1000;
   size_t start_vertex = 0;
 
   std::vector<std::vector<std::pair<size_t, int>>> adj_list(kNumVertices);
@@ -108,7 +108,7 @@ TEST(plekhanov_d_dijkstra_omp, test_pipeline_run) {
 }
 
 TEST(plekhanov_d_dijkstra_omp, test_task_run) {
-  constexpr size_t kNumVertices = 2000;
+  constexpr size_t kNumVertices = 1000;
   size_t start_vertex = 0;
 
   std::vector<std::vector<std::pair<size_t, int>>> adj_list(kNumVertices);
