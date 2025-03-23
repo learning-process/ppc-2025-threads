988ccbaa39171558801b3edce33e8f21d178084d
MikasaAkc <daria.komshina@mail.ru>
2025-03-23 11:22:27 +0300

    tasks

--- tasks/seq/komshina_d_image_filtering_vertical_gaussian/src/ops_seq.cpp
+++ tasks/seq/komshina_d_image_filtering_vertical_gaussian/src/ops_seq.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 #include <cmath>
 #include <cstddef>
-#include <cstdint>
 #include <vector>
 
 bool komshina_d_image_filtering_vertical_gaussian_seq::TestTaskSequential::PreProcessingImpl() {
