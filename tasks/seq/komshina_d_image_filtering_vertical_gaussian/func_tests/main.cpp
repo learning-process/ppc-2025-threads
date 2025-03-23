e02211e2572f7d931a12762df47998a85449963c
MikasaAkc <daria.komshina@mail.ru>
2025-03-23 17:20:02 +0300

    tasks

--- tasks/seq/komshina_d_image_filtering_vertical_gaussian/func_tests/main.cpp
+++ tasks/seq/komshina_d_image_filtering_vertical_gaussian/func_tests/main.cpp
@@ -231,3 +231,40 @@ TEST(komshina_d_image_filtering_vertical_gaussian_seq, RandomImage) {
     EXPECT_LE(out[i], 255);
   }
 }
+
+TEST(komshina_d_image_filtering_vertical_gaussian_seq, RandomImage2) {
+  std::size_t width = 17;
+  std::size_t height = 23;
+  std::vector<unsigned char> in(width * height * 3);
+  std::vector<float> kernel = {1, 2, 1, 2, 4, 2, 1, 2, 1};
+  std::vector<unsigned char> out(in.size());
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, 255);
+
+  for (size_t i = 0; i < in.size(); ++i) {
+    in[i] = dis(gen);
+  }
+
+  std::shared_ptr<ppc::core::TaskData> task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs.emplace_back(in.data());
+  task_data->inputs_count.emplace_back(width);
+  task_data->inputs_count.emplace_back(height);
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(kernel.data()));
+  task_data->inputs_count.emplace_back(kernel.size());
+  task_data->outputs.emplace_back(out.data());
+  task_data->outputs_count.emplace_back(out.size());
+
+  komshina_d_image_filtering_vertical_gaussian_seq::TestTaskSequential test_task(task_data);
+
+  ASSERT_EQ(test_task.Validation(), true);
+  test_task.PreProcessing();
+  test_task.Run();
+  test_task.PostProcessing();
+
+  for (size_t i = 0; i < out.size(); ++i) {
+    EXPECT_GE(out[i], 0);
+    EXPECT_LE(out[i], 255);
+  }
+}
