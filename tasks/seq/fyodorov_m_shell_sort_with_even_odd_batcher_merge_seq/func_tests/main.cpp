60a9fb07b68cb3c5f24c8e44f3e34ffc18f70270
Matthewsoul124 <132999776+Matthewsoul124@users.noreply.github.com>
2025-03-23 10:39:58 +0300

    fix4

--- tasks/seq/fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq/func_tests/main.cpp
+++ tasks/seq/fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq/func_tests/main.cpp
@@ -95,10 +95,13 @@ TEST(fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq, test_random_sequence
   task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
   task_data_seq->outputs_count.emplace_back(output.size());
 
-  auto test_task_sequential =
-      std::make_shared<fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq::TestTaskSequential>(task_data_seq);
+  fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq::TestTaskSequential test_task_sequential(task_data_seq);
 
-  test_task_sequential->Run();
+  ASSERT_EQ(test_task_sequential.Validation(), true);
 
-  ASSERT_EQ(output, expected_output);
+  test_task_sequential.PreProcessing();
+  test_task_sequential.Run();
+  test_task_sequential.PostProcessing();
+
+  EXPECT_EQ(output, expected_output);
 }
