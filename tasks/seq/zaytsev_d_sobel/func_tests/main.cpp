78fedf4b26cd01e181049861e7dfe82fe26277a8
DZaytsev4 <deniszaytsev79524555@gmail.com>
2025-03-24 18:10:34 +0300

    add_test

--- tasks/seq/zaytsev_d_sobel/func_tests/main.cpp
+++ tasks/seq/zaytsev_d_sobel/func_tests/main.cpp
@@ -78,6 +78,40 @@ TEST(zaytsev_d_sobel_seq, SobelEdgeDetection_5x5) {
   EXPECT_EQ(output, expected_output);
 }
 
+TEST(zaytsev_d_sobel_seq, Sobel_12x12) {
+  constexpr size_t kSize = 12;
+  std::vector<int> input = {0,   0,   0,   0,   0,   0,   50,  50,  50,  0,   0,   0,   0,   50,  100, 150, 200, 255,
+                            255, 200, 150, 100, 50,  0,   0,   100, 150, 200, 255, 255, 255, 255, 200, 150, 100, 0,
+                            0,   150, 200, 255, 255, 255, 255, 255, 255, 200, 150, 0,   0,   200, 255, 255, 255, 255,
+                            255, 255, 255, 255, 200, 0,   0,   255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0,
+                            50,  255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 50,  100, 200, 255, 255, 255, 255,
+                            255, 255, 255, 255, 200, 100, 150, 150, 200, 255, 255, 255, 255, 255, 255, 200, 150, 150,
+                            200, 100, 150, 200, 255, 255, 255, 200, 150, 100, 150, 200, 255, 50,  100, 150, 200, 255,
+                            200, 150, 100, 50,  50,  255, 0,   0,   0,   50,  100, 150, 150, 100, 50,  0,   0,   0};
+  std::vector<int> expected_output = {
+      0, 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0,
+      0, 255, 255, 255, 255, 77,  77,  255, 255, 255, 255, 0, 0, 255, 255, 255, 77,  0,   0,   77,  255, 255, 255, 0,
+      0, 255, 255, 77,  0,   0,   0,   0,   77,  255, 255, 0, 0, 255, 77,  0,   0,   0,   0,   0,   0,   77,  255, 0,
+      0, 255, 77,  0,   0,   0,   0,   0,   0,   77,  255, 0, 0, 255, 255, 77,  0,   0,   0,   0,   77,  255, 255, 0,
+      0, 255, 255, 255, 77,  0,   77,  239, 255, 255, 219, 0, 0, 255, 255, 255, 255, 110, 255, 255, 255, 255, 255, 0,
+      0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0, 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0};
+  std::vector<int> output(kSize * kSize, 0);
+  
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs.push_back(reinterpret_cast<uint8_t *>(input.data()));
+  task_data->inputs_count.push_back(input.size());
+  task_data->outputs.push_back(reinterpret_cast<uint8_t *>(output.data()));
+  task_data->outputs_count.push_back(output.size());
+
+  zaytsev_d_sobel_seq::TestTaskSequential sobel_task(task_data);
+  ASSERT_TRUE(sobel_task.Validation());
+  sobel_task.PreProcessing();
+  sobel_task.Run();
+  sobel_task.PostProcessing();
+
+  EXPECT_EQ(output, expected_output);
+}
+
 TEST(zaytsev_d_sobel_seq, SobelEdgeDetection_UniformImage) {
   constexpr size_t kSize = 5;
   std::vector<int> input(kSize * kSize, 10);
