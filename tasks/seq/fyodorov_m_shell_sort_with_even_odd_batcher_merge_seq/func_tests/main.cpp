f5c70f9e4866d05a26cec73d267f57ad52115e60
Matthewsoul124 <132999776+Matthewsoul124@users.noreply.github.com>
2025-03-24 12:02:48 +0300

    fix6

--- tasks/seq/fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq/func_tests/main.cpp
+++ tasks/seq/fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq/func_tests/main.cpp
@@ -84,3 +84,24 @@ TEST(fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq, test_random_sequence
 
   EXPECT_EQ(output, expected_output);
 }
+
+TEST(fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq, test_sorted_array) {
+  std::vector<int> input = {1, 2, 33, 4, 5, 6, 7, 8};
+  std::vector<int> expected_output = {1, 2, 3, 4, 5, 6, 7, 8};
+
+  std::vector<int> output(input.size(), 0);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+  task_data_seq->inputs_count.emplace_back(input.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
+  task_data_seq->outputs_count.emplace_back(output.size());
+
+  fyodorov_m_shell_sort_with_even_odd_batcher_merge_seq::TestTaskSequential test_task_sequential(task_data_seq);
+  ASSERT_EQ(test_task_sequential.Validation(), true);
+  test_task_sequential.PreProcessing();
+  test_task_sequential.Run();
+  test_task_sequential.PostProcessing();
+
+  EXPECT_EQ(output, expected_output);
+}
