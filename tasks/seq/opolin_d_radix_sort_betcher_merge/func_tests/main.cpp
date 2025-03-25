a27d3259c97852c90d1128abd099a6442259bd4a
Dmitry Alekseevich <opolindmitry@gmail.com>
2025-03-25 21:21:08 +0300

    added tests

--- tasks/seq/opolin_d_radix_sort_betcher_merge/func_tests/main.cpp
+++ tasks/seq/opolin_d_radix_sort_betcher_merge/func_tests/main.cpp
@@ -179,6 +179,50 @@ TEST(opolin_d_radix_betcher_sort_seq, test_equal_values) {
   ASSERT_EQ(out, expected);
 }
 
+TEST(opolin_d_radix_betcher_sort_seq, test_reversed) {
+  int size = 10;
+  std::vector<int> expected;
+  std::vector<int> input;
+  input = {12, 8, 6, 3, 2, 0, -4, -6, -7, -10};
+  expected = {-10, -7, -6, -4, 0, 2, 3, 6, 8, 12};
+  std::vector<int> out(size, 0);
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+  task_data_seq->inputs_count.emplace_back(out.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_seq->outputs_count.emplace_back(out.size());
+
+  auto test_task_sequential =
+      std::make_shared<opolin_d_radix_betcher_sort_seq::RadixBetcherSortTaskSequential>(task_data_seq);
+  ASSERT_EQ(test_task_sequential->Validation(), true);
+  test_task_sequential->PreProcessing();
+  test_task_sequential->Run();
+  test_task_sequential->PostProcessing();
+  ASSERT_EQ(out, expected);
+}
+
+TEST(opolin_d_radix_betcher_sort_seq, test_varying_digit_counts) {
+  int size = 6;
+  std::vector<int> expected;
+  std::vector<int> input;
+  input = {123456, 12, 123, 1, 12345, 1234};
+  expected = {1, 12, 123, 1234, 123456};
+  std::vector<int> out(size, 0);
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+  task_data_seq->inputs_count.emplace_back(out.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_seq->outputs_count.emplace_back(out.size());
+
+  auto test_task_sequential =
+      std::make_shared<opolin_d_radix_betcher_sort_seq::RadixBetcherSortTaskSequential>(task_data_seq);
+  ASSERT_EQ(test_task_sequential->Validation(), true);
+  test_task_sequential->PreProcessing();
+  test_task_sequential->Run();
+  test_task_sequential->PostProcessing();
+  ASSERT_EQ(out, expected);
+}
+
 TEST(opolin_d_radix_betcher_sort_seq, test_negative_size) {
   int size = -1;
   std::vector<int> expected;
