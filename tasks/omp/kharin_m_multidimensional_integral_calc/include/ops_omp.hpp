6e0d5cf7d8faa618c04f6b731167da2ea559f298
Kharin Matvey <harin.matveix@ya.ru>
2025-03-26 20:42:37 +0300

    .

--- tasks/omp/kharin_m_multidimensional_integral_calc/include/ops_omp.hpp
+++ tasks/omp/kharin_m_multidimensional_integral_calc/include/ops_omp.hpp
@@ -19,7 +19,7 @@ class TaskSequential : public ppc::core::Task {
   bool PostProcessingImpl() override;
 
  private:
-  std::vector<double> input_;       // Значения функции на сетке
+  std::vector<double> input_;       // Значения функции на сеткe
   std::vector<size_t> grid_sizes_;  // Размеры сетки в каждом измерении
   std::vector<double> step_sizes_;  // Шаги интегрирования в каждом измерении
   double output_result_{0.0};       // Результат вычисления интеграла
