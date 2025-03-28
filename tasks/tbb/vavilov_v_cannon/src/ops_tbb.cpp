fb18e89d92c68f48bbd9ee5fbeeff2efeddac704
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2025-03-28 22:41:48 +0300

    Update ops_tbb.cpp

--- tasks/tbb/vavilov_v_cannon/src/ops_tbb.cpp
+++ tasks/tbb/vavilov_v_cannon/src/ops_tbb.cpp
@@ -50,33 +50,31 @@ void vavilov_v_cannon_tbb::CannonTBB::InitialShift() {
 }
 
 void vavilov_v_cannon_tbb::CannonTBB::BlockMultiply() {
-  std::vector<std::vector<double>> thread_local_C(num_threads, std::vector<double>(N_ * N_, 0.0));
+  tbb::concurrent_vector<double> local_C(N_ * N_, 0.0);
   tbb::parallel_for(tbb::blocked_range2d<int>(0, N_, block_size_, 0, N_, block_size_),
                     [&](const tbb::blocked_range2d<int>& r) {
-                      int thread_idx = tbb::this_task_arena::current_thread_index();
-                      auto& local_C = thread_local_C[thread_idx];
-                      for (int bi = r.rows().begin(); bi < r.rows().end(); bi += block_size_) {
-                        for (int bj = r.cols().begin(); bj < r.cols().end(); bj += block_size_) {
-                          for (int i = 0; i < block_size_ && (bi + i) < N_; ++i) {
-                            for (int j = 0; j < block_size_ && (bj + j) < N_; ++j) {
+                      for (int bi = r.rows().begin(); bi != r.rows().end(); bi += block_size_) {
+                        for (int bj = r.cols().begin(); bj != r.cols().end(); bj += block_size_) {
+                          for (int i = bi; i < bi + block_size_; i++) {
+                            for (int j = bj; j < bj + block_size_; j++) {
                               double temp = 0.0;
-                              for (int k = 0; k < block_size_ && (bi + k) < N_; ++k) {
-                                temp += A_[(bi + i) * N_ + (bj + k)] * B_[(bi + k) * N_ + (bj + j)];
+                              for (int k = 0; k < block_size_; k++) {
+                                int row_a = bi + (i - bi);
+                                int col_a = bj + k;
+                                int row_b = bi + k;
+                                int col_b = bj + (j - bj);
+                                temp += A_[(row_a * N_) + col_a] * B_[(row_b * N_) + col_b];
                               }
-                              local_C[(bi + i) * N_ + (bj + j)] += temp;
+                              local_C[i * N_ + j] += temp;
                             }
                           }
                         }
                       }
                     });
-  // Суммирование результатов от всех потоков
+
   tbb::parallel_for(tbb::blocked_range<int>(0, N_ * N_), [&](const tbb::blocked_range<int>& r) {
     for (int idx = r.begin(); idx != r.end(); ++idx) {
-      double sum = 0.0;
-      for (const auto& local : thread_local_C) {
-        sum += local[idx];
-      }
-      C_[idx] += sum;
+      C_[idx] += local_C[idx];
     }
   });
 }
