e4d6a30ab8d6b7a4906a40c31274068343728480
ARBUZNIJ <kovkonst@gmail.com>
2025-03-29 22:44:39 +0300

    + bugs fix

--- tasks/omp/kovalev_k_radix_sort_batcher_merge/src/sourse.cpp
+++ tasks/omp/kovalev_k_radix_sort_batcher_merge/src/sourse.cpp
@@ -138,7 +138,7 @@ bool kovalev_k_radix_sort_batcher_merge_omp::TestTaskOpenMP::PreProcessingImpl()
 
   effective_num_threads_ = static_cast<int>(std::pow(2, std::floor(std::log2(omp_get_max_threads()))));
   auto e_n_f = static_cast<unsigned int>(effective_num_threads_);
-  n_ = n_input_ + (((2 * e_n_f) - n_ % (2 * e_n_f))) % (2 * e_n_f);
+  n_ = n_input_ + (((2 * e_n_f) - n_input_ % (2 * e_n_f))) % (2 * e_n_f);
   loc_lenght_ = n_ / effective_num_threads_;
 
   mas_ = new long long int[n_];
