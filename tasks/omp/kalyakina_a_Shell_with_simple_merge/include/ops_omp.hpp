c4105f147e6627e10dc7c944f8c966f32d4c01b7
kalyakinaa <nastyakalyakinann@gmail.com>
2025-03-31 17:37:46 +0300

    Adding output vector

--- tasks/omp/kalyakina_a_Shell_with_simple_merge/include/ops_omp.hpp
+++ tasks/omp/kalyakina_a_Shell_with_simple_merge/include/ops_omp.hpp
@@ -9,8 +9,8 @@ namespace kalyakina_a_shell_with_simple_merge_omp {
 
 class ShellSortOpenMP : public ppc::core::Task {
   static std::vector<unsigned int> CalculationOfGapLengths(const unsigned int& size);
-  void ShellSort(std::vector<int>& vec, unsigned int& left, unsigned int& right);
-  static void SimpleMergeSort(std::vector<int>& vec, unsigned int& left, unsigned int& middle, unsigned int& right);
+  void ShellSort(unsigned int& left, unsigned int& right);
+  void SimpleMergeSort(unsigned int& left, unsigned int& middle, unsigned int& right);
 
  public:
   explicit ShellSortOpenMP(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
@@ -21,6 +21,7 @@ class ShellSortOpenMP : public ppc::core::Task {
 
  private:
   std::vector<int> input_;
+  std::vector<int> output_;
   std::vector<unsigned int> Sedgwick_sequence_;
 };
 
--- tasks/omp/kalyakina_a_Shell_with_simple_merge/src/ops_omp.cpp
+++ tasks/omp/kalyakina_a_Shell_with_simple_merge/src/ops_omp.cpp
@@ -23,44 +23,42 @@ std::vector<unsigned int> kalyakina_a_shell_with_simple_merge_omp::ShellSortOpen
   return result;
 }
 
-void kalyakina_a_shell_with_simple_merge_omp::ShellSortOpenMP::ShellSort(std::vector<int> &vec, unsigned int &left,
-                                                                         unsigned int &right) {
+void kalyakina_a_shell_with_simple_merge_omp::ShellSortOpenMP::ShellSort(unsigned int &left, unsigned int &right) {
   for (unsigned int k = Sedgwick_sequence_.size(); k > 0;) {
     unsigned int gap = Sedgwick_sequence_[--k];
     for (unsigned int i = left; i < left + gap; i++) {
       for (unsigned int j = i; j < right; j += gap) {
         unsigned int index = j;
-        int tmp = vec[index];
-        while ((index >= i + gap) && (tmp < vec[index - gap])) {
-          vec[index] = vec[index - gap];
+        int tmp = output_[index];
+        while ((index >= i + gap) && (tmp < output_[index - gap])) {
+          output_[index] = output_[index - gap];
           index -= gap;
         }
-        vec[index] = tmp;
+        output_[index] = tmp;
       }
     }
   }
 }
 
-void kalyakina_a_shell_with_simple_merge_omp::ShellSortOpenMP::SimpleMergeSort(std::vector<int> &vec,
-                                                                               unsigned int &left, unsigned int &middle,
+void kalyakina_a_shell_with_simple_merge_omp::ShellSortOpenMP::SimpleMergeSort(unsigned int &left, unsigned int &middle,
                                                                                unsigned int &right) {
   std::vector<int> first_part(middle - left);
-  std::copy(vec.begin() + left, vec.begin() + middle, first_part.begin());
+  std::copy(output_.begin() + left, output_.begin() + middle, first_part.begin());
   unsigned int l = 0;
   unsigned int r = middle;
   unsigned int j = left;
   for (; (l < first_part.size()) && (r < right); j++) {
-    if (first_part[l] < vec[r]) {
-      vec[j] = first_part[l++];
+    if (first_part[l] < output_[r]) {
+      output_[j] = first_part[l++];
     } else {
-      vec[j] = vec[r++];
+      output_[j] = output_[r++];
     }
   }
   while (l < first_part.size()) {
-    vec[j++] = first_part[l++];
+    output_[j++] = first_part[l++];
   }
   while (r < right) {
-    vec[j++] = vec[r++];
+    output_[j++] = output_[r++];
   }
 }
 
@@ -68,6 +66,9 @@ bool kalyakina_a_shell_with_simple_merge_omp::ShellSortOpenMP::PreProcessingImpl
   input_ = std::vector<int>(task_data->inputs_count[0]);
   auto *in_ptr = reinterpret_cast<int *>(task_data->inputs[0]);
   std::copy(in_ptr, in_ptr + task_data->inputs_count[0], input_.begin());
+
+  output_ = std::vector<int>(task_data->inputs_count[0]);
+  std::ranges::copy(input_, output_.begin());
 
   return true;
 }
@@ -80,11 +81,11 @@ bool kalyakina_a_shell_with_simple_merge_omp::ShellSortOpenMP::ValidationImpl() 
 bool kalyakina_a_shell_with_simple_merge_omp::ShellSortOpenMP::RunImpl() {
   std::vector<std::pair<unsigned int, unsigned int>> bounds;
   unsigned int num =
-      ((unsigned int)omp_get_max_threads() > input_.size()) ? input_.size() : (unsigned int)omp_get_max_threads();
-  unsigned int part = input_.size() / num;
-  unsigned int reminder = input_.size() % num;
+      ((unsigned int)omp_get_max_threads() > output_.size()) ? output_.size() : (unsigned int)omp_get_max_threads();
+  unsigned int part = output_.size() / num;
+  unsigned int reminder = output_.size() % num;
   unsigned int left = 0;
-  Sedgwick_sequence_ = CalculationOfGapLengths(input_.size() / num);
+  Sedgwick_sequence_ = CalculationOfGapLengths(output_.size() / num);
   for (unsigned int i = 0; i < num; i++) {
     unsigned int right = (i < reminder) ? left + part + 1 : left + part;
     bounds.emplace_back(left, right);
@@ -92,7 +93,7 @@ bool kalyakina_a_shell_with_simple_merge_omp::ShellSortOpenMP::RunImpl() {
   }
 #pragma omp parallel for schedule(static)
   for (int i = 0; i < (int)num; i++) {
-    ShellSort(input_, bounds[i].first, bounds[i].second);
+    ShellSort(bounds[i].first, bounds[i].second);
   }
   num = std::ceil((double)num / 2);
   unsigned int step = 1;
@@ -103,7 +104,7 @@ bool kalyakina_a_shell_with_simple_merge_omp::ShellSortOpenMP::RunImpl() {
       unsigned int middle = (step / 2) + (step * i);
       if (middle < bounds.size()) {
         SimpleMergeSort(
-            input_, bounds[i * step].first, bounds[middle].first,
+            bounds[i * step].first, bounds[middle].first,
             bounds[(bounds.size() - 1 < (i + 1) * step - 1) ? bounds.size() - 1 : ((i + 1) * step) - 1].second);
       }
     }
@@ -114,7 +115,7 @@ bool kalyakina_a_shell_with_simple_merge_omp::ShellSortOpenMP::RunImpl() {
 
 bool kalyakina_a_shell_with_simple_merge_omp::ShellSortOpenMP::PostProcessingImpl() {
   for (size_t i = 0; i < input_.size(); i++) {
-    reinterpret_cast<int *>(task_data->outputs[0])[i] = input_[i];
+    reinterpret_cast<int *>(task_data->outputs[0])[i] = output_[i];
   }
   return true;
 }
