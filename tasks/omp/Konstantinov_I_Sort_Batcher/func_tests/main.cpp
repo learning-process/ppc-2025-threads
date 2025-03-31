cc16cb3f5d5244f07e034e02838b449d189242e4
илья константинов <ilyukon2020@gmail.com>
2025-03-31 20:29:12 +0300

    add test

--- tasks/omp/Konstantinov_I_Sort_Batcher/func_tests/main.cpp
+++ tasks/omp/Konstantinov_I_Sort_Batcher/func_tests/main.cpp
@@ -167,6 +167,26 @@ TEST(Konstantinov_I_Sort_Batcher_omp, test_random_100_values) {
   EXPECT_EQ(exp_out, out);
 }
 
+TEST(Konstantinov_I_Sort_Batcher_omp, test_alternating_sign_values) {
+  std::vector<double> in = {10.5, -9.3, 8.1, -7.7, 6.6, -5.5, 4.4, -3.3, 2.2, -1.1};
+  std::vector<double> exp_out = in;
+  std::ranges::sort(exp_out);
+  std::vector<double> out(in.size());
+
+  auto task_data_omp = std::make_shared<ppc::core::TaskData>();
+  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_omp->inputs_count.emplace_back(in.size());
+  task_data_omp->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_omp->outputs_count.emplace_back(out.size());
+
+  konstantinov_i_sort_batcher_omp::RadixSortBatcherOmp test_task_omp(task_data_omp);
+  ASSERT_EQ(test_task_omp.ValidationImpl(), true);
+  test_task_omp.PreProcessingImpl();
+  test_task_omp.RunImpl();
+  test_task_omp.PostProcessingImpl();
+  EXPECT_EQ(exp_out, out);
+}
+
 TEST(Konstantinov_I_Sort_Batcher_omp, test_random_10000_values) {
   constexpr size_t kCount = 11887;
   std::vector<double> in(kCount);
