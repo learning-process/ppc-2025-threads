e5a43e1e9c5eaa66343d6442e9e7f63138d83515
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2025-04-02 21:12:21 +0300

    fix review

--- tasks/omp/kolodkin_g_multiplication_matrix_CRS/func_tests/main.cpp
+++ tasks/omp/kolodkin_g_multiplication_matrix_CRS/func_tests/main.cpp
@@ -331,3 +331,101 @@ TEST(kolodkin_g_multiplication_omp, test_matmul_row_by_col) {
       kolodkin_g_multiplication_matrix_omp::ParseVectorIntoMatrix(out);
   ASSERT_TRUE(kolodkin_g_multiplication_matrix_omp::CheckMatrixesEquality(res, c));
 }
+
+TEST(kolodkin_g_multiplication_omp, test_matmul_diag_matrix) {
+  // Create data
+  kolodkin_g_multiplication_matrix_omp::SparseMatrixCRS a(3, 3);
+  kolodkin_g_multiplication_matrix_omp::SparseMatrixCRS b(3, 3);
+  kolodkin_g_multiplication_matrix_omp::SparseMatrixCRS c(3, 3);
+  std::vector<Complex> in = {};
+  std::vector<Complex> in_a;
+  std::vector<Complex> in_b;
+  std::vector<Complex> out(a.numCols * b.numRows * 100, 0);
+
+  a.AddValue(0, Complex(-1, 0), 0);
+  a.AddValue(1, Complex(-2, 0), 1);
+  a.AddValue(2, Complex(-3, 0), 2);
+
+  b.AddValue(0, Complex(1, 0), 0);
+  b.AddValue(1, Complex(2, 0), 1);
+  b.AddValue(2, Complex(3, 0), 2);
+  in_a = kolodkin_g_multiplication_matrix_omp::ParseMatrixIntoVec(a);
+  in_b = kolodkin_g_multiplication_matrix_omp::ParseMatrixIntoVec(b);
+  in.reserve(in_a.size() + in_b.size());
+  for (unsigned int i = 0; i < in_a.size(); i++) {
+    in.emplace_back(in_a[i]);
+  }
+  for (unsigned int i = 0; i < in_b.size(); i++) {
+    in.emplace_back(in_b[i]);
+  }
+  c.AddValue(0, Complex(-1, 0), 0);
+  c.AddValue(1, Complex(-4, 0), 1);
+  c.AddValue(2, Complex(-9, 0), 2);
+  // Create task_data
+  auto task_data_omp = std::make_shared<ppc::core::TaskData>();
+  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_omp->inputs_count.emplace_back(in.size());
+  task_data_omp->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_omp->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  kolodkin_g_multiplication_matrix_omp::TestTaskOpenMP test_task_omp(task_data_omp);
+  ASSERT_EQ(test_task_omp.Validation(), true);
+  test_task_omp.PreProcessing();
+  test_task_omp.Run();
+  test_task_omp.PostProcessing();
+  kolodkin_g_multiplication_matrix_omp::SparseMatrixCRS res =
+      kolodkin_g_multiplication_matrix_omp::ParseVectorIntoMatrix(out);
+  ASSERT_TRUE(kolodkin_g_multiplication_matrix_omp::CheckMatrixesEquality(res, c));
+}
+
+TEST(kolodkin_g_multiplication_omp, test_matmul_only_imag) {
+  // Create data
+  kolodkin_g_multiplication_matrix_omp::SparseMatrixCRS a(3, 3);
+  kolodkin_g_multiplication_matrix_omp::SparseMatrixCRS b(3, 3);
+  kolodkin_g_multiplication_matrix_omp::SparseMatrixCRS c(3, 3);
+  std::vector<Complex> in = {};
+  std::vector<Complex> in_a;
+  std::vector<Complex> in_b;
+  std::vector<Complex> out(a.numCols * b.numRows * 100, 0);
+
+  a.AddValue(0, Complex(0, 1), 0);
+  a.AddValue(0, Complex(0, 2), 2);
+  a.AddValue(1, Complex(0, 3), 1);
+  a.AddValue(2, Complex(0, 4), 0);
+  a.AddValue(2, Complex(0, 5), 1);
+
+  b.AddValue(0, Complex(0, 6), 1);
+  b.AddValue(1, Complex(0, 7), 0);
+  b.AddValue(2, Complex(0, 8), 2);
+  in_a = kolodkin_g_multiplication_matrix_omp::ParseMatrixIntoVec(a);
+  in_b = kolodkin_g_multiplication_matrix_omp::ParseMatrixIntoVec(b);
+  in.reserve(in_a.size() + in_b.size());
+  for (unsigned int i = 0; i < in_a.size(); i++) {
+    in.emplace_back(in_a[i]);
+  }
+  for (unsigned int i = 0; i < in_b.size(); i++) {
+    in.emplace_back(in_b[i]);
+  }
+  c.AddValue(0, Complex(-6, 0), 1);
+  c.AddValue(0, Complex(-16, 0), 2);
+  c.AddValue(1, Complex(-21, 0), 0);
+  c.AddValue(2, Complex(-24, 0), 1);
+  c.AddValue(2, Complex(-35, 0), 0);
+  // Create task_data
+  auto task_data_omp = std::make_shared<ppc::core::TaskData>();
+  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_omp->inputs_count.emplace_back(in.size());
+  task_data_omp->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_omp->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  kolodkin_g_multiplication_matrix_omp::TestTaskOpenMP test_task_omp(task_data_omp);
+  ASSERT_EQ(test_task_omp.Validation(), true);
+  test_task_omp.PreProcessing();
+  test_task_omp.Run();
+  test_task_omp.PostProcessing();
+  kolodkin_g_multiplication_matrix_omp::SparseMatrixCRS res =
+      kolodkin_g_multiplication_matrix_omp::ParseVectorIntoMatrix(out);
+  ASSERT_TRUE(kolodkin_g_multiplication_matrix_omp::CheckMatrixesEquality(res, c));
+}
