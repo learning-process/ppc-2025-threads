565048d6826bbec5539fcf4e0be9ae688eb78447
odincovm <odincovmisa16@gmail.com>
2025-04-03 23:11:09 +0300

    try get students aproves

--- tasks/tbb/odintsov_m_multmatrix_cannon/func_tests/main.cpp
+++ tasks/tbb/odintsov_m_multmatrix_cannon/func_tests/main.cpp
@@ -9,7 +9,11 @@
 #include "core/task/include/task.hpp"
 #include "tbb/odintsov_m_multmatrix_cannon/include/ops_tbb.hpp"
 
-std::vector<double> odintsov_m_mulmatrix_cannon_tbb::GenerateMatrix(int sz) {
+namespace odintsov_m_mulmatrix_cannon_tbb {
+namespace {
+static std::vector<double> GenerateMatrix(int sz);
+static std::vector<double> MultiplyMatrices(const std::vector<double> &a, const std::vector<double> &b, int n);
+std::vector<double> GenerateMatrix(int sz) {
   std::vector<double> matrix(sz * sz);
   std::random_device rd;
   std::mt19937 gen(rd());
@@ -22,8 +26,7 @@ std::vector<double> odintsov_m_mulmatrix_cannon_tbb::GenerateMatrix(int sz) {
   return matrix;
 }
 
-std::vector<double> odintsov_m_mulmatrix_cannon_tbb::MultiplyMatrices(const std::vector<double> &a,
-                                                                      const std::vector<double> &b, int n) {
+std::vector<double> MultiplyMatrices(const std::vector<double> &a, const std::vector<double> &b, int n) {
   std::vector<double> c(n * n, 0.0);
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
@@ -36,6 +39,8 @@ std::vector<double> odintsov_m_mulmatrix_cannon_tbb::MultiplyMatrices(const std:
   }
   return c;
 }
+}  // namespace
+}  // namespace odintsov_m_mulmatrix_cannon_tbb
 
 TEST(odintsov_m_mulmatrix_cannon_tbb, test_matrix_4) {
   std::vector<double> matrix_a = odintsov_m_mulmatrix_cannon_tbb::GenerateMatrix(4);
@@ -43,14 +48,14 @@ TEST(odintsov_m_mulmatrix_cannon_tbb, test_matrix_4) {
   std::vector<double> out_tbb(16, 0);
   std::vector<double> out_ans = odintsov_m_mulmatrix_cannon_tbb::MultiplyMatrices(matrix_a, matrix_b, 4);
 
-  auto task_data_omp = std::make_shared<ppc::core::TaskData>();
-  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_a.data()));
-  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_b.data()));
-  task_data_omp->inputs_count.emplace_back(matrix_a.size());
-  task_data_omp->inputs_count.emplace_back(matrix_b.size());
-  task_data_omp->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_tbb.data()));
+  auto task_data_tbb = std::make_shared<ppc::core::TaskData>();
+  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_a.data()));
+  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_b.data()));
+  task_data_tbb->inputs_count.emplace_back(matrix_a.size());
+  task_data_tbb->inputs_count.emplace_back(matrix_b.size());
+  task_data_tbb->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_tbb.data()));
 
-  odintsov_m_mulmatrix_cannon_tbb::MulMatrixCannonTBB test_task_tbb(task_data_omp);
+  odintsov_m_mulmatrix_cannon_tbb::MulMatrixCannonTBB test_task_tbb(task_data_tbb);
   ASSERT_EQ(test_task_tbb.Validation(), true);
   test_task_tbb.PreProcessing();
   test_task_tbb.Run();
@@ -67,14 +72,14 @@ TEST(odintsov_m_mulmatrix_cannon_tbb, test_matrix_100) {
   std::vector<double> out_tbb(100, 0);
   std::vector<double> out_ans = odintsov_m_mulmatrix_cannon_tbb::MultiplyMatrices(matrix_a, matrix_b, 10);
 
-  auto task_data_omp = std::make_shared<ppc::core::TaskData>();
-  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_a.data()));
-  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_b.data()));
-  task_data_omp->inputs_count.emplace_back(matrix_a.size());
-  task_data_omp->inputs_count.emplace_back(matrix_b.size());
-  task_data_omp->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_tbb.data()));
+  auto task_data_tbb = std::make_shared<ppc::core::TaskData>();
+  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_a.data()));
+  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_b.data()));
+  task_data_tbb->inputs_count.emplace_back(matrix_a.size());
+  task_data_tbb->inputs_count.emplace_back(matrix_b.size());
+  task_data_tbb->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_tbb.data()));
 
-  odintsov_m_mulmatrix_cannon_tbb::MulMatrixCannonTBB test_task_tbb(task_data_omp);
+  odintsov_m_mulmatrix_cannon_tbb::MulMatrixCannonTBB test_task_tbb(task_data_tbb);
   ASSERT_EQ(test_task_tbb.Validation(), true);
   test_task_tbb.PreProcessing();
   test_task_tbb.Run();
@@ -91,14 +96,14 @@ TEST(odintsov_m_mulmatrix_cannon_tbb, test_matrix_900) {
   std::vector<double> out_tbb(900, 0);
   std::vector<double> out_ans = odintsov_m_mulmatrix_cannon_tbb::MultiplyMatrices(matrix_a, matrix_b, 30);
 
-  auto task_data_omp = std::make_shared<ppc::core::TaskData>();
-  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_a.data()));
-  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_b.data()));
-  task_data_omp->inputs_count.emplace_back(matrix_a.size());
-  task_data_omp->inputs_count.emplace_back(matrix_b.size());
-  task_data_omp->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_tbb.data()));
+  auto task_data_tbb = std::make_shared<ppc::core::TaskData>();
+  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_a.data()));
+  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix_b.data()));
+  task_data_tbb->inputs_count.emplace_back(matrix_a.size());
+  task_data_tbb->inputs_count.emplace_back(matrix_b.size());
+  task_data_tbb->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_tbb.data()));
 
-  odintsov_m_mulmatrix_cannon_tbb::MulMatrixCannonTBB test_task_tbb(task_data_omp);
+  odintsov_m_mulmatrix_cannon_tbb::MulMatrixCannonTBB test_task_tbb(task_data_tbb);
   ASSERT_EQ(test_task_tbb.Validation(), true);
   test_task_tbb.PreProcessing();
   test_task_tbb.Run();
