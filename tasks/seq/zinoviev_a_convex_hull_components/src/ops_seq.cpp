bd68c0d997420c56bb76fc408c9aa99300bd96c4
kobych <kobychit1@gmail.com>
2025-04-03 22:24:52 +0300

    clang format fix

--- tasks/seq/zinoviev_a_convex_hull_components/src/ops_seq.cpp
+++ tasks/seq/zinoviev_a_convex_hull_components/src/ops_seq.cpp
@@ -50,9 +50,8 @@ std::vector<Point> ConvexHullSequential::FindConvexHull(const std::vector<Point>
   }
 
   std::vector<Point> sorted_points(points);
-  std::ranges::sort(sorted_points, [](const Point& a, const Point& b) {
-    return a.x < b.x || (a.x == b.x && a.y < b.y);
-  });
+  std::ranges::sort(sorted_points,
+                    [](const Point& a, const Point& b) { return a.x < b.x || (a.x == b.x && a.y < b.y); });
 
   std::vector<Point> hull;
   hull.reserve(sorted_points.size() * 2);
