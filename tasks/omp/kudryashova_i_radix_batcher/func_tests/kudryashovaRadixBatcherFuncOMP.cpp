474dee717fbeb5389b3ff2fd5428d262ab59605c
Irina2004-tech <irakudr2015@gmail.com>
2025-04-03 19:11:05 +0300

    add double reverse test

--- tasks/omp/kudryashova_i_radix_batcher/func_tests/kudryashovaRadixBatcherFuncOMP.cpp
+++ tasks/omp/kudryashova_i_radix_batcher/func_tests/kudryashovaRadixBatcherFuncOMP.cpp
@@ -173,3 +173,24 @@ TEST(kudryashova_i_radix_batcher_omp, omp_radix_test_reverse_order) {
   std::ranges::sort(sorted_global_vector);
   ASSERT_EQ(result, sorted_global_vector);
 }
+
+TEST(kudryashova_i_radix_batcher_omp, omp_radix_test_double_reverse_order) {
+  int global_vector_size = 88;
+  std::vector<double> global_vector = kudryashova_i_radix_batcher_omp::GetRandomDoubleVector(global_vector_size);
+  std::ranges::sort(global_vector, std::greater<>());
+  std::ranges::reverse(global_vector.begin(), global_vector.end());
+  std::vector<double> result(global_vector_size);
+  std::shared_ptr<ppc::core::TaskData> task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_vector.data()));
+  task_data->inputs_count.emplace_back(global_vector.size());
+  task_data->outputs.emplace_back(reinterpret_cast<uint8_t *>(result.data()));
+  task_data->outputs_count.emplace_back(result.size());
+  kudryashova_i_radix_batcher_omp::TestTaskOpenMP task_open_mp(task_data);
+  ASSERT_TRUE(task_open_mp.ValidationImpl());
+  task_open_mp.PreProcessingImpl();
+  task_open_mp.RunImpl();
+  task_open_mp.PostProcessingImpl();
+  std::vector<double> sorted_global_vector = global_vector;
+  std::ranges::sort(sorted_global_vector);
+  ASSERT_EQ(result, sorted_global_vector);
+}
