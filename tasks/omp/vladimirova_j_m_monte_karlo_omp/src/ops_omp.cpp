3f009d5cb597a57a303d4c1d44af7628dd291636
VlJulia <129722951+VlJulia@users.noreply.github.com>
2025-04-04 14:29:02 +0300

    fix_random_0

--- tasks/omp/vladimirova_j_m_monte_karlo_omp/src/ops_omp.cpp
+++ tasks/omp/vladimirova_j_m_monte_karlo_omp/src/ops_omp.cpp
@@ -8,8 +8,8 @@
 
 namespace {
 
-std::random_device rd;
-std::mt19937 gen(rd());
+thread_local std::mt19937 gen(std::random_device{}());
+
 double CreateRandomVal(double min_v, double max_v) {
   std::uniform_real_distribution<double> dis(min_v, max_v);
   return dis(gen);
@@ -58,14 +58,13 @@ bool vladimirova_j_m_monte_karlo_omp::TestTaskOpenMP::RunImpl() {
   {
     std::vector<double> random_val = std::vector<double>(var_size_);
     size_t local_successful_point = 0;
-#pragma omp for
+#pragma omp for reduction(+ : successful_point)
     for (int i = 0; i < (int)accuracy_; i++) {
       for (size_t j = 0; j < var_size_; j++) {
         random_val[j] = CreateRandomVal(var_integr_[j].min, var_integr_[j].max);
       }
       local_successful_point += (int)(func_(random_val, var_size_));
     }
-#pragma omp atomic
     successful_point += local_successful_point;
   }
   double s = 1;
