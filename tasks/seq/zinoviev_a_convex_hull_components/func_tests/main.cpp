3fd3d71c3f6471a506f1199dbcd25c696fc95dfc
kobych <kobychit1@gmail.com>
2025-04-04 21:01:51 +0300

    Clang tidy func tests update

--- tasks/seq/zinoviev_a_convex_hull_components/func_tests/main.cpp
+++ tasks/seq/zinoviev_a_convex_hull_components/func_tests/main.cpp
@@ -10,14 +10,14 @@
 
 namespace {
 void SetupTaskData(const std::vector<int>& input, int width, int height, size_t output_size,
-                   std::shared_ptr<ppc::core::TaskData>& taskData) {
-  taskData = std::make_shared<ppc::core::TaskData>();
-  taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(const_cast<int*>(input.data())));
-  taskData->inputs_count.emplace_back(width);
-  taskData->inputs_count.emplace_back(height);
-  taskData->outputs.emplace_back(
+                   std::shared_ptr<ppc::core::TaskData>& task_data) {
+  task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(const_cast<int*>(input.data())));
+  task_data->inputs_count.emplace_back(width);
+  task_data->inputs_count.emplace_back(height);
+  task_data->outputs.emplace_back(
       reinterpret_cast<uint8_t*>(new zinoviev_a_convex_hull_components_seq::Point[output_size]));
-  taskData->outputs_count.emplace_back(output_size);
+  task_data->outputs_count.emplace_back(output_size);
 }
 
 void VerifyResult(const std::vector<zinoviev_a_convex_hull_components_seq::Point>& actual,
@@ -31,20 +31,20 @@ void VerifyResult(const std::vector<zinoviev_a_convex_hull_components_seq::Point
 
 void RunAndValidate(const std::vector<int>& input,
                     const std::vector<zinoviev_a_convex_hull_components_seq::Point>& expected, int width, int height) {
-  std::shared_ptr<ppc::core::TaskData> taskData;
-  SetupTaskData(input, width, height, expected.size(), taskData);
+  std::shared_ptr<ppc::core::TaskData> task_data;
+  SetupTaskData(input, width, height, expected.size(), task_data);
 
-  zinoviev_a_convex_hull_components_seq::ConvexHullSequential task(taskData);
+  zinoviev_a_convex_hull_components_seq::ConvexHullSequential task(task_data);
   ASSERT_TRUE(task.Validation());
   ASSERT_TRUE(task.PreProcessing());
   ASSERT_TRUE(task.Run());
   ASSERT_TRUE(task.PostProcessing());
 
-  auto* output = reinterpret_cast<zinoviev_a_convex_hull_components_seq::Point*>(taskData->outputs[0]);
+  auto* output = reinterpret_cast<zinoviev_a_convex_hull_components_seq::Point*>(task_data->outputs[0]);
   std::vector<zinoviev_a_convex_hull_components_seq::Point> actual(output, output + expected.size());
 
   VerifyResult(actual, expected);
-  delete[] reinterpret_cast<zinoviev_a_convex_hull_components_seq::Point*>(taskData->outputs[0]);
+  delete[] reinterpret_cast<zinoviev_a_convex_hull_components_seq::Point*>(task_data->outputs[0]);
 }
 }  // namespace
 
