19ab2e937cb174f4248e238be3df81415b12444a
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2025-04-04 13:24:59 +0300

    add new test

--- tasks/omp/solovev_a_ccs_mmult_sparse_omp/func_tests/main.cpp
+++ tasks/omp/solovev_a_ccs_mmult_sparse_omp/func_tests/main.cpp
@@ -25,6 +25,34 @@ bool AreComplexNumbersApproxEqual(const std::complex<double>& c1, const std::com
 }
 }  // namespace
 
+TEST(solovev_a_ccs_mmult_sparse_omp, test_zero_matrix_random) {
+  const int size = 50;
+  solovev_a_matrix_omp::MatrixInCcsSparse m1(size, size, 0);
+  solovev_a_matrix_omp::MatrixInCcsSparse m2(size, size, 0);
+  solovev_a_matrix_omp::MatrixInCcsSparse m3(size, size, 0);
+
+  for (int i = 0; i < size; i++) {
+    m2.col_p.push_back(i);
+    m2.row.push_back(i);
+    m2.val.push_back(GenerateRandomComplex(-10.0, 10.0));
+  }
+
+  std::shared_ptr<ppc::core::TaskData> task_data_omp = std::make_shared<ppc::core::TaskData>();
+  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t*>(&m1));
+  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t*>(&m2));
+  task_data_omp->outputs.emplace_back(reinterpret_cast<uint8_t*>(&m3));
+
+  solovev_a_matrix_omp::OMPMatMultCcs test_task_omp(task_data_omp);
+  ASSERT_EQ(test_task_omp.ValidationImpl(), true);
+  test_task_omp.PreProcessingImpl();
+  test_task_omp.RunImpl();
+  test_task_omp.PostProcessingImpl();
+
+  for (size_t i = 0; i < m3.val.size(); i++) {
+    ASSERT_EQ(m3.val[i], std::complex<double>(0.0, 0.0));
+  }
+}
+
 TEST(solovev_a_ccs_mmult_sparse_omp, test_I) {
   solovev_a_matrix_omp::MatrixInCcsSparse m1(1, 1, 1);
   solovev_a_matrix_omp::MatrixInCcsSparse m2(1, 1, 1);
