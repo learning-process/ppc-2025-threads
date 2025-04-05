b1729da60dadc39cddc2947110cb41703c6667f9
ArtemPch <131471743+ArtemPch@users.noreply.github.com>
2025-04-05 16:23:35 +0300

    del

--- tasks/omp/petrov_a_radix_double_batcher/perf_tests/main.cpp
+++ tasks/omp/petrov_a_radix_double_batcher/perf_tests/main.cpp
@@ -24,7 +24,7 @@ std::vector<double> RandomVector(size_t size) {
 }  // namespace
 
 TEST(petrov_a_radix_double_batcher_omp, test_pipeline_run) {
-  auto in = RandomVector(32'000'000);
+  auto in = RandomVector(25'000'000);
   std::vector<double> out(in.size());
 
   auto task_data_omp = std::make_shared<ppc::core::TaskData>();
@@ -52,7 +52,7 @@ TEST(petrov_a_radix_double_batcher_omp, test_pipeline_run) {
 }
 
 TEST(petrov_a_radix_double_batcher_omp, test_task_run) {
-  auto in = RandomVector(32'000'000);
+  auto in = RandomVector(25'000'000);
   std::vector<double> out(in.size());
 
   auto task_data_omp = std::make_shared<ppc::core::TaskData>();
