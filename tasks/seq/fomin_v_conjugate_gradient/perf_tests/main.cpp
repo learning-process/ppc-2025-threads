565b012267e25c3414e57e81edd32b0a2920cb9c
W-Snack <90415228+W-Snack@users.noreply.github.com>
2025-04-05 21:14:10 +0300

    747945

--- tasks/seq/fomin_v_conjugate_gradient/perf_tests/main.cpp
+++ tasks/seq/fomin_v_conjugate_gradient/perf_tests/main.cpp
@@ -10,12 +10,19 @@
 #include "seq/fomin_v_conjugate_gradient/include/ops_seq.hpp"
 
 TEST(fomin_v_conjugate_gradient_seq, test_pipeline_run) {
-  constexpr int kCount = 5000;  // размер системы
+  constexpr int kCount = 500;  // Оптимальный размер системы
 
+  // Создаем трехдиагональную матрицу с диагональным преобладанием
   std::vector<double> input((kCount * kCount) + kCount, 0.0);
   for (int i = 0; i < kCount; ++i) {
-    input[(i * kCount) + i] = 1.0;       // Единичная матрица
-    input[(kCount * kCount) + i] = 1.0;  // Вектор b = {1, 1, ..., 1}
+    input[(i * kCount) + i] = 4.0;
+    if (i > 0) {
+      input[(i * kCount) + (i - 1)] = -1.0;
+    }
+    if (i < kCount - 1) {
+      input[(i * kCount) + (i + 1)] = -1.0;
+    }
+    input[(kCount * kCount) + i] = 1.0;
   }
 
   // Создаем task_data
