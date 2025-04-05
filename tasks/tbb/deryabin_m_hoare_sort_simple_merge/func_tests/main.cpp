30bf196a404fcb84e3c61451314bc56977c1d474
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-04-05 04:15:54 +0300

    Update main.cpp

--- tasks/tbb/deryabin_m_hoare_sort_simple_merge/func_tests/main.cpp
+++ tasks/tbb/deryabin_m_hoare_sort_simple_merge/func_tests/main.cpp
@@ -74,6 +74,69 @@ TEST(deryabin_m_hoare_sort_simple_merge_tbb, test_double_reverse_array) {
   ASSERT_EQ(true_solution, out_array[0]);
 }
 
+TEST(deryabin_m_hoare_sort_simple_merge_tbb, test_half_of_the_same_elements_array) {
+  // Create data
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::vector<double> input_array(800);
+  const auto half = input_array.size() / 2U;
+  std::ranges::generate(input_array.begin(), input_array.end() - (long)half,
+                        [&]() mutable { return 0; });
+  std::ranges::generate(input_array.end() - (long)half, input_array.end(),
+                        [&]() mutable { return distribution(gen); });
+  std::shuffle(input_array.begin(), input_array.end(), gen);
+  std::vector<std::vector<double>> in_array(1, input_array);
+  size_t chunk_count = 8;
+  std::vector<double> output_array(800);
+  std::vector<std::vector<double>> out_array(1, output_array);
+  std::vector<double> true_solution(input_array);
+  std::ranges::sort(true_solution.begin(), true_solution.end());
+
+  // Create TaskData
+  auto task_data_tbb = std::make_shared<ppc::core::TaskData>();
+  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t*>(in_array.data()));
+  task_data_tbb->inputs_count.emplace_back(input_array.size());
+  task_data_tbb->inputs_count.emplace_back(chunk_count);
+  task_data_tbb->outputs.emplace_back(reinterpret_cast<uint8_t*>(out_array.data()));
+  task_data_tbb->outputs_count.emplace_back(output_array.size());
+
+  // Create Task
+  deryabin_m_hoare_sort_simple_merge_tbb::HoareSortTaskTBB hoare_sort_task_tbb(task_data_tbb);
+  ASSERT_EQ(hoare_sort_task_tbb.Validation(), true);
+  hoare_sort_task_tbb.PreProcessing();
+  hoare_sort_task_tbb.Run();
+  hoare_sort_task_tbb.PostProcessing();
+  ASSERT_EQ(true_solution, out_array[0]);
+}
+
+TEST(deryabin_m_hoare_sort_simple_merge_tbb, test_same_elements_array) {
+  // Create data
+  std::vector<double> input_array(800);
+  std::ranges::generate(input_array.begin(), input_array.end(),
+                        [&]() mutable { return 0; });
+  std::vector<std::vector<double>> in_array(1, input_array);
+  size_t chunk_count = 8;
+  std::vector<double> output_array(800);
+  std::vector<std::vector<double>> out_array(1, output_array);
+  std::vector<double> true_solution(input_array);
+
+  // Create TaskData
+  auto task_data_tbb = std::make_shared<ppc::core::TaskData>();
+  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t*>(in_array.data()));
+  task_data_tbb->inputs_count.emplace_back(input_array.size());
+  task_data_tbb->inputs_count.emplace_back(chunk_count);
+  task_data_tbb->outputs.emplace_back(reinterpret_cast<uint8_t*>(out_array.data()));
+  task_data_tbb->outputs_count.emplace_back(output_array.size());
+
+  // Create Task
+  deryabin_m_hoare_sort_simple_merge_tbb::HoareSortTaskTBB hoare_sort_task_tbb(task_data_tbb);
+  ASSERT_EQ(hoare_sort_task_tbb.Validation(), true);
+  hoare_sort_task_tbb.PreProcessing();
+  hoare_sort_task_tbb.Run();
+  hoare_sort_task_tbb.PostProcessing();
+  ASSERT_EQ(true_solution, out_array[0]);
+}
+
 TEST(deryabin_m_hoare_sort_simple_merge_tbb, test_large_array) {
   // Create data
   std::random_device rd;
