559edc06ffdb0c9a2c49448c0e90b9084332943a
Ofuuse <Ofuuse@gmail.com>
2025-04-06 19:19:50 +0300

    Last fixed clang

--- tasks/omp/durynichev_d_integrals_simpson_method/func_tests/main.cpp
+++ tasks/omp/durynichev_d_integrals_simpson_method/func_tests/main.cpp
@@ -61,24 +61,6 @@ TEST(durynichev_d_integrals_simpson_method_omp, test_integral_2D_x2_plus_y2) {
   EXPECT_NEAR(out[0], 2.0 / 3.0, 1e-4);
 }
 
-TEST(durynichev_d_integrals_simpson_method_omp, test_integral_2D_x2_plus_y2_wider_range) {
-  std::vector<double> in = {0.0, 2.0, 0.0, 2.0, 100};
-  std::vector<double> out(1, 0.0);
-
-  auto task_data = std::make_shared<ppc::core::TaskData>();
-  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-  task_data->inputs_count.emplace_back(in.size());
-  task_data->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-  task_data->outputs_count.emplace_back(out.size());
-
-  durynichev_d_integrals_simpson_method_omp::SimpsonIntegralOpenMP task(task_data);
-  ASSERT_TRUE(task.ValidationImpl());
-  task.PreProcessingImpl();
-  task.RunImpl();
-  task.PostProcessingImpl();
-  EXPECT_NEAR(out[0], 16.0 / 3.0, 1e-4);
-}
-
 TEST(durynichev_d_integrals_simpson_method_omp, test_integral_2D_x2_plus_y2_symmetric_range) {
   std::vector<double> in = {-1.0, 1.0, -1.0, 1.0, 100};
   std::vector<double> out(1, 0.0);
