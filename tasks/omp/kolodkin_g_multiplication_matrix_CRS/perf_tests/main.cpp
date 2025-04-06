7ccbc90e37a894668ad9248d97639909e2326cb5
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2025-04-06 08:58:53 +0300

    add seed

--- tasks/omp/kolodkin_g_multiplication_matrix_CRS/perf_tests/main.cpp
+++ tasks/omp/kolodkin_g_multiplication_matrix_CRS/perf_tests/main.cpp
@@ -11,6 +11,7 @@
 #include "omp/kolodkin_g_multiplication_matrix_CRS/include/ops_omp.hpp"
 
 TEST(kolodkin_g_multiplication_matrix__task_omp, test_pipeline_run) {
+  srand(time(0));
   kolodkin_g_multiplication_matrix_omp::SparseMatrixCRS a(400, 400);
   kolodkin_g_multiplication_matrix_omp::SparseMatrixCRS b(400, 400);
   std::vector<Complex> in = {};
@@ -70,6 +71,7 @@ TEST(kolodkin_g_multiplication_matrix__task_omp, test_pipeline_run) {
 }
 
 TEST(kolodkin_g_multiplication_matrix__task_omp, test_task_run) {
+  srand(time(0));
   kolodkin_g_multiplication_matrix_omp::SparseMatrixCRS a(400, 400);
   kolodkin_g_multiplication_matrix_omp::SparseMatrixCRS b(400, 400);
   std::vector<Complex> in = {};
