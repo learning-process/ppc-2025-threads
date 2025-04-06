ec6b02179e53b17cc894b3ac9270ff0ca670267e
W-Snack <90415228+W-Snack@users.noreply.github.com>
2025-04-06 03:13:14 +0300

    600

--- tasks/seq/fomin_v_conjugate_gradient/perf_tests/main.cpp
+++ tasks/seq/fomin_v_conjugate_gradient/perf_tests/main.cpp
@@ -10,7 +10,7 @@
 #include "seq/fomin_v_conjugate_gradient/include/ops_seq.hpp"
 
 TEST(fomin_v_conjugate_gradient_seq, test_pipeline_run) {
-  constexpr int kCount = 500;
+  constexpr int kCount = 600;
 
   // Создаем трехдиагональную матрицу с диагональным преобладанием
   std::vector<double> input((kCount * kCount) + kCount, 0.0);
