ec7cb4e5174db485728a8ae6d6096d6cb99a27fd
NeeMiyuki <zaxarov2706@gmail.com>
2025-04-07 21:34:33 +0300

    clang-format

--- tasks/tbb/shulpin_i_jarvis_passage/perf_tests/main.cpp
+++ tasks/tbb/shulpin_i_jarvis_passage/perf_tests/main.cpp
@@ -35,7 +35,7 @@ bool ValidateConvexHull(const std::vector<shulpin_i_jarvis_tbb::Point> &hull, co
 
 TEST(shulpin_i_jarvis_tbb, test_pipeline_run) {
   size_t num_points = 10000;
-std::vector<shulpin_i_jarvis_tbb::Point> input = shulpin_tbb_test_module::GenerateRandomPoints(num_points);
+  std::vector<shulpin_i_jarvis_tbb::Point> input = shulpin_tbb_test_module::GenerateRandomPoints(num_points);
   std::vector<shulpin_i_jarvis_tbb::Point> out(input.size());
 
   auto task_data_par = std::make_shared<ppc::core::TaskData>();
