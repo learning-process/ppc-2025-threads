935de535a4ec4517a963687f871cab246f483af0
kobych <kobychit1@gmail.com>
2025-04-08 19:01:10 +0300

    Clang format fix in perf tests new

--- tasks/omp/zinoviev_a_convex_hull_components/perf_tests/main.cpp
+++ tasks/omp/zinoviev_a_convex_hull_components/perf_tests/main.cpp
@@ -48,7 +48,7 @@ TEST(zinoviev_a_convex_hull_omp, test_pipeline_run) {
   std::vector<zinoviev_a_convex_hull_components_omp::Point> actual(res, res + res_size);
 
   std::vector<zinoviev_a_convex_hull_components_omp::Point> expect{
-      {.x = 0, .y =0}, {.x = 99, .y = 0}, {.x = 98, .y = 99}, {.x = 0, .y = 99}};
+      {.x = 0, .y = 0}, {.x = 99, .y = 0}, {.x = 98, .y = 99}, {.x = 0, .y = 99}};
   CheckResult(actual, expect);
 
   delete[] reinterpret_cast<zinoviev_a_convex_hull_components_omp::Point*>(data->outputs[0]);
