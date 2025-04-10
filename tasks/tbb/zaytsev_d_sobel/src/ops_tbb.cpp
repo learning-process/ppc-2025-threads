337a9b6d02857d720aca114dbeb53d198f7fa5a5
DZaytsev4 <deniszaytsev79524555@gmail.com>
2025-04-11 02:47:26 +0300

    clangv2

--- tasks/tbb/zaytsev_d_sobel/src/ops_tbb.cpp
+++ tasks/tbb/zaytsev_d_sobel/src/ops_tbb.cpp
@@ -1,12 +1,13 @@
 #include "tbb/zaytsev_d_sobel/include/ops_tbb.hpp"
 
-#include <tbb/blocked_range.h>
-#include <tbb/parallel_for.h>
 #include <tbb/tbb.h>
 
 #include <algorithm>
 #include <cmath>
 #include <vector>
+
+#include "oneapi/tbb/blocked_range.h"
+#include "oneapi/tbb/parallel_for.h"
 
 bool zaytsev_d_sobel_tbb::TestTaskTBB::PreProcessingImpl() {
   auto *in_ptr = reinterpret_cast<int *>(task_data->inputs[0]);
