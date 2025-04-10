0735f07898a75f269d586ec79f2a88f967c9dfce
NeeMiyuki <zaxarov2706@gmail.com>
2025-04-10 22:10:14 +0300

    dir rename

--- tasks/omp/shulpin_i_jarvis_passage/include/ops_omp.hpp
+++ tasks/omp/shulpin_i_jarvis_passage/include/ops_omp.hpp
@@ -16,6 +16,14 @@ struct Point {
   Point(double x_coordinate, double y_coordinate) : x(x_coordinate), y(y_coordinate) {}
 };
 
+struct PointEqual {
+  bool operator()(const Point& a, const Point& b) const { return a.x == b.x && a.y == b.y; }
+};
+
+struct PointEqual {
+  bool operator()(const Point& a, const Point& b) const { return a.x == b.x && a.y == b.y; }
+};
+
 class JarvisSequential : public ppc::core::Task {
  public:
   explicit JarvisSequential(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
