1880b5fbd8964472ec2ae9b66032428e8f20faa3
NeeMiyuki <zaxarov2706@gmail.com>
2025-04-11 15:27:41 +0300

    large number of points fix

--- tasks/seq/shulpin_i_jarvis_passage/src/ops_seq.cpp
+++ tasks/seq/shulpin_i_jarvis_passage/src/ops_seq.cpp
@@ -6,8 +6,8 @@
 #include <vector>
 
 int shulpin_i_jarvis_seq::JarvisSequential::Orientation(const Point& p, const Point& q, const Point& r) {
-  int val = static_cast<int>(((q.y - p.y) * (r.x - q.x)) - ((q.x - p.x) * (r.y - q.y)));
-  if (val == 0) {
+  double val = ((q.y - p.y) * (r.x - q.x)) - ((q.x - p.x) * (r.y - q.y));
+  if (std::fabs(val) < 1e-9) {
     return 0;
   }
   return (val > 0) ? 1 : 2;
