b9f2aa79f55c83156f691a6ed1430df0eead8435
MikasaAkc <daria.komshina@mail.ru>
2025-04-13 15:25:34 +0300

    tbb

--- tasks/tbb/komshina_d_image_filtering_vertical_gaussian/perf_tests/main.cpp
+++ tasks/tbb/komshina_d_image_filtering_vertical_gaussian/perf_tests/main.cpp
@@ -11,8 +11,8 @@
 #include "tbb/komshina_d_image_filtering_vertical_gaussian/include/ops_tbb.hpp"
 
 TEST(komshina_d_image_filtering_vertical_gaussian_tbb, test_pipeline_run) {
-  constexpr int kWidth = 3000;
-  constexpr int kHeight = 3000;
+  constexpr int kWidth = 4000;
+  constexpr int kHeight = 4000;
 
   std::vector<unsigned char> in(kWidth * kHeight * 3, 0);
   std::vector<unsigned char> out(kWidth * kHeight * 3, 0);
@@ -52,8 +52,8 @@ TEST(komshina_d_image_filtering_vertical_gaussian_tbb, test_pipeline_run) {
 }
 
 TEST(komshina_d_image_filtering_vertical_gaussian_tbb, test_task_run) {
-  constexpr int kWidth = 3000;
-  constexpr int kHeight = 3000;
+  constexpr int kWidth = 4000;
+  constexpr int kHeight = 4000;
 
   std::vector<unsigned char> in(kWidth * kHeight * 3, 0);
   std::vector<unsigned char> out(kWidth * kHeight * 3, 0);
