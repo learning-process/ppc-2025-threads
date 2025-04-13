5c96ba61652fc0bf21f3a929b8c485f088dc42ce
kobych <kobychit1@gmail.com>
2025-04-13 16:54:47 +0300

    perf tests data change

--- tasks/seq/zinoviev_a_convex_hull_components/perf_tests/main.cpp
+++ tasks/seq/zinoviev_a_convex_hull_components/perf_tests/main.cpp
@@ -10,7 +10,7 @@
 #include "seq/zinoviev_a_convex_hull_components/include/ops_seq.hpp"
 
 TEST(zinoviev_a_convex_hull_components_seq, test_pipeline_run) {
-  const int size = 3300;
+  const int size = 5000;
   std::vector<int> input(size * size, 0);
   for (int i = 0; i < size; ++i) {
     input[i] = 1;
@@ -29,7 +29,7 @@ TEST(zinoviev_a_convex_hull_components_seq, test_pipeline_run) {
   auto task = std::make_shared<zinoviev_a_convex_hull_components_seq::ConvexHullSequential>(task_data);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-  perf_attr->num_running = 10;
+  perf_attr->num_running = 100;
   const auto t0 = std::chrono::high_resolution_clock::now();
   perf_attr->current_timer = [&] {
     auto current_time_point = std::chrono::high_resolution_clock::now();
@@ -47,7 +47,7 @@ TEST(zinoviev_a_convex_hull_components_seq, test_pipeline_run) {
 }
 
 TEST(zinoviev_a_convex_hull_components_seq, test_task_run) {
-  const int size = 3300;
+  const int size = 5000;
   std::vector<int> input(size * size, 0);
   for (int i = 0; i < size; ++i) {
     input[i] = 1;
@@ -66,7 +66,7 @@ TEST(zinoviev_a_convex_hull_components_seq, test_task_run) {
   auto task = std::make_shared<zinoviev_a_convex_hull_components_seq::ConvexHullSequential>(task_data);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-  perf_attr->num_running = 10;
+  perf_attr->num_running = 100;
   const auto t0 = std::chrono::high_resolution_clock::now();
   perf_attr->current_timer = [&] {
     auto current_time_point = std::chrono::high_resolution_clock::now();
