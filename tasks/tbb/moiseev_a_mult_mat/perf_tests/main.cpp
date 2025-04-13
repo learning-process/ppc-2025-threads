7ae7bdb4c35455e9c192a7cd35455914746eed4e
MoiseevArt <amoiseev_00@mail.ru>
2025-04-13 23:27:38 +0000

    fix 0.5

--- tasks/tbb/moiseev_a_mult_mat/perf_tests/main.cpp
+++ tasks/tbb/moiseev_a_mult_mat/perf_tests/main.cpp
@@ -30,9 +30,9 @@ void NaiveMultiply(const std::vector<double> &a, const std::vector<double> &b, s
     for (size_t j = 0; j < n; ++j) {
       double sum = 0.0;
       for (size_t k = 0; k < n; ++k) {
-        sum += a[i * n + k] * b[k * n + j];
+        sum += a[(i * n) + k] * b[(k * n) + j];
       }
-      c[i * n + j] = sum;
+      c[(i * n) + j] = sum;
     }
   }
 }
