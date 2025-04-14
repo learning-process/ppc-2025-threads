20645967c7b005bd63ac51b9ef12c987b7c1c4f9
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2025-04-14 21:49:17 +0300

    Update ops_tbb.cpp

--- tasks/tbb/vavilov_v_cannon/src/ops_tbb.cpp
+++ tasks/tbb/vavilov_v_cannon/src/ops_tbb.cpp
@@ -136,39 +136,24 @@ bool vavilov_v_cannon_tbb::CannonTBB::RunImpl() {
 void vavilov_v_cannon_tbb::CannonTBB::InitialShift() {
   std::vector<double> a_tmp = A_;
   std::vector<double> b_tmp = B_;
-
-  tbb::parallel_invoke(
-      [&] {  // обработка A_
-        tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_),
-                          [&](const tbb::blocked_range2d<int>& r) {
-                            for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
-                              for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
-                                int src_col = (bj + bi) % num_blocks_;
-                                for (int i = 0; i < block_size_; ++i) {
-                                  int dst = (bi * block_size_ + i) * N_ + bj * block_size_;
-                                  int src = (bi * block_size_ + i) * N_ + src_col * block_size_;
-                                  std::copy_n(&a_tmp[src], block_size_, &A_[dst]);
-                                }
-                              }
-                            }
-                          });
-      },
-      [&] {  // обработка B_
-        tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_),
-                          [&](const tbb::blocked_range2d<int>& r) {
-                            for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
-                              for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
-                                int src_row = (bi + bj) % num_blocks_;
-                                for (int i = 0; i < block_size_; ++i) {
-                                  int dst = (bi * block_size_ + i) * N_ + bj * block_size_;
-                                  int src = (src_row * block_size_ + i) * N_ + bj * block_size_;
-                                  std::copy_n(&b_tmp[src], block_size_, &B_[dst]);
-                                }
-                              }
-                            }
-                          });
-      });
+  tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_), [&](const tbb::blocked_range2d<int>& r) {
+    for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
+      for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
+        int src_row = (bi + bj) % num_blocks_;
+        int src_col = (bj + bi) % num_blocks_;
+        for (int i = 0; i < block_size_; ++i) {
+          for (int j = 0; j < block_size_; ++j) {
+            B_[(bi * block_size_ + i) * N_ + (bj * block_size_ + j)] =
+                b_tmp[(src_row * block_size_ + i) * N_ + (bj * block_size_ + j)];
+            A_[(bi * block_size_ + i) * N_ + (bj * block_size_ + j)] =
+                a_tmp[(bi * block_size_ + i) * N_ + (src_col * block_size_ + j)];
+          }
+        }
+      }
+    }
+  });
 }
+
 /*
 void vavilov_v_cannon_tbb::CannonTBB::BlockMultiply() {
   oneapi::tbb::parallel_for(
@@ -269,42 +254,25 @@ void vavilov_v_cannon_tbb::CannonTBB::BlockMultiply() {
     }
   }
 }
-
 void vavilov_v_cannon_tbb::CannonTBB::ShiftBlocks() {
   std::vector<double> a_tmp = A_;
   std::vector<double> b_tmp = B_;
-
-  tbb::parallel_invoke(
-      [&] {  // обработка A_
-        tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_),
-                          [&](const tbb::blocked_range2d<int>& r) {
-                            for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
-                              for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
-                                int src_col = (bj + 1) % num_blocks_;
-                                for (int i = 0; i < block_size_; ++i) {
-                                  int dst = (bi * block_size_ + i) * N_ + bj * block_size_;
-                                  int src = (bi * block_size_ + i) * N_ + src_col * block_size_;
-                                  std::copy_n(&a_tmp[src], block_size_, &A_[dst]);
-                                }
-                              }
-                            }
-                          });
-      },
-      [&] {  // обработка B_
-        tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_),
-                          [&](const tbb::blocked_range2d<int>& r) {
-                            for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
-                              for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
-                                int src_row = (bi + 1) % num_blocks_;
-                                for (int i = 0; i < block_size_; ++i) {
-                                  int dst = (bi * block_size_ + i) * N_ + bj * block_size_;
-                                  int src = (src_row * block_size_ + i) * N_ + bj * block_size_;
-                                  std::copy_n(&b_tmp[src], block_size_, &B_[dst]);
-                                }
-                              }
-                            }
-                          });
-      });
+  tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_), [&](const tbb::blocked_range2d<int>& r) {
+    for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
+      for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
+        int src_row = (bi + 1) % num_blocks_;
+        int src_col = (bj + 1) % num_blocks_;
+        for (int i = 0; i < block_size_; ++i) {
+          for (int j = 0; j < block_size_; ++j) {
+            B_[(bi * block_size_ + i) * N_ + (bj * block_size_ + j)] =
+                b_tmp[(src_row * block_size_ + i) * N_ + (bj * block_size_ + j)];
+            A_[(bi * block_size_ + i) * N_ + (bj * block_size_ + j)] =
+                a_tmp[(bi * block_size_ + i) * N_ + (src_col * block_size_ + j)];
+          }
+        }
+      }
+    }
+  });
 }
 
 bool vavilov_v_cannon_tbb::CannonTBB::RunImpl() {
