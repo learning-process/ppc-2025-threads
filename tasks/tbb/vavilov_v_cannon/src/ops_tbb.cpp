c55d87d42cdfd79b8da25bd25ca1939f767dd979
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2025-04-14 10:19:05 +0300

    Update ops_tbb.cpp

--- tasks/tbb/vavilov_v_cannon/src/ops_tbb.cpp
+++ tasks/tbb/vavilov_v_cannon/src/ops_tbb.cpp
@@ -138,35 +138,35 @@ void vavilov_v_cannon_tbb::CannonTBB::InitialShift() {
   std::vector<double> b_tmp = B_;
 
   tbb::parallel_invoke(
-       [&] {  // обработка A_
-         tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_),
-                           [&](const tbb::blocked_range2d<int>& r) {
-                             for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
-                               for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
-                                 int src_col = (bj + bi) % num_blocks_;
-                                 for (int i = 0; i < block_size_; ++i) {
-                                   int dst = (bi * block_size_ + i) * N_ + bj * block_size_;
-                                   int src = (bi * block_size_ + i) * N_ + src_col * block_size_;
-                                   std::copy_n(&a_tmp[src], block_size_, &A_[dst]);
-                                 }
-                               }
-                             }
-                           });
-       },
-       [&] {  // обработка B_
-         tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_),
-                           [&](const tbb::blocked_range2d<int>& r) {
-                             for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
-                               for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
-                                 int src_row = (bi + bj) % num_blocks_;
-                                 for (int i = 0; i < block_size_; ++i) {
-                                   int dst = (bi * block_size_ + i) * N_ + bj * block_size_;
-                                   int src = (src_row * block_size_ + i) * N_ + bj * block_size_;
-                                   std::copy_n(&b_tmp[src], block_size_, &B_[dst]);
-                                 }
-                               }
-                             }
-                           });
+      [&] {  // обработка A_
+        tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_),
+                          [&](const tbb::blocked_range2d<int>& r) {
+                            for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
+                              for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
+                                int src_col = (bj + bi) % num_blocks_;
+                                for (int i = 0; i < block_size_; ++i) {
+                                  int dst = (bi * block_size_ + i) * N_ + bj * block_size_;
+                                  int src = (bi * block_size_ + i) * N_ + src_col * block_size_;
+                                  std::copy_n(&a_tmp[src], block_size_, &A_[dst]);
+                                }
+                              }
+                            }
+                          });
+      },
+      [&] {  // обработка B_
+        tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_),
+                          [&](const tbb::blocked_range2d<int>& r) {
+                            for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
+                              for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
+                                int src_row = (bi + bj) % num_blocks_;
+                                for (int i = 0; i < block_size_; ++i) {
+                                  int dst = (bi * block_size_ + i) * N_ + bj * block_size_;
+                                  int src = (src_row * block_size_ + i) * N_ + bj * block_size_;
+                                  std::copy_n(&b_tmp[src], block_size_, &B_[dst]);
+                                }
+                              }
+                            }
+                          });
       });
 }
 
@@ -223,35 +223,35 @@ void vavilov_v_cannon_tbb::CannonTBB::ShiftBlocks() {
   std::vector<double> b_tmp = B_;
 
   tbb::parallel_invoke(
-       [&] {  // обработка A_
-         tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_),
-                           [&](const tbb::blocked_range2d<int>& r) {
-                             for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
-                               for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
-                                 int src_col = (bj + 1) % num_blocks_;
-                                 for (int i = 0; i < block_size_; ++i) {
-                                   int dst = (bi * block_size_ + i) * N_ + bj * block_size_;
-                                   int src = (bi * block_size_ + i) * N_ + src_col * block_size_;
-                                   std::copy_n(&a_tmp[src], block_size_, &A_[dst]);
-                                 }
-                               }
-                             }
-                           });
-       },
-       [&] {  // обработка B_
-         tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_),
-                           [&](const tbb::blocked_range2d<int>& r) {
-                             for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
-                               for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
-                                 int src_row = (bi + 1) % num_blocks_;
-                                 for (int i = 0; i < block_size_; ++i) {
-                                   int dst = (bi * block_size_ + i) * N_ + bj * block_size_;
-                                   int src = (src_row * block_size_ + i) * N_ + bj * block_size_;
-                                   std::copy_n(&b_tmp[src], block_size_, &B_[dst]);
-                                 }
-                               }
-                             }
-                           });
+      [&] {  // обработка A_
+        tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_),
+                          [&](const tbb::blocked_range2d<int>& r) {
+                            for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
+                              for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
+                                int src_col = (bj + 1) % num_blocks_;
+                                for (int i = 0; i < block_size_; ++i) {
+                                  int dst = (bi * block_size_ + i) * N_ + bj * block_size_;
+                                  int src = (bi * block_size_ + i) * N_ + src_col * block_size_;
+                                  std::copy_n(&a_tmp[src], block_size_, &A_[dst]);
+                                }
+                              }
+                            }
+                          });
+      },
+      [&] {  // обработка B_
+        tbb::parallel_for(tbb::blocked_range2d<int>(0, num_blocks_, 0, num_blocks_),
+                          [&](const tbb::blocked_range2d<int>& r) {
+                            for (int bi = r.rows().begin(); bi != r.rows().end(); ++bi) {
+                              for (int bj = r.cols().begin(); bj != r.cols().end(); ++bj) {
+                                int src_row = (bi + 1) % num_blocks_;
+                                for (int i = 0; i < block_size_; ++i) {
+                                  int dst = (bi * block_size_ + i) * N_ + bj * block_size_;
+                                  int src = (src_row * block_size_ + i) * N_ + bj * block_size_;
+                                  std::copy_n(&b_tmp[src], block_size_, &B_[dst]);
+                                }
+                              }
+                            }
+                          });
       });
 }
 
