5c7f85370dd112ee68d8d08dfd57a47d9a39d843
0xG00SE <dsolo03@mail.ru>
2025-04-15 20:01:47 +0300

    clang-format fix

--- tasks/tbb/solovyev_d_shell_sort_simple/src/ops_tbb.cpp
+++ tasks/tbb/solovyev_d_shell_sort_simple/src/ops_tbb.cpp
@@ -1,10 +1,10 @@
+#include "tbb/solovyev_d_shell_sort_simple/include/ops_tbb.hpp"
+
 #include <oneapi/tbb/parallel_for.h>
 
 #include <cmath>
 #include <cstddef>
 #include <vector>
-
-#include "tbb/solovyev_d_shell_sort_simple/include/ops_tbb.hpp"
 
 bool solovyev_d_shell_sort_simple_tbb::TaskTBB::PreProcessingImpl() {
   unsigned int input_size = task_data->inputs_count[0];
@@ -19,21 +19,22 @@ bool solovyev_d_shell_sort_simple_tbb::TaskTBB::ValidationImpl() {
 }
 
 bool solovyev_d_shell_sort_simple_tbb::TaskTBB::RunImpl() {
-    for (int gap = static_cast<int>(input_.size()) / 2; gap > 0; gap /= 2) {
-      tbb::parallel_for(0, gap, [this, gap](int i) {
-        for (int f = gap + i; f < static_cast<int>(input_.size()); f += gap) {
-          int val = input_[f];
-          int j = f;
-          while (j >= gap && input_[j - gap] > val) {
-            input_[j] = input_[j - gap];
-            j -= gap;
-          }
-          input_[j] = val;
+  for (int gap = static_cast<int>(input_.size()) / 2; gap > 0; gap /= 2) {
+    tbb::parallel_for(0, gap, [this, gap](int i) {
+      for (int f = gap + i; f < static_cast<int>(input_.size()); f += gap) {
+        int val = input_[f];
+        int j = f;
+        while (j >= gap && input_[j - gap] > val) {
+          input_[j] = input_[j - gap];
+          j -= gap;
         }
-      });
-    }
-    return true;
-  }
+        input_[j] = val;
+      }
+    });
+   }
+  return true;
+}
+
 bool solovyev_d_shell_sort_simple_tbb::TaskTBB::PostProcessingImpl() {
   for (size_t i = 0; i < input_.size(); i++) {
     reinterpret_cast<int *>(task_data->outputs[0])[i] = input_[i];
