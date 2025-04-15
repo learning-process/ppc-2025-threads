636eca6baa1a52f4ae891f8f7818493f64146b80
Semyon1104 <129722895+Semyon1104@users.noreply.github.com>
2025-04-15 20:26:56 +0300

    add check in perf

--- tasks/tbb/titov_s_ImageFilter_HorizGaussian3x3/perf_tests/main.cpp
+++ tasks/tbb/titov_s_ImageFilter_HorizGaussian3x3/perf_tests/main.cpp
@@ -10,30 +10,30 @@
 #include "core/task/include/task.hpp"
 #include "tbb/titov_s_ImageFilter_HorizGaussian3x3/include/ops_tbb.hpp"
 
-TEST(titov_s_image_filter_horiz_gaussian3x3_tbb, test_pipeline_run) {
+TEST(titov_s_image_filter_horiz_gaussian3x3_stl, test_pipeline_run) {
   constexpr size_t kWidth = 15000;
   constexpr size_t kHeight = 15000;
-  std::vector<double> input_image(kWidth * kHeight, 0.0);
-  std::vector<double> output_image(kWidth * kHeight, 0.0);
+  std::vector<double> input(kWidth * kHeight, 0.0);
+  std::vector<double> output(kWidth * kHeight, 0.0);
+  std::vector<double> expected(kWidth * kHeight, 0.0);
   std::vector<int> kernel = {1, 2, 1};
 
   for (size_t i = 0; i < kHeight; ++i) {
     for (size_t j = 0; j < kWidth; ++j) {
-      input_image[(i * kWidth) + j] = static_cast<double>(j) / (kWidth - 1) * 255.0;
+      input[i * kWidth + j] = (j % 3 == 0) ? 100.0 : 0.0;
+      expected[i * kWidth + j] = (j % 3 == 0) ? 50.0 : 25.0;
     }
   }
 
-  auto task_data_tbb = std::make_shared<ppc::core::TaskData>();
-  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(input_image.data()));
-  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(kernel.data()));
-  task_data_tbb->inputs_count.emplace_back(input_image.size());
-  task_data_tbb->outputs.emplace_back(reinterpret_cast<uint8_t *>(output_image.data()));
-  task_data_tbb->outputs_count.emplace_back(output_image.size());
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(kernel.data()));
+  task_data_seq->inputs_count.emplace_back(input.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
+  task_data_seq->outputs_count.emplace_back(output.size());
 
-  // Create Task
-  auto test_task_tbb = std::make_shared<titov_s_image_filter_horiz_gaussian3x3_tbb::ImageFilterTBB>(task_data_tbb);
+  auto test_task_stl = std::make_shared<titov_s_image_filter_horiz_gaussian3x3_stl::GaussianFilterSTL>(task_data_seq);
 
-  // Create Perf attributes
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
@@ -43,39 +43,42 @@ TEST(titov_s_image_filter_horiz_gaussian3x3_tbb, test_pipeline_run) {
     return static_cast<double>(duration) * 1e-9;
   };
 
-  // Create and init perf results
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
-
-  // Create Perf analyzer
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_tbb);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_stl);
   perf_analyzer->PipelineRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
+
+  for (size_t i = 0; i < kHeight; ++i) {
+    for (size_t j = 0; j < kWidth; ++j) {
+      ASSERT_NEAR(output[i * kWidth + j], expected[i * kWidth + j], 1e-6);
+    }
+  }
 }
 
-TEST(titov_s_image_filter_horiz_gaussian3x3_tbb, test_task_run) {
+TEST(titov_s_image_filter_horiz_gaussian3x3_stl, test_task_run) {
   constexpr size_t kWidth = 15000;
   constexpr size_t kHeight = 15000;
-  std::vector<double> input_image(kWidth * kHeight, 0.0);
-  std::vector<double> output_image(kWidth * kHeight, 0.0);
+  std::vector<double> input(kWidth * kHeight, 0.0);
+  std::vector<double> output(kWidth * kHeight, 0.0);
+  std::vector<double> expected(kWidth * kHeight, 0.0);
   std::vector<int> kernel = {1, 2, 1};
 
   for (size_t i = 0; i < kHeight; ++i) {
     for (size_t j = 0; j < kWidth; ++j) {
-      input_image[(i * kWidth) + j] = static_cast<double>(j) / (kWidth - 1) * 255.0;
+      input[i * kWidth + j] = (j % 3 == 0) ? 100.0 : 0.0;
+      expected[i * kWidth + j] = (j % 3 == 0) ? 50.0 : 25.0;
     }
   }
 
-  auto task_data_tbb = std::make_shared<ppc::core::TaskData>();
-  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(input_image.data()));
-  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(kernel.data()));
-  task_data_tbb->inputs_count.emplace_back(input_image.size());
-  task_data_tbb->outputs.emplace_back(reinterpret_cast<uint8_t *>(output_image.data()));
-  task_data_tbb->outputs_count.emplace_back(output_image.size());
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(kernel.data()));
+  task_data_seq->inputs_count.emplace_back(input.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
+  task_data_seq->outputs_count.emplace_back(output.size());
 
-  // Create Task
-  auto test_task_tbb = std::make_shared<titov_s_image_filter_horiz_gaussian3x3_tbb::ImageFilterTBB>(task_data_tbb);
+  auto test_task_stl = std::make_shared<titov_s_image_filter_horiz_gaussian3x3_stl::GaussianFilterSTL>(task_data_seq);
 
-  // Create Perf attributes
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
@@ -85,11 +88,14 @@ TEST(titov_s_image_filter_horiz_gaussian3x3_tbb, test_task_run) {
     return static_cast<double>(duration) * 1e-9;
   };
 
-  // Create and init perf results
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
-
-  // Create Perf analyzer
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_tbb);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_stl);
   perf_analyzer->TaskRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
+
+  for (size_t i = 0; i < kHeight; ++i) {
+    for (size_t j = 0; j < kWidth; ++j) {
+      ASSERT_NEAR(output[i * kWidth + j], expected[i * kWidth + j], 1e-6);
+    }
+  }
 }
