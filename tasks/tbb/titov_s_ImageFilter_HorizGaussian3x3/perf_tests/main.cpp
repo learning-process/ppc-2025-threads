a49bfd16b1a89175821b38d8e3ad52708026fbdf
Semyon1104 <129722895+Semyon1104@users.noreply.github.com>
2025-04-15 21:40:04 +0300

    Update main.cpp

--- tasks/tbb/titov_s_ImageFilter_HorizGaussian3x3/perf_tests/main.cpp
+++ tasks/tbb/titov_s_ImageFilter_HorizGaussian3x3/perf_tests/main.cpp
@@ -10,7 +10,7 @@
 #include "core/task/include/task.hpp"
 #include "tbb/titov_s_ImageFilter_HorizGaussian3x3/include/ops_tbb.hpp"
 
-TEST(titov_s_image_filter_horiz_gaussian3x3_stl, test_pipeline_run) {
+TEST(titov_s_image_filter_horiz_gaussian3x3_tbb, test_pipeline_run) {
   constexpr size_t kWidth = 15000;
   constexpr size_t kHeight = 15000;
   std::vector<double> input(kWidth * kHeight, 0.0);
@@ -20,8 +20,8 @@ TEST(titov_s_image_filter_horiz_gaussian3x3_stl, test_pipeline_run) {
 
   for (size_t i = 0; i < kHeight; ++i) {
     for (size_t j = 0; j < kWidth; ++j) {
-      input[i * kWidth + j] = (j % 3 == 0) ? 100.0 : 0.0;
-      expected[i * kWidth + j] = (j % 3 == 0) ? 50.0 : 25.0;
+      input[(i * kWidth) + j] = (j % 3 == 0) ? 100.0 : 0.0;
+      expected[(i * kWidth) + j] = (j % 3 == 0) ? 50.0 : 25.0;
     }
   }
 
@@ -32,7 +32,7 @@ TEST(titov_s_image_filter_horiz_gaussian3x3_stl, test_pipeline_run) {
   task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
   task_data_seq->outputs_count.emplace_back(output.size());
 
-  auto test_task_stl = std::make_shared<titov_s_image_filter_horiz_gaussian3x3_stl::GaussianFilterSTL>(task_data_seq);
+  auto test_task_tbb = std::make_shared<titov_s_image_filter_horiz_gaussian3x3_tbb::ImageFilterTBB>(task_data_tbb);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
@@ -44,18 +44,18 @@ TEST(titov_s_image_filter_horiz_gaussian3x3_stl, test_pipeline_run) {
   };
 
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_stl);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_tbb);
   perf_analyzer->PipelineRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
 
   for (size_t i = 0; i < kHeight; ++i) {
     for (size_t j = 0; j < kWidth; ++j) {
-      ASSERT_NEAR(output[i * kWidth + j], expected[i * kWidth + j], 1e-6);
+      ASSERT_NEAR(output[(i * kWidth) + j], expected[(i * kWidth) + j], 1e-6);
     }
   }
 }
 
-TEST(titov_s_image_filter_horiz_gaussian3x3_stl, test_task_run) {
+TEST(titov_s_image_filter_horiz_gaussian3x3_tbb, test_task_run) {
   constexpr size_t kWidth = 15000;
   constexpr size_t kHeight = 15000;
   std::vector<double> input(kWidth * kHeight, 0.0);
@@ -65,8 +65,8 @@ TEST(titov_s_image_filter_horiz_gaussian3x3_stl, test_task_run) {
 
   for (size_t i = 0; i < kHeight; ++i) {
     for (size_t j = 0; j < kWidth; ++j) {
-      input[i * kWidth + j] = (j % 3 == 0) ? 100.0 : 0.0;
-      expected[i * kWidth + j] = (j % 3 == 0) ? 50.0 : 25.0;
+      input[(i * kWidth) + j] = (j % 3 == 0) ? 100.0 : 0.0;
+      expected[(i * kWidth) + j] = (j % 3 == 0) ? 50.0 : 25.0;
     }
   }
 
@@ -77,7 +77,7 @@ TEST(titov_s_image_filter_horiz_gaussian3x3_stl, test_task_run) {
   task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
   task_data_seq->outputs_count.emplace_back(output.size());
 
-  auto test_task_stl = std::make_shared<titov_s_image_filter_horiz_gaussian3x3_stl::GaussianFilterSTL>(task_data_seq);
+  auto test_task_tbb = std::make_shared<titov_s_image_filter_horiz_gaussian3x3_tbb::ImageFilterTBB>(task_data_tbb);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
@@ -89,13 +89,13 @@ TEST(titov_s_image_filter_horiz_gaussian3x3_stl, test_task_run) {
   };
 
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_stl);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_tbb);
   perf_analyzer->TaskRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
 
   for (size_t i = 0; i < kHeight; ++i) {
     for (size_t j = 0; j < kWidth; ++j) {
-      ASSERT_NEAR(output[i * kWidth + j], expected[i * kWidth + j], 1e-6);
+      ASSERT_NEAR(output[(i * kWidth + j)], expected[(i * kWidth) + j], 1e-6);
     }
   }
 }
