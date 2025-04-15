52d72478619f8221615f11f25c89b7d254ce86fe
IljaSmirnov <ilsmirnov693n@gmail.com>
2025-04-15 16:57:38 +0300

    clang

--- tasks/tbb/smirnov_i_radix_sort_simple_merge/include/ops_tbb.hpp
+++ tasks/tbb/smirnov_i_radix_sort_simple_merge/include/ops_tbb.hpp
@@ -25,6 +25,6 @@ class TestTaskTBB : public ppc::core::Task {
   static void RadixSort(std::vector<int>& mas);
   static std::vector<int> Merge(std::vector<int>& mas1, std::vector<int>& mas2);
   void SortChunk(int i, int size, int nth, int& start, tbb::mutex& mtx_start, tbb::mutex& mtx_firstdq,
-                std::deque<std::vector<int>>& firstdq);
+                 std::deque<std::vector<int>>& firstdq);
 };
 }  // namespace smirnov_i_radix_sort_simple_merge_tbb
