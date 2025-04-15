62128a9e159c2ac79bb2c05618f477892a086bb0
ARBUZNIJ <kovkonst@gmail.com>
2025-04-15 22:51:10 +0300

    review fix

--- tasks/tbb/kovalev_k_radix_sort_batcher_merge/func_tests/main_func.cpp
+++ tasks/tbb/kovalev_k_radix_sort_batcher_merge/func_tests/main_func.cpp
@@ -120,6 +120,90 @@ TEST(kovalev_k_radix_sort_batcher_merge_tbb, Test_5_int) {
   ASSERT_EQ(count_viol, 0);
 }
 
+TEST(kovalev_k_radix_sort_batcher_merge_tbb, Test_13_int) {
+  const unsigned int length = 13;
+  std::vector<long long int> in(length);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<long long int> dis(kMinLl, kMaxLl);
+  std::ranges::generate(in.begin(), in.end(), [&]() { return dis(gen); });
+  std::vector<long long int> out(length);
+  std::shared_ptr<ppc::core::TaskData> task_data_tbb = std::make_shared<ppc::core::TaskData>();
+  task_data_tbb->inputs_count.emplace_back(in.size());
+  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_tbb->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_tbb->outputs_count.emplace_back(out.size());
+  kovalev_k_radix_sort_batcher_merge_tbb::TestTaskTBB test_task_tbb(task_data_tbb);
+  ASSERT_TRUE(test_task_tbb.Validation());
+  test_task_tbb.PreProcessing();
+  test_task_tbb.Run();
+  test_task_tbb.PostProcessing();
+  std::ranges::sort(in.begin(), in.end(), [](long long int a, long long int b) { return a < b; });
+  int count_viol = 0;
+  for (unsigned int i = 0; i < length; i++) {
+    if (out[i] != in[i]) {
+      count_viol++;
+    }
+  }
+  ASSERT_EQ(count_viol, 0);
+}
+
+TEST(kovalev_k_radix_sort_batcher_merge_tbb, Test_139_int) {
+  const unsigned int length = 139;
+  std::vector<long long int> in(length);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<long long int> dis(kMinLl, kMaxLl);
+  std::ranges::generate(in.begin(), in.end(), [&]() { return dis(gen); });
+  std::vector<long long int> out(length);
+  std::shared_ptr<ppc::core::TaskData> task_data_tbb = std::make_shared<ppc::core::TaskData>();
+  task_data_tbb->inputs_count.emplace_back(in.size());
+  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_tbb->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_tbb->outputs_count.emplace_back(out.size());
+  kovalev_k_radix_sort_batcher_merge_tbb::TestTaskTBB test_task_tbb(task_data_tbb);
+  ASSERT_TRUE(test_task_tbb.Validation());
+  test_task_tbb.PreProcessing();
+  test_task_tbb.Run();
+  test_task_tbb.PostProcessing();
+  std::ranges::sort(in.begin(), in.end(), [](long long int a, long long int b) { return a < b; });
+  int count_viol = 0;
+  for (unsigned int i = 0; i < length; i++) {
+    if (out[i] != in[i]) {
+      count_viol++;
+    }
+  }
+  ASSERT_EQ(count_viol, 0);
+}
+
+TEST(kovalev_k_radix_sort_batcher_merge_tbb, Test_449_int) {
+  const unsigned int length = 449;
+  std::vector<long long int> in(length);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<long long int> dis(kMinLl, kMaxLl);
+  std::ranges::generate(in.begin(), in.end(), [&]() { return dis(gen); });
+  std::vector<long long int> out(length);
+  std::shared_ptr<ppc::core::TaskData> task_data_tbb = std::make_shared<ppc::core::TaskData>();
+  task_data_tbb->inputs_count.emplace_back(in.size());
+  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_tbb->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_tbb->outputs_count.emplace_back(out.size());
+  kovalev_k_radix_sort_batcher_merge_tbb::TestTaskTBB test_task_tbb(task_data_tbb);
+  ASSERT_TRUE(test_task_tbb.Validation());
+  test_task_tbb.PreProcessing();
+  test_task_tbb.Run();
+  test_task_tbb.PostProcessing();
+  std::ranges::sort(in.begin(), in.end(), [](long long int a, long long int b) { return a < b; });
+  int count_viol = 0;
+  for (unsigned int i = 0; i < length; i++) {
+    if (out[i] != in[i]) {
+      count_viol++;
+    }
+  }
+  ASSERT_EQ(count_viol, 0);
+}
+
 TEST(kovalev_k_radix_sort_batcher_merge_tbb, Test_793_int) {
   const unsigned int length = 793;
   std::vector<long long int> in(length);
