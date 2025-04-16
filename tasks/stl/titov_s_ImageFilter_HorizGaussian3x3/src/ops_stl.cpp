45cb7c6748be8b0fe036c2283d0668c8e7df6031
Semyon1104 <sema_titov_04@mail.ru>
2025-04-16 17:55:31 +0300

    tidy

--- tasks/stl/titov_s_ImageFilter_HorizGaussian3x3/src/ops_stl.cpp
+++ tasks/stl/titov_s_ImageFilter_HorizGaussian3x3/src/ops_stl.cpp
@@ -1,9 +1,7 @@
 #include "stl/titov_s_ImageFilter_HorizGaussian3x3/include/ops_stl.hpp"
 
-#include <algorithm>
 #include <cmath>
 #include <cstddef>
-#include <numeric>
 #include <thread>
 #include <vector>
 
@@ -31,36 +29,41 @@ bool titov_s_image_filter_horiz_gaussian3x3_stl::GaussianFilterSTL::ValidationIm
   return task_data->inputs_count[0] == task_data->outputs_count[0];
 }
 
-bool titov_s_image_filter_horiz_gaussian3x3_stl::GaussianFilterSTL::RunImpl() {
-  const double sum = kernel_[0] + kernel_[1] + kernel_[2];
-  const int width = width_;
-  const int height = height_;
+const double sum = kernel_[0] + kernel_[1] + kernel_[2];
+const int width = width_;
+const int height = height_;
 
-  const int num_threads = std::thread::hardware_concurrency();
-  std::vector<std::thread> threads;
-  threads.reserve(num_threads);
+const auto num_threads = static_cast<int>(std::thread::hardware_concurrency());
+std::vector<std::thread> threads;
+threads.reserve(num_threads);
 
-  const int rows_per_thread = height / num_threads;
+const int rows_per_thread = height / num_threads;
 
-  for (int t = 0; t < num_threads; ++t) {
-    const int start_row = t * rows_per_thread;
-    const int end_row = (t == num_threads - 1) ? height : start_row + rows_per_thread;
+for (int t = 0; t < num_threads; ++t) {
+  const int start_row = t * rows_per_thread;
+  const int end_row = (t == num_threads - 1) ? height : (start_row + rows_per_thread);
 
-    threads.emplace_back([=, &input = input_, &output = output_, &kernel = kernel_] {
-      for (int i = start_row; i < end_row; ++i) {
-        const int row_offset = i * width;
-        for (int j = 0; j < width; ++j) {
-          double val = input[row_offset + j] * kernel[1];
-          if (j > 0) val += input[row_offset + j - 1] * kernel[0];
-          if (j < width - 1) val += input[row_offset + j + 1] * kernel[2];
-          output[row_offset + j] = val / sum;
+  threads.emplace_back([=, &input = input_, &output = output_, &kernel = kernel_] {
+    for (int i = start_row; i < end_row; ++i) {
+      const int row_offset = i * width;
+      for (int j = 0; j < width; ++j) {
+        double val = input[row_offset + j] * kernel[1];
+        if (j > 0) {
+          val += input[row_offset + j - 1] * kernel[0];
         }
+        if (j < width - 1) {
+          val += input[row_offset + j + 1] * kernel[2];
+        }
+        output[row_offset + j] = val / sum;
       }
-    });
-  }
+    }
+  });
+}
 
-  for (auto &t : threads) t.join();
-  return true;
+for (auto &t : threads) {
+  t.join();
+}
+return true;
 }
 
 bool titov_s_image_filter_horiz_gaussian3x3_stl::GaussianFilterSTL::PostProcessingImpl() {
