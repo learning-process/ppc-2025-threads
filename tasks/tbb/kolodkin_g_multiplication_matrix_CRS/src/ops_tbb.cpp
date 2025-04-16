d558401ab8a454643ab67f969e9ed30eca70f95b
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2025-04-16 12:32:10 +0300

    fix tidy

--- tasks/tbb/kolodkin_g_multiplication_matrix_CRS/src/ops_tbb.cpp
+++ tasks/tbb/kolodkin_g_multiplication_matrix_CRS/src/ops_tbb.cpp
@@ -1,7 +1,7 @@
 #include "tbb/kolodkin_g_multiplication_matrix_CRS/include/ops_tbb.hpp"
 
-#include <tbb/blocked_range.h>
-#include <tbb/parallel_for.h>
+#include <oneapi/tbb/parallel_for.h>
+
 #include <tbb/tbb.h>
 
 #include <cmath>
