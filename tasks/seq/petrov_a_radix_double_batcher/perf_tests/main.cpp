23a6e173a8c27389ece18c4e4908d13e3f323679
NeiroYT <neiro15000000let@gmail.com>
2025-04-17 18:17:44 +0300

    STL

--- tasks/seq/petrov_a_radix_double_batcher/perf_tests/main.cpp
+++ tasks/seq/petrov_a_radix_double_batcher/perf_tests/main.cpp
@@ -1,29 +1,40 @@
-// Copyright 2023 Nesterov Alexander
 #include <gtest/gtest.h>
 
+#include <algorithm>
 #include <chrono>
+#include <cstddef>
 #include <cstdint>
 #include <memory>
+#include <random>
 #include <vector>
 
 #include "core/perf/include/perf.hpp"
 #include "core/task/include/task.hpp"
-#include "seq/leontev_n_average/include/ops_seq.hpp"
+#include "seq/petrov_a_radix_double_batcher/include/ops_seq.hpp"
 
-TEST(leontev_n_average_seq, test_pipeline_run) {
-  const int count = 30000000;
-  // Create data
-  std::vector<int> in(count, 0);
-  std::vector<int> out(1, 0);
-  // Create TaskData
-  std::shared_ptr<ppc::core::TaskData> task_data_seq = std::make_shared<ppc::core::TaskData>();
+namespace {
+std::vector<double> RandomVector(size_t size) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  std::uniform_real_distribution<> dist(-5000, 8000);
+  std::vector<double> vec(size);
+  std::ranges::generate(vec, [&dist, &gen] { return dist(gen); });
+  return vec;
+}
+}  // namespace
+
+TEST(petrov_a_radix_double_batcher_seq, test_pipeline_run) {
+  auto in = RandomVector(25'000'000);
+  std::vector<double> out(in.size());
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
   task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
   task_data_seq->inputs_count.emplace_back(in.size());
   task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
   task_data_seq->outputs_count.emplace_back(out.size());
-  // Create Task
-  auto vec_avg_sequential = std::make_shared<leontev_n_average_seq::VecAvgSequential<int>>(task_data_seq);
-  // Create Perf attributes
+
+  auto task = std::make_shared<petrov_a_radix_double_batcher_seq::TestTaskSequential>(task_data_seq);
+
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
@@ -32,29 +43,25 @@ TEST(leontev_n_average_seq, test_pipeline_run) {
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
     return static_cast<double>(duration) * 1e-9;
   };
-  // Create and init perf results
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
-  // Create Perf analyzer
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(vec_avg_sequential);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task);
   perf_analyzer->PipelineRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
-  ASSERT_EQ(0, out[0]);
+  ASSERT_TRUE(std::ranges::is_sorted(out));
 }
 
-TEST(leontev_n_average_seq, test_task_run) {
-  const int count = 30000000;
-  // Create data
-  std::vector<int> in(count, 0);
-  std::vector<int> out(1, 0);
-  // Create TaskData
-  std::shared_ptr<ppc::core::TaskData> task_data_seq = std::make_shared<ppc::core::TaskData>();
+TEST(petrov_a_radix_double_batcher_seq, test_task_run) {
+  auto in = RandomVector(25'000'000);
+  std::vector<double> out(in.size());
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
   task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
   task_data_seq->inputs_count.emplace_back(in.size());
   task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
   task_data_seq->outputs_count.emplace_back(out.size());
-  // Create Task
-  auto vec_avg_sequential = std::make_shared<leontev_n_average_seq::VecAvgSequential<int>>(task_data_seq);
-  // create perf attrib
+
+  auto task = std::make_shared<petrov_a_radix_double_batcher_seq::TestTaskSequential>(task_data_seq);
+
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
@@ -63,11 +70,9 @@ TEST(leontev_n_average_seq, test_task_run) {
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
     return static_cast<double>(duration) * 1e-9;
   };
-  // Create and init perf results
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
-  // Create Perf analyzer
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(vec_avg_sequential);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task);
   perf_analyzer->TaskRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
-  ASSERT_EQ(0, out[0]);
+  ASSERT_TRUE(std::ranges::is_sorted(out));
 }
