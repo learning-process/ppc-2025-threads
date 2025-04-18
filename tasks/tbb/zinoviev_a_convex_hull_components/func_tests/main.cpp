1e3988afbee440fd3418973f0d06540ad9c0a745
kobych <kobychit1@gmail.com>
2025-04-18 13:30:05 +0300

    New tests in func_tests

--- tasks/tbb/zinoviev_a_convex_hull_components/func_tests/main.cpp
+++ tasks/tbb/zinoviev_a_convex_hull_components/func_tests/main.cpp
@@ -61,3 +61,38 @@ TEST(zinoviev_a_convex_hull_components_tbb, TriangleShape) {
       {.x = 0, .y = 0}, {.x = 2, .y = 2}, {.x = 0, .y = 4}};
   RunAndValidate(input, expected, 5, 5);
 }
+
+TEST(zinoviev_a_convex_hull_components_tbb, SinglePoint) {
+  constexpr int kWidth = 1;
+  constexpr int kHeight = 1;
+  const std::vector<int> input = {1};
+  const std::vector<zinoviev_a_convex_hull_components_tbb::Point> expected = {{.x = 0, .y = 0}};
+  RunAndValidate(input, expected, kWidth, kHeight);
+}
+
+TEST(zinoviev_a_convex_hull_components_tbb, SmallGrid) {
+  constexpr int kWidth = 4;
+  constexpr int kHeight = 4;
+  const std::vector<int> input = {1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1};
+  const std::vector<zinoviev_a_convex_hull_components_tbb::Point> expected = {
+      {.x = 0, .y = 0}, {.x = 3, .y = 0}, {.x = 2, .y = 3}, {.x = 0, .y = 3}};
+  RunAndValidate(input, expected, kWidth, kHeight);
+}
+
+TEST(zinoviev_a_convex_hull_components_tbb, StarShape) {
+  constexpr int kWidth = 5;
+  constexpr int kHeight = 5;
+  const std::vector<int> input = {1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1};
+  const std::vector<zinoviev_a_convex_hull_components_tbb::Point> expected = {
+      {.x = 0, .y = 0}, {.x = 4, .y = 0}, {.x = 3, .y = 3}, {.x = 0, .y = 4}};
+  RunAndValidate(input, expected, kWidth, kHeight);
+}
+
+TEST(zinoviev_a_convex_hull_components_tbb, CircleShape) {
+  constexpr int kWidth = 5;
+  constexpr int kHeight = 5;
+  const std::vector<int> input = {0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0};
+  const std::vector<zinoviev_a_convex_hull_components_tbb::Point> expected = {
+      {.x = 0, .y = 2}, {.x = 2, .y = 0}, {.x = 4, .y = 2}, {.x = 2, .y = 4}};
+  RunAndValidate(input, expected, kWidth, kHeight);
+}
