dbfd03bb6496f0672a4478eb67581b67b057fb54
IljaSmirnov <ilsmirnov693n@gmail.com>
2025-04-19 13:19:01 +0300

    clang

--- tasks/stl/smirnov_i_radix_sort_simple_merge/include/ops_stl.hpp
+++ tasks/stl/smirnov_i_radix_sort_simple_merge/include/ops_stl.hpp
@@ -21,11 +21,11 @@ class TestTaskSTL : public ppc::core::Task {
   bool PostProcessingImpl() override;
 
  private:
- std::vector<int> mas_, output_;
- static void RadixSort(std::vector<int>& mas);
- static std::vector<int> Merge(std::vector<int>& mas1, std::vector<int>& mas2);
- void Merging(std::deque<std::vector<int>> &firstdq, std::deque<std::vector<int>> &seconddq, std::mutex &mtx);
- std::vector<int> Sorting(int id, std::vector<int> &mas_, int max_th);
+  std::vector<int> mas_, output_;
+  static void RadixSort(std::vector<int> &mas);
+  static std::vector<int> Merge(std::vector<int> &mas1, std::vector<int> &mas2);
+  void Merging(std::deque<std::vector<int>> &firstdq, std::deque<std::vector<int>> &seconddq, std::mutex &mtx);
+  std::vector<int> Sorting(int id, std::vector<int> &mas_, int max_th);
 };
 
 }  // namespace smirnov_i_radix_sort_simple_merge_stl
--- tasks/stl/smirnov_i_radix_sort_simple_merge/src/ops_stl.cpp
+++ tasks/stl/smirnov_i_radix_sort_simple_merge/src/ops_stl.cpp
@@ -41,7 +41,7 @@ std::vector<int> smirnov_i_radix_sort_simple_merge_stl::TestTaskSTL::Merge(std::
   }
   return res;
 }
-void smirnov_i_radix_sort_simple_merge_stl::TestTaskSTL::RadixSort(std::vector<int>& mas) {
+void smirnov_i_radix_sort_simple_merge_stl::TestTaskSTL::RadixSort(std::vector<int> &mas) {
   if (mas.empty()) {
     return;
   }
@@ -64,8 +64,7 @@ void smirnov_i_radix_sort_simple_merge_stl::TestTaskSTL::RadixSort(std::vector<i
   }
 }
 std::vector<int> smirnov_i_radix_sort_simple_merge_stl::TestTaskSTL::Sorting(int id, std::vector<int> &mas_,
-                                                                             int max_th)
-{
+                                                                             int max_th) {
   int start = static_cast<int>(id * mas_.size() / max_th);
   int end = static_cast<int>(std::min((id + 1) * mas_.size() / max_th, mas_.size()));
   std::vector<int> local_mas_(end - start);
@@ -75,31 +74,25 @@ std::vector<int> smirnov_i_radix_sort_simple_merge_stl::TestTaskSTL::Sorting(int
 }
 void smirnov_i_radix_sort_simple_merge_stl::TestTaskSTL::Merging(std::deque<std::vector<int>> &firstdq,
                                                                  std::deque<std::vector<int>> &seconddq,
-                                                                 std::mutex &mtx)
-{
+                                                                 std::mutex &mtx) {
   std::vector<int> mas1{};
   std::vector<int> mas2{};
   std::vector<int> merge_mas{};
   mtx.lock();
-  if (static_cast<int>(firstdq.size()) >= 2)
-  {
+  if (static_cast<int>(firstdq.size()) >= 2) {
     mas1 = std::move(firstdq.front());
     firstdq.pop_front();
     mas2 = std::move(firstdq.front());
     firstdq.pop_front();
-  }
-  else
-  {
+  } else {
     mtx.unlock();
     return;
   }
   mtx.unlock();
-  if (!mas1.empty() && !mas2.empty())
-  {
+  if (!mas1.empty() && !mas2.empty()) {
     merge_mas = Merge(mas1, mas2);
   }
-  if (!merge_mas.empty())
-  {
+  if (!merge_mas.empty()) {
     mtx.lock();
     seconddq.push_back(std::move(merge_mas));
     mtx.unlock();
@@ -107,7 +100,7 @@ void smirnov_i_radix_sort_simple_merge_stl::TestTaskSTL::Merging(std::deque<std:
 }
 bool smirnov_i_radix_sort_simple_merge_stl::TestTaskSTL::PreProcessingImpl() {
   unsigned int input_size = task_data->inputs_count[0];
-  auto* in_ptr = reinterpret_cast<int*>(task_data->inputs[0]);
+  auto *in_ptr = reinterpret_cast<int *>(task_data->inputs[0]);
   mas_ = std::vector<int>(in_ptr, in_ptr + input_size);
 
   unsigned int output_size = task_data->outputs_count[0];
@@ -140,18 +133,14 @@ bool smirnov_i_radix_sort_simple_merge_stl::TestTaskSTL::RunImpl() {
   }
   flag = static_cast<int>(firstdq.size()) != 1;
   std::vector<std::thread> threads(max_th);
-  while (flag)
-  {
-    for (int i = 0; i < max_th; i++)
-    {
+  while (flag) {
+    for (int i = 0; i < max_th; i++) {
       threads[i] = std::thread(Merging, std::ref(firstdq), std::ref(seconddq), std::ref(mtx));
     }
-    for (auto &th : threads)
-    {
+    for (auto &th : threads) {
       th.join();
     }
-    if (static_cast<int>(firstdq.size()) == 1)
-    {
+    if (static_cast<int>(firstdq.size()) == 1) {
       seconddq.push_back(std::move(firstdq.front()));
       firstdq.pop_front();
     }
@@ -164,7 +153,7 @@ bool smirnov_i_radix_sort_simple_merge_stl::TestTaskSTL::RunImpl() {
 
 bool smirnov_i_radix_sort_simple_merge_stl::TestTaskSTL::PostProcessingImpl() {
   for (size_t i = 0; i < output_.size(); i++) {
-    reinterpret_cast<int*>(task_data->outputs[0])[i] = output_[i];
+    reinterpret_cast<int *>(task_data->outputs[0])[i] = output_[i];
   }
   return true;
 }
