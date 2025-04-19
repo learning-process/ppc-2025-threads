0b6a7f51dc7de49a487f1e9df6e9a2eb1aa40b2a
Александра Вершинина <xx.vershinina.xx@yandex.ru>
2025-04-20 01:58:02 +0300

    added only negative test

--- tasks/tbb/vershinina_a_hoare_sort_tbb/func_tests/main.cpp
+++ tasks/tbb/vershinina_a_hoare_sort_tbb/func_tests/main.cpp
@@ -179,3 +179,25 @@ TEST(vershinina_a_hoare_sort_tbb, test_random_even_len_333) {
 
   ASSERT_TRUE(std::ranges::is_sorted(out));
 }
+TEST(vershinina_a_hoare_sort_tbb, test_not_random_negative_len_100) {
+  std::vector<double> in{-13, -68,  -8,  -33, -87, -31, -20, -86,  -78, -20, -71, -89, -4,  -44, -94, -21, -80,
+                         -31, -54,  -43, -48, -60, -45, -24, -70,  -70, -88, -82, -41, -73, -77, -35, -54, -81,
+                         -2,  0,    -25, -51, -57, -9,  -34, -57,  -87, -22, -39, -42, -90, -27, -57, -65, -48,
+                         -68, -100, -95, -1,  -46, -16, -32, -100, -48, -49, -34, -91, -68, -72, -95, -19, -51,
+                         -23, -48,  -21, -84, -89, -18, -33, -49,  -14, -80, -21, 0,   -91, -62, -47, -25, -20,
+                         -92, -98,  -17, -2,  -66, -71, -64, -71,  -41, -53, -99, -98, -93, -48, -64};
+  std::vector<double> out(in.size());
+
+  auto task_data_tbb = std::make_shared<ppc::core::TaskData>();
+  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_tbb->inputs_count.emplace_back(in.size());
+  task_data_tbb->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_tbb->outputs_count.emplace_back(out.size());
+
+  vershinina_a_hoare_sort_tbb::TestTaskTBB test_task_omp(task_data_tbb);
+  ASSERT_EQ(test_task_omp.Validation(), true);
+  test_task_omp.PreProcessing();
+  test_task_omp.Run();
+  test_task_omp.PostProcessing();
+  ASSERT_TRUE(std::ranges::is_sorted(out));
+}
