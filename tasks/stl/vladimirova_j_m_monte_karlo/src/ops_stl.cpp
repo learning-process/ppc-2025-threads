6d72b0cfde9b5bd20fffd9022d54851fc54fa79d
VlJulia <129722951+VlJulia@users.noreply.github.com>
2025-04-22 00:42:57 +0300

    fix zero threads

--- tasks/stl/vladimirova_j_m_monte_karlo/src/ops_stl.cpp
+++ tasks/stl/vladimirova_j_m_monte_karlo/src/ops_stl.cpp
@@ -61,7 +61,7 @@ bool vladimirova_j_m_monte_karlo_stl::TestTaskStl::RunImpl() {
   std::vector<std::thread> threads(count_t);
   std::vector<size_t> local_res(count_t, 0);
 
-  size_t dl = accuracy_ / count_t;
+  size_t dl = accuracy_ / (count_t + (size_t)(count_t == 0));
 
   for (size_t i = 1; i < count_t; i++) {
     local_res[i] = dl;
