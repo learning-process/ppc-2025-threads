92550cd6961addb974dbd13b343265650a5dab9a
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2025-04-25 09:10:47 +0300

    Update ops_all.cpp

--- tasks/all/vavilov_v_cannon/src/ops_all.cpp
+++ tasks/all/vavilov_v_cannon/src/ops_all.cpp
@@ -42,8 +42,8 @@ void vavilov_v_cannon_all::CannonALL::InitialShift(std::vector<double>& local_A,
   std::vector<double> tmp_A(block_size_ * block_size_);
   std::vector<double> tmp_B(block_size_ * block_size_);
 
-  world_.sendrecv(local_A.data(), block_size_ * block_size_, mpi::get_mpi_datatype<double>(), a_dest, 0,
-                  tmp_A.data(), block_size_ * block_size_, mpi::get_mpi_datatype<double>(), a_src, 0);
+  world_.sendrecv(local_A.data(), block_size_ * block_size_, mpi::get_mpi_datatype<double>(), a_dest, 0, tmp_A.data(),
+                  block_size_ * block_size_, mpi::get_mpi_datatype<double>(), a_src, 0);
   world_.sendrecv(local_B.data(), block_size_ * block_size_, mpi::get_mpi_datatype<double>(), b_dest, 1, tmp_B.data(),
                   block_size_ * block_size_, mpi::get_mpi_datatype<double>(), b_src, 1);
   local_A = tmp_A;
@@ -79,8 +79,8 @@ void vavilov_v_cannon_all::CannonALL::ShiftBlocks(std::vector<double>& local_A, 
   std::vector<double> tmp_A(block_size_ * block_size_);
   std::vector<double> tmp_B(block_size_ * block_size_);
 
-  world_.sendrecv(local_A.data(), block_size_ * block_size_, mpi::get_mpi_datatype<double>(), left_dest, 2, tmp_A.data(),
-                  block_size_ * block_size_, mpi::DOUBLE, left_src, 2);
+  world_.sendrecv(local_A.data(), block_size_ * block_size_, mpi::get_mpi_datatype<double>(), left_dest, 2,
+                  tmp_A.data(), block_size_ * block_size_, mpi::DOUBLE, left_src, 2);
   world_.sendrecv(local_B.data(), block_size_ * block_size_, mpi::get_mpi_datatype<double>(), up_dest, 3, tmp_B.data(),
                   block_size_ * block_size_, mpi::DOUBLE, up_src, 3);
   local_A = tmp_A;
