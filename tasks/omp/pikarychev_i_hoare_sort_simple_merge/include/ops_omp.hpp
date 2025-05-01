ae521fce368aac263426df87a2c0e2b9905d0603
flashkasi <ilyapikarychev@gmail.com>
2025-05-01 15:51:29 +0300

    fixed

--- tasks/omp/pikarychev_i_hoare_sort_simple_merge/include/ops_omp.hpp
+++ tasks/omp/pikarychev_i_hoare_sort_simple_merge/include/ops_omp.hpp
@@ -62,15 +62,15 @@ class HoareOpenMP : public ppc::core::Task {
     auto partind = parallelism;
     for (auto wide = decltype(parallelism){1}; partind > 1; wide *= 2, partind /= 2) {
 #pragma omp parallel for if (blocks[wide].sz >= 40)
-      for (int k = 0; k < static_cast<int>(partind / 2); ++k) {
+      for (std::size_t k = 0; k < static_cast<std::size_t>(partind / 2); ++k) {
         const auto idx = 2 * wide * k;
-        DoInplaceMerge(&blocks[idx], &blocks[idx + wide], reverse_ ? ReverseComp : StandardComp);
+        DoInplaceMerge(blocks.data() + idx, blocks.data() + idx + wide, reverse_ ? ReverseComp : StandardComp);
       }
       if ((partind / 2) == 1) {
         DoInplaceMerge(&blocks.front(), &blocks.back(), reverse_ ? ReverseComp : StandardComp);
-      } else if ((partind / 2) & 2) {
-        DoInplaceMerge(&blocks[2 * wide * (partind / 2 - 2)], &blocks[2 * wide * (partind / 2 - 1)],
-                       reverse_ ? ReverseComp : StandardComp);
+      } else if ((partind / 2) % 2 != 0) {
+        DoInplaceMerge(blocks.data() + (2 * wide * ((partind / 2) - 2)),
+                       blocks.data() + (2 * wide * ((partind / 2) - 1)), reverse_ ? ReverseComp : StandardComp);
       }
     }
 
