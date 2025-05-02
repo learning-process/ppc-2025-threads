f6062232f57bbbc419eac1c021bc46a7c0673519
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2025-05-03 02:23:30 +0300

    Update ops_all.hpp

--- tasks/all/vavilov_v_cannon/include/ops_all.hpp
+++ tasks/all/vavilov_v_cannon/include/ops_all.hpp
@@ -28,9 +28,10 @@ class CannonALL : public ppc::core::Task {
   std::vector<double> C_;
   boost::mpi::communicator world_;
 
-  // void InitialShift(std::vector<double>& local_A, std::vector<double>& local_B);
+  void InitialShift(std::vector<double>& local_A, std::vector<double>& local_B);
   void BlockMultiply(const std::vector<double>& local_A, const std::vector<double>& local_B,
                      std::vector<double>& local_C);
-  // void ShiftBlocks(std::vector<double>& local_A, std::vector<double>& local_B);
+  void ShiftBlocks(std::vector<double>& local_A, std::vector<double>& local_B);
+  void CalculateGridDimensions(int size, int& rows, int& cols);
 };
 }  // namespace vavilov_v_cannon_all
