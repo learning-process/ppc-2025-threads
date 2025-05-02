959fbe8afba182c34a58aaf69c04700c48962ac1
Danie <dxppi@vk.com>
2025-05-02 16:24:59 +0300

    fix

--- tasks/all/plekhanov_d_dijkstra/src/ops_all.cpp
+++ tasks/all/plekhanov_d_dijkstra/src/ops_all.cpp
@@ -3,8 +3,11 @@
 #include <omp.h>
 
 #include <algorithm>
+#include <boost/mpi.hpp>
 #include <boost/mpi/collectives.hpp>
 #include <boost/mpi/communicator.hpp>
+#include <boost/serialization/utility.hpp>
+#include <boost/serialization/vector.hpp>
 #include <climits>
 #include <cstddef>
 #include <cstdlib>
