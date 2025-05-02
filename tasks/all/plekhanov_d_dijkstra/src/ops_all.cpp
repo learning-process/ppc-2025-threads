349e86ebb3f1f5fd3797047a84f73e86911b4ef3
Danie <dxppi@vk.com>
2025-05-02 14:59:56 +0300

    first commit

---
+++ tasks/all/plekhanov_d_dijkstra/src/ops_all.cpp
@@ -0,0 +1,143 @@
+#include "all/plekhanov_d_dijkstra/include/ops_all.hpp"
+
+#include <boost/mpi.hpp>
+#include <boost/serialization/vector.hpp>
+#include <omp.h>
+
+#include <atomic>
+#include <climits>
+#include <cstddef>
+#include <cstdlib>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <utility>
+#include <vector>
+
+#include "core/util/include/util.hpp"
+
+namespace plekhanov_d_dijkstra_all {
+
+namespace {
+
+bool ConvertGraphToAdjacencyList(const std::vector<int>& graph_data, size_t num_vertices,
+                                 std::vector<std::vector<std::pair<int, int>>>& graph) {
+  graph.assign(num_vertices, {});
+  size_t current_vertex = 0;
+  size_t i = 0;
+  while (i < graph_data.size() && current_vertex < num_vertices) {
+    if (graph_data[i] == -1) {
+      current_vertex++;
+      i++;
+      continue;
+    }
+    if (i + 1 >= graph_data.size()) {
+      break;
+    }
+    size_t dest = graph_data[i];
+    int weight = graph_data[i + 1];
+    if (weight < 0) {
+      return false;
+    }
+    if (dest < num_vertices) {
+      graph[current_vertex].emplace_back(static_cast<int>(dest), weight);
+    }
+    i += 2;
+  }
+  return true;
+}
+
+}  // namespace
+
+}  // namespace plekhanov_d_dijkstra_all
+
+const int plekhanov_d_dijkstra_all::TestTaskALL::kEndOfVertexList = -1;
+
+bool plekhanov_d_dijkstra_all::TestTaskALL::PreProcessingImpl() {
+  unsigned int input_size = task_data->inputs_count[0];
+  auto* in_ptr = reinterpret_cast<int*>(task_data->inputs[0]);
+  graph_data_.assign(in_ptr, in_ptr + input_size);
+  num_vertices_ = task_data->outputs_count[0];
+  distances_.assign(num_vertices_, INT_MAX);
+
+  if (task_data->inputs.size() > 1 && task_data->inputs[1] != nullptr) {
+    start_vertex_ = *reinterpret_cast<int*>(task_data->inputs[1]);
+  } else {
+    start_vertex_ = 0;
+  }
+  distances_[start_vertex_] = 0;
+  return true;
+}
+
+bool plekhanov_d_dijkstra_all::TestTaskALL::ValidationImpl() {
+  return !task_data->inputs_count.empty() && task_data->inputs_count[0] > 0 && !task_data->outputs_count.empty() &&
+         task_data->outputs_count[0] > 0;
+}
+
+bool plekhanov_d_dijkstra_all::TestTaskALL::RunImpl() {;
+  boost::mpi::communicator world;
+
+  std::vector<std::vector<std::pair<int, int>>> graph;
+  if (!ConvertGraphToAdjacencyList(graph_data_, num_vertices_, graph)) {
+    return false;
+  }
+
+  std::vector<int> local_distances(num_vertices_, INT_MAX);
+  local_distances[start_vertex_] = 0;
+
+  std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> pq;
+  pq.emplace(0, start_vertex_);
+  std::mutex pq_mutex;
+
+#pragma omp parallel
+  {
+    while (true) {
+      int u = -1;
+      int cur_dist = -1;
+
+      {
+        std::lock_guard<std::mutex> lock(pq_mutex);
+        if (!pq.empty()) {
+          auto top = pq.top();
+          pq.pop();
+          u = top.second;
+          cur_dist = top.first;
+        } else {
+          break;
+        }
+      }
+
+      for (const auto& [v, weight] : graph[u]) {
+        int new_dist = cur_dist + weight;
+        if (new_dist < local_distances[v]) {
+          local_distances[v] = new_dist;
+          std::lock_guard<std::mutex> lock(pq_mutex);
+          pq.emplace(new_dist, v);
+        }
+      }
+    }
+  }
+
+  std::vector<std::vector<int>> gathered;
+  boost::mpi::gather(world, local_distances, gathered, 0);
+
+  if (world.rank() == 0) {
+    distances_.assign(num_vertices_, INT_MAX);
+    for (int i = 0; i < num_vertices_; ++i) {
+      for (const auto& proc_dists : gathered) {
+        distances_[i] = std::min(distances_[i], proc_dists[i]);
+      }
+    }
+  }
+
+  boost::mpi::broadcast(world, distances_, 0);
+  return true;
+}
+
+bool plekhanov_d_dijkstra_all::TestTaskALL::PostProcessingImpl() {
+  auto* output = reinterpret_cast<int*>(task_data->outputs[0]);
+  for (size_t i = 0; i < distances_.size(); ++i) {
+    output[i] = distances_[i];
+  }
+  return true;
+}
