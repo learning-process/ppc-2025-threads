14d0b2fc756ea54293e8e01bdd2d170b9b8cdbab
Vlad <laganinaelena@gmail.com>
2025-05-03 10:31:30 +0300

    f_ppc

--- tasks/stl/laganina_e_component_labeling/src/ops_stl.cpp
+++ tasks/stl/laganina_e_component_labeling/src/ops_stl.cpp
@@ -70,10 +70,10 @@ bool laganina_e_component_labeling_stl::TestTaskSTL::PostProcessingImpl() {
 
 void laganina_e_component_labeling_stl::TestTaskSTL::InitializeParents(std::vector<int>& parent) {
   const int size = m_ * n_;
-  int num_threads = ppc::util::GetPPCNumThreads();
-  if (num_threads >= 6) {
-    num_threads = 5;
-  }
+  int num_threads = std::thread::hardware_concurrency();
+  // if (num_threads >= 6) {
+  //   num_threads = 5;
+  // }
   const int chunk_size = (size + num_threads - 1) / num_threads;
 
   std::vector<std::thread> threads;
@@ -95,10 +95,10 @@ void laganina_e_component_labeling_stl::TestTaskSTL::InitializeParents(std::vect
 
 void laganina_e_component_labeling_stl::TestTaskSTL::ProcessSweep(bool reverse, std::vector<int>& parent,
                                                                   bool& changed) const {
-  int num_threads = ppc::util::GetPPCNumThreads();
-  if (num_threads >= 6) {
-    num_threads = 5;
-  }
+  int num_threads = std::thread::hardware_concurrency();
+  // if (num_threads >= 6) {
+  //   num_threads = 5;
+  // }
   const int chunk_size = (m_ + num_threads - 1) / num_threads;
   std::atomic<bool> global_changed(false);
 
@@ -190,10 +190,10 @@ bool laganina_e_component_labeling_stl::TestTaskSTL::UnionNodes(int a, int b, st
 
 void laganina_e_component_labeling_stl::TestTaskSTL::FinalizeRoots(std::vector<int>& parent) const {
   const int size = m_ * n_;
-  int num_threads = ppc::util::GetPPCNumThreads();
-  if (num_threads >= 6) {
-    num_threads = 5;
-  }
+  int num_threads = std::thread::hardware_concurrency();
+  // if (num_threads >= 6) {
+  //   num_threads = 5;
+  // }
   const int chunk_size = (size + num_threads - 1) / num_threads;
 
   std::vector<std::thread> threads;
@@ -217,10 +217,10 @@ void laganina_e_component_labeling_stl::TestTaskSTL::FinalizeRoots(std::vector<i
 
 void laganina_e_component_labeling_stl::TestTaskSTL::AssignLabels(std::vector<int>& parent) {
   const int size = m_ * n_;
-  int num_threads = ppc::util::GetPPCNumThreads();
-  if (num_threads >= 6) {
-    num_threads = 5;
-  }
+  int num_threads = std::thread::hardware_concurrency();
+  // if (num_threads >= 6) {
+  //   num_threads = 5;
+  // }
   std::vector<int> labels(size + 1, 0);
   const int chunk_size = (size + num_threads - 1) / num_threads;
 
