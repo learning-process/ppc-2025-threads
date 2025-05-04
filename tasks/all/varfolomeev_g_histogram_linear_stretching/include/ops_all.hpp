f3b1e7d17e2e1e7b75a22e98f9c12dda1333afdc
thunderbird <greg.varfolomeev@yandex.ru>
2025-05-04 17:30:46 +0300

    added utility

--- tasks/all/varfolomeev_g_histogram_linear_stretching/include/ops_all.hpp
+++ tasks/all/varfolomeev_g_histogram_linear_stretching/include/ops_all.hpp
@@ -3,7 +3,7 @@
 #include <boost/mpi/collectives.hpp>
 #include <boost/mpi/communicator.hpp>
 #include <cstdint>
-#include <memory>
+#include <utility>
 #include <vector>
 
 #include "core/task/include/task.hpp"
