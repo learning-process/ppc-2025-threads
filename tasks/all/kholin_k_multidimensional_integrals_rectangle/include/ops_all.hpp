d07f31f61fe5413e669ea406815144eddfb9b529
Kirius257 <holin.kirill@yandex.ru>
2025-05-04 19:19:25 +0300

    delete mpi.h from ops_all.hpp because unused

--- tasks/all/kholin_k_multidimensional_integrals_rectangle/include/ops_all.hpp
+++ tasks/all/kholin_k_multidimensional_integrals_rectangle/include/ops_all.hpp
@@ -1,16 +1,5 @@
 #pragma once
 
-#ifdef __clang__
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wcast-align"
-#endif
-
-#include <mpi.h>
-
-#ifdef __clang__
-#pragma clang diagnostic pop
-#endif
-
 #include <boost/mpi/collectives.hpp>
 #include <functional>
 #include <utility>
