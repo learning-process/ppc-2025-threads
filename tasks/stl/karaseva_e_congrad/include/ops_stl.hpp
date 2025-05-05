e117092dae1defba65e0f686a10ba8de4b0dd197
Katerina Karaseva <k.karaseva2010@mail.ru>
2025-05-05 12:11:27 +0300

    stl realization

---
+++ tasks/stl/karaseva_e_congrad/include/ops_stl.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace karaseva_a_test_task_stl {
+
+class TestTaskSTL : public ppc::core::Task {
+ public:
+  explicit TestTaskSTL(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
+  bool PreProcessingImpl() override;
+  bool ValidationImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+ private:
+  std::vector<double> A_;  // Coefficient matrix
+  std::vector<double> b_;  // Right-hand side vector
+  std::vector<double> x_;  // Solution vector
+  size_t size_{};
+};
+
+}  // namespace karaseva_a_test_task_stl
