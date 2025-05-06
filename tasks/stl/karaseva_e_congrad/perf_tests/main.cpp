520f4bbd11f5a34dd8fdc269cbe098f1ff209b8f
Katerina Karaseva <k.karaseva2010@mail.ru>
2025-05-06 20:01:07 +0300

    perf tests

--- tasks/stl/karaseva_e_congrad/perf_tests/main.cpp
+++ tasks/stl/karaseva_e_congrad/perf_tests/main.cpp
@@ -10,32 +10,35 @@
 #include "core/task/include/task.hpp"
 #include "stl/karaseva_e_congrad/include/ops_stl.hpp"
 
-TEST(karaseva_a_test_task_stl, test_pipeline_run) {
-  constexpr int kCount = 10000;
+TEST(karaseva_e_congrad_stl, test_pipeline_run) {
+  constexpr int kSize = 10000;
 
   // Create data
-  std::vector<int> in(kCount * kCount, 0);
-  std::vector<int> out(kCount * kCount, 0);
+  std::vector<double> a(kSize * kSize, 0.0);
+  std::vector<double> b(kSize, 1.0);
+  std::vector<double> x(kSize, 0.0);
 
-  for (size_t i = 0; i < kCount; i++) {
-    in[(i * kCount) + i] = 1;
+  for (int i = 0; i < kSize; ++i) {
+    a[(i * kSize) + i] = 1.0;
   }
 
   // Create task_data
-  auto task_data_stl = std::make_shared<ppc::core::TaskData>();
-  task_data_stl->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  task_data_stl->inputs_count.emplace_back(in.size());
-  task_data_stl->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  task_data_stl->outputs_count.emplace_back(out.size());
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(a.data()));
+  task_data->inputs_count.emplace_back(a.size());
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(b.data()));
+  task_data->inputs_count.emplace_back(b.size());
+  task_data->outputs.emplace_back(reinterpret_cast<uint8_t*>(x.data()));
+  task_data->outputs_count.emplace_back(x.size());
 
   // Create Task
-  auto test_task_sequential = std::make_shared<karaseva_a_test_task_stl::TestTaskSTL>(task_data_stl);
+  auto test_task_stl = std::make_shared<karaseva_a_test_task_stl::TestTaskSTL>(task_data);
 
   // Create Perf attributes
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
-  perf_attr->current_timer = [&] {
+  perf_attr->current_timer = [t0]() {
     auto current_time_point = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
     return static_cast<double>(duration) * 1e-9;
@@ -45,38 +48,42 @@ TEST(karaseva_a_test_task_stl, test_pipeline_run) {
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
   // Create Perf analyzer
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_sequential);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_stl);
   perf_analyzer->PipelineRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
-  ASSERT_EQ(in, out);
+
+  ASSERT_EQ(b, x);
 }
 
-TEST(karaseva_a_test_task_stl, test_task_run) {
-  constexpr int kCount = 10000;
+TEST(karaseva_e_congrad_stl, test_task_run) {
+  constexpr int kSize = 10000;
 
   // Create data
-  std::vector<int> in(kCount * kCount, 0);
-  std::vector<int> out(kCount * kCount, 0);
+  std::vector<double> a(kSize * kSize, 0.0);
+  std::vector<double> b(kSize, 1.0);
+  std::vector<double> x(kSize, 0.0);
 
-  for (size_t i = 0; i < kCount; i++) {
-    in[(i * kCount) + i] = 1;
+  for (int i = 0; i < kSize; ++i) {
+    a[(i * kSize) + i] = 1.0;
   }
 
   // Create task_data
-  auto task_data_stl = std::make_shared<ppc::core::TaskData>();
-  task_data_stl->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  task_data_stl->inputs_count.emplace_back(in.size());
-  task_data_stl->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  task_data_stl->outputs_count.emplace_back(out.size());
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(a.data()));
+  task_data->inputs_count.emplace_back(a.size());
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(b.data()));
+  task_data->inputs_count.emplace_back(b.size());
+  task_data->outputs.emplace_back(reinterpret_cast<uint8_t*>(x.data()));
+  task_data->outputs_count.emplace_back(x.size());
 
   // Create Task
-  auto test_task_sequential = std::make_shared<karaseva_a_test_task_stl::TestTaskSTL>(task_data_stl);
+  auto test_task_stl = std::make_shared<karaseva_a_test_task_stl::TestTaskSTL>(task_data);
 
   // Create Perf attributes
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
-  perf_attr->current_timer = [&] {
+  perf_attr->current_timer = [t0]() {
     auto current_time_point = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
     return static_cast<double>(duration) * 1e-9;
@@ -86,8 +93,9 @@ TEST(karaseva_a_test_task_stl, test_task_run) {
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
   // Create Perf analyzer
-  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_sequential);
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_stl);
   perf_analyzer->TaskRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
-  ASSERT_EQ(in, out);
+
+  ASSERT_EQ(b, x);
 }
