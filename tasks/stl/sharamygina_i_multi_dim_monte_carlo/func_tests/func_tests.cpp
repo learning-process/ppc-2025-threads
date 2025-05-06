c281f0ae6e75aa7183fbcdffaa12ac88155eaa5c
isharamygina <121282432+isharamygina@users.noreply.github.com>
2025-05-06 17:44:12 +0300

    Add files via upload

--- tasks/stl/sharamygina_i_multi_dim_monte_carlo/func_tests/func_tests.cpp
+++ tasks/stl/sharamygina_i_multi_dim_monte_carlo/func_tests/func_tests.cpp
@@ -230,6 +230,70 @@ TEST(sharamygina_i_multi_dim_monte_carlo_stl, 3DFunction) {
   EXPECT_NEAR(result, expected, tol);
 }
 
+TEST(sharamygina_i_multi_dim_monte_carlo_stl, 3DFunction5k) {
+  int iterations = 5000;
+  std::vector<double> boundaries = GetBoundaries(0.0, 3.0, 3);
+  auto test_function = [](const std::vector<double>& values) {
+    assert(values.size() == 3);
+    return 1.0;
+  };
+  std::function<double(const std::vector<double>&)> function_ptr = test_function;
+  std::shared_ptr<ppc::core::TaskData> task_data = std::make_shared<ppc::core::TaskData>();
+
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(boundaries.data()));
+  task_data->inputs_count.emplace_back(boundaries.size());
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(&iterations));
+  task_data->inputs_count.emplace_back(1);
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(&function_ptr));
+  task_data->inputs_count.emplace_back(1);
+
+  double result = 0.0;
+  task_data->outputs.emplace_back(reinterpret_cast<uint8_t*>(&result));
+  task_data->outputs_count.emplace_back(1);
+
+  sharamygina_i_multi_dim_monte_carlo_stl::MultiDimMonteCarloTask test_task(task_data);
+  ASSERT_TRUE(test_task.ValidationImpl());
+  ASSERT_TRUE(test_task.PreProcessingImpl());
+  ASSERT_TRUE(test_task.RunImpl());
+  ASSERT_TRUE(test_task.PostProcessingImpl());
+
+  double expected = 27;
+  double tol = 0.07 * expected;
+  EXPECT_NEAR(result, expected, tol);
+}
+
+TEST(sharamygina_i_multi_dim_monte_carlo_stl, 4DFunction5k) {
+  int iterations = 5000;
+  std::vector<double> boundaries = GetBoundaries(-1.0, 5.0, 4);
+  auto test_function = [](const std::vector<double>& values) {
+    assert(values.size() == 3);
+    return (values[0] * values[3]) + values[2] - (0.3 * values[1]);
+  };
+  std::function<double(const std::vector<double>&)> function_ptr = test_function;
+  std::shared_ptr<ppc::core::TaskData> task_data = std::make_shared<ppc::core::TaskData>();
+
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(boundaries.data()));
+  task_data->inputs_count.emplace_back(boundaries.size());
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(&iterations));
+  task_data->inputs_count.emplace_back(1);
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(&function_ptr));
+  task_data->inputs_count.emplace_back(1);
+
+  double result = 0.0;
+  task_data->outputs.emplace_back(reinterpret_cast<uint8_t*>(&result));
+  task_data->outputs_count.emplace_back(1);
+
+  sharamygina_i_multi_dim_monte_carlo_stl::MultiDimMonteCarloTask test_task(task_data);
+  ASSERT_TRUE(test_task.ValidationImpl());
+  ASSERT_TRUE(test_task.PreProcessingImpl());
+  ASSERT_TRUE(test_task.RunImpl());
+  ASSERT_TRUE(test_task.PostProcessingImpl());
+
+  double expected = 6998.4;
+  double tol = 0.07 * expected;
+  EXPECT_NEAR(result, expected, tol);
+}
+
 TEST(sharamygina_i_multi_dim_monte_carlo_stl, 4DFunction) {
   int iterations = 30000;
   std::vector<double> boundaries = GetBoundaries(-1.0, 5.0, 4);
