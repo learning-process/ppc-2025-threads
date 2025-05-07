ccee7442baf46686109721916707016c5e9c12d5
MoiseevArt <amoiseev_00@mail.ru>
2025-05-07 14:03:00 +0000

    1.2

--- tasks/all/moiseev_a_mult_mat/src/ops_mpi.cpp
+++ tasks/all/moiseev_a_mult_mat/src/ops_mpi.cpp
@@ -1,8 +1,12 @@
 #include "all/moiseev_a_mult_mat/include/ops_mpi.hpp"
 
-#include <mpi.h>
-
 #include <algorithm>
+#include <boost/mpi/collectives/broadcast.hpp>
+#include <boost/mpi/collectives/gather.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <boost/mpi/environment.hpp>
+#include <boost/mpi/nonblocking.hpp>
+#include <boost/serialization/vector.hpp>
 #include <cmath>
 #include <vector>
 
@@ -34,120 +38,109 @@ bool moiseev_a_mult_mat_mpi::MultMatMPI::ValidationImpl() {
 }
 
 bool moiseev_a_mult_mat_mpi::MultMatMPI::RunImpl() {  // NOLINT
-  int world_size = 0;
-  int world_rank = 0;
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-  int grid_dim = static_cast<int>(std::sqrt(world_size));
-  while (grid_dim > 1 && (matrix_size_ % grid_dim != 0 || grid_dim * grid_dim > world_size)) {
-    grid_dim--;
+  boost::mpi::communicator world;
+  int world_size = world.size();
+  int world_rank = world.rank();
+
+  int p = static_cast<int>(std::sqrt(world_size));
+  while (p > 1 && (matrix_size_ % p != 0 || (p * p) > world_size)) {
+    p--;
   }
-  grid_dim = std::max(grid_dim, 1);
-  const int active_procs = grid_dim * grid_dim;
-  const int p = grid_dim;
-  const int block_size = matrix_size_ / p;
+  p = std::max(p, 1);
+  int active_procs = p * p;
+  int block = matrix_size_ / p;
 
-  MPI_Comm active_comm{};
-  int color = (world_rank < active_procs ? 0 : MPI_UNDEFINED);
-  MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &active_comm);
-
-  bool is_active = (active_comm != MPI_COMM_NULL);
-  MPI_Comm row_comm{};
+  boost::mpi::communicator active = world.split(world_rank < active_procs ? 0 : MPI_UNDEFINED, world_rank);
+  bool is_active = (world_rank < active_procs);
 
   if (is_active) {
     int my_row = world_rank / p;
     int my_col = world_rank % p;
 
-    std::vector<double> a_block(block_size * block_size);
-    std::vector<double> b_block(block_size * block_size);
-    std::vector<double> c_block(block_size * block_size, 0.0);
+    std::vector<double> a_block(block * block);
+    std::vector<double> b_block(block * block);
+    std::vector<double> c_block(block * block, 0.0);
 
     if (world_rank == 0) {
-      for (int proc = 0; proc < active_procs; proc++) {
+      for (int proc = 0; proc < active_procs; ++proc) {
         int row = proc / p;
         int col = proc % p;
-        std::vector<double> tmp(block_size * block_size);
-        for (int i = 0; i < block_size; i++) {
-          int src = ((row * block_size + i) * matrix_size_) + (col * block_size);
-          std::copy_n(&matrix_a_[src], block_size, &tmp[i * block_size]);
+        std::vector<double> tmp(block * block);
+        for (int i = 0; i < block; ++i) {
+          int src = ((row * block + i) * matrix_size_) + (col * block);
+          std::copy_n(&matrix_a_[src], block, &tmp[i * block]);
         }
         if (proc == 0) {
           a_block = tmp;
         } else {
-          MPI_Send(tmp.data(), static_cast<int>(tmp.size()), MPI_DOUBLE, proc, 0, MPI_COMM_WORLD);
+          world.send(proc, 0, tmp);
         }
       }
-      for (int proc = 0; proc < active_procs; proc++) {
+      for (int proc = 0; proc < active_procs; ++proc) {
         int row = proc / p;
         int col = proc % p;
-        std::vector<double> tmp(block_size * block_size);
-        for (int i = 0; i < block_size; i++) {
-          int src = ((row * block_size + i) * matrix_size_) + (col * block_size);
-          std::copy_n(&matrix_b_[src], block_size, &tmp[i * block_size]);
+        std::vector<double> tmp(block * block);
+        for (int i = 0; i < block; ++i) {
+          int src = ((row * block + i) * matrix_size_) + (col * block);
+          std::copy_n(&matrix_b_[src], block, &tmp[i * block]);
         }
         if (proc == 0) {
           b_block = tmp;
         } else {
-          MPI_Send(tmp.data(), static_cast<int>(tmp.size()), MPI_DOUBLE, proc, 1, MPI_COMM_WORLD);
+          world.send(proc, 1, tmp);
         }
       }
     } else {
-      MPI_Recv(a_block.data(), static_cast<int>(a_block.size()), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      MPI_Recv(b_block.data(), static_cast<int>(b_block.size()), MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      world.recv(0, 0, a_block);
+      world.recv(0, 1, b_block);
     }
 
-    std::vector<double> orig_a_block = a_block;
+    auto orig_a = a_block;
+    boost::mpi::communicator row_comm = active.split(my_row, my_col);
 
-    MPI_Comm_split(active_comm, my_row, my_col, &row_comm);
-
-    for (int step = 0; step < p; step++) {
-      a_block = orig_a_block;
+    for (int step = 0; step < p; ++step) {
+      a_block = orig_a;
       int root = (my_row + step) % p;
-      MPI_Bcast(a_block.data(), static_cast<int>(a_block.size()), MPI_DOUBLE, root, row_comm);
+      broadcast(row_comm, a_block, root);
 
 #pragma omp parallel for
-      for (int i = 0; i < block_size; i++) {
-        for (int j = 0; j < block_size; j++) {
+      for (int i = 0; i < block; ++i) {
+        for (int j = 0; j < block; ++j) {
           double sum = 0;
-          for (int k = 0; k < block_size; k++) {
-            sum += a_block[(i * block_size) + k] * b_block[(k * block_size) + j];
+          for (int k = 0; k < block; ++k) {
+            sum += a_block[(i * block) + k] * b_block[(k * block) + j];
           }
-          c_block[(i * block_size) + j] += sum;
+          c_block[(i * block) + j] += sum;
         }
       }
 
-      int next = (my_row + 1) % p;
       int prev = (my_row - 1 + p) % p;
-      MPI_Sendrecv_replace(b_block.data(), static_cast<int>(b_block.size()), MPI_DOUBLE, (prev * p) + my_col, 0,
-                           (next * p) + my_col, 0, active_comm, MPI_STATUS_IGNORE);
+      int next = (my_row + 1) % p;
+      active.sendrecv((prev * p) + my_col, 2, b_block, (next * p) + my_col, 2, b_block);
     }
 
     if (world_rank == 0) {
-      for (int i = 0; i < block_size; i++) {
-        int dst = ((i + my_row * block_size) * matrix_size_) + (my_col * block_size);
-        std::copy_n(&c_block[i * block_size], block_size, &matrix_c_[dst]);
+      for (int i = 0; i < block; ++i) {
+        int dst = ((i + my_row * block) * matrix_size_) + (my_col * block);
+        std::copy_n(&c_block[i * block], block, &matrix_c_[dst]);
       }
-      for (int proc = 1; proc < active_procs; proc++) {
-        std::vector<double> tmp(block_size * block_size);
-        MPI_Recv(tmp.data(), static_cast<int>(tmp.size()), MPI_DOUBLE, proc, 0, active_comm, MPI_STATUS_IGNORE);
+      for (int proc = 1; proc < active_procs; ++proc) {
+        std::vector<double> tmp(block * block);
+        world.recv(proc, 3, tmp);
         int row = proc / p;
         int col = proc % p;
-        for (int i = 0; i < block_size; i++) {
-          int dst = ((i + row * block_size) * matrix_size_) + (col * block_size);
-          std::copy_n(&tmp[i * block_size], block_size, &matrix_c_[dst]);
+        for (int i = 0; i < block; ++i) {
+          int dst = ((i + row * block) * matrix_size_) + (col * block);
+          std::copy_n(&tmp[i * block], block, &matrix_c_[dst]);
         }
       }
     } else {
-      MPI_Send(c_block.data(), static_cast<int>(c_block.size()), MPI_DOUBLE, 0, 0, active_comm);
+      world.send(0, 3, c_block);
     }
-
-    MPI_Comm_free(&row_comm);
-    MPI_Comm_free(&active_comm);
   }
 
-  MPI_Bcast(matrix_c_.data(), matrix_size_ * matrix_size_, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
+  broadcast(world, matrix_c_, 0);
   return true;
 }
 
