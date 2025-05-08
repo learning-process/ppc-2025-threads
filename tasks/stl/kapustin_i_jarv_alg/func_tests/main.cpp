1b395f9d78f56b6037693f9f153c4e48431f6aea
Ivan <ivankapustin60@gmail.com>
2025-05-08 12:01:57 +0300

    fix again

--- tasks/stl/kapustin_i_jarv_alg/func_tests/main.cpp
+++ tasks/stl/kapustin_i_jarv_alg/func_tests/main.cpp
@@ -237,3 +237,25 @@ TEST(KapustinJarvAlgSTLTest, Star4Points) {
     EXPECT_EQ(expected_result[i], output_result[i]);
   }
 }
+
+TEST(KapustinJarvAlgSTLTest, Circle) {
+  std::vector<std::pair<int, int>> input_points = {{1, 2}, {2, 1}, {2, 3}, {3, 2}};
+  std::vector<std::pair<int, int>> expected_result = {{1, 2}, {2, 1}, {3, 2}, {2, 3}};
+  std::vector<std::pair<int, int>> output_result(expected_result.size());
+
+  auto task_data_stl = std::make_shared<ppc::core::TaskData>();
+  task_data_stl->inputs.emplace_back(reinterpret_cast<uint8_t *>(input_points.data()));
+  task_data_stl->inputs_count.emplace_back(input_points.size());
+  task_data_stl->outputs.emplace_back(reinterpret_cast<uint8_t *>(output_result.data()));
+  task_data_stl->outputs_count.emplace_back(output_result.size());
+
+  kapustin_i_jarv_alg_stl::TestTaskSTL test_task_stl(task_data_stl);
+  ASSERT_TRUE(test_task_stl.Validation());
+  test_task_stl.PreProcessing();
+  test_task_stl.Run();
+  test_task_stl.PostProcessing();
+
+  for (size_t i = 0; i < expected_result.size(); ++i) {
+    EXPECT_EQ(expected_result[i], output_result[i]);
+  }
+}
