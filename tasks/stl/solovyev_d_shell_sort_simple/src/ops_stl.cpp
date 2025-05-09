e7c51693e594313a968df10c7417c6c79db8fc84
0xG00SE <61384845+DSolo03@users.noreply.github.com>
2025-05-09 16:39:12 +0300

    fix

--- tasks/stl/solovyev_d_shell_sort_simple/src/ops_stl.cpp
+++ tasks/stl/solovyev_d_shell_sort_simple/src/ops_stl.cpp
@@ -1,3 +1,90 @@
+<<<<<<< HEAD
+#include "stl/solovyev_d_shell_sort_simple/include/ops_stl.hpp"
+
+#include <chrono>
+#include <cmath>
+#include <cstddef>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+#include "core/util/include/util.hpp"
+
+using namespace std::chrono_literals;
+
+bool solovyev_d_shell_sort_simple_stl::TaskSTL::PreProcessingImpl() {
+  size_t input_size = task_data->inputs_count[0];
+  auto *in_ptr = reinterpret_cast<int *>(task_data->inputs[0]);
+  input_ = std::vector<int>(in_ptr, in_ptr + input_size);
+
+  return true;
+}
+
+bool solovyev_d_shell_sort_simple_stl::TaskSTL::ValidationImpl() {
+  return task_data->inputs_count[0] == task_data->outputs_count[0];
+}
+
+void solovyev_d_shell_sort_simple_stl::TaskSTL::ThreadWorker(int t) {
+  int local_gap = 0;
+  while (true) {
+    std::this_thread::sleep_for(0.01ms);
+    std::unique_lock lock(m_);
+    cv_.wait(lock, [&] { return local_gap != gap_ || done_; });
+    if (done_) {
+      return;
+    }
+    local_gap = gap_;
+    lock.unlock();
+    for (int i = t; i < local_gap; i += num_threads_) {
+      for (size_t f = i + local_gap; f < input_.size(); f += local_gap) {
+        int val = input_[f];
+        size_t j = f;
+        while (j >= static_cast<size_t>(local_gap) && input_[j - local_gap] > val) {
+          input_[j] = input_[j - local_gap];
+          j -= local_gap;
+        }
+        input_[j] = val;
+      }
+    }
+    if (++threads_completed_ == num_threads_) {
+      cv_done_.notify_one();
+    }
+  }
+}
+
+bool solovyev_d_shell_sort_simple_stl::TaskSTL::RunImpl() {
+  num_threads_ = ppc::util::GetPPCNumThreads();
+  std::vector<std::thread> threads(num_threads_);
+  for (int t = 0; t < num_threads_; ++t) {
+    threads[t] = std::thread(&TaskSTL::ThreadWorker, this, t);
+    done_ = false;
+  }
+  for (gap_ = static_cast<int>(input_.size()) / 2; gap_ > 0; gap_ /= 2) {
+    cv_.notify_all();
+    std::unique_lock lock(m_);
+    cv_done_.wait(lock, [&] { return threads_completed_ == num_threads_; });
+    threads_completed_ = 0;
+  }
+  {
+    std::lock_guard<std::mutex> lock(m_);
+    done_ = true;
+  }
+  cv_.notify_all();
+  for (auto &th : threads) {
+    if (th.joinable()) {
+      th.join();
+    }
+  }
+  return true;
+}
+
+bool solovyev_d_shell_sort_simple_stl::TaskSTL::PostProcessingImpl() {
+  for (size_t i = 0; i < input_.size(); i++) {
+    reinterpret_cast<int *>(task_data->outputs[0])[i] = input_[i];
+  }
+  return true;
+}
+=======
 #include "stl/solovyev_d_shell_sort_simple/include/ops_stl.hpp"
 
 #include <cmath>
@@ -79,3 +166,4 @@ bool solovyev_d_shell_sort_simple_stl::TaskSTL::PostProcessingImpl() {
   }
   return true;
 }
+>>>>>>> e80c8cde7755b26e5a703b5bf5976d00bdad725a
