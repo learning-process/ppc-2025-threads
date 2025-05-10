9728d9e728c2ed8af88ca4edb526078d11bd36f4
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-10 05:56:45 +0300

    Update ops_stl.cpp

--- tasks/stl/deryabin_m_hoare_sort_simple_merge/src/ops_stl.cpp
+++ tasks/stl/deryabin_m_hoare_sort_simple_merge/src/ops_stl.cpp
@@ -23,8 +23,7 @@ void deryabin_m_hoare_sort_simple_merge_stl::HoaraSort(std::vector<double>& a, s
     size_t i = first_;
     size_t j = last_;
     double tmp = 0;
-    double x = std::max(std::min(a[first_], a[(first_ + last_) / 2]),
-                        std::min(std::max(a[first_], a[(first_ + last_) / 2]), a[last_]));
+    double x = a[(first_ + last_) / 2];
     do {
       while (a[i] < x) {
         i++;
