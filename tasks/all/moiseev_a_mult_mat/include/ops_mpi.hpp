59b63ec8c49edfe6e41b32429fea63520fa91c75
MoiseevArt <amoiseev_00@mail.ru>
2025-05-14 19:16:18 +0000

    2.2

--- tasks/all/moiseev_a_mult_mat/include/ops_mpi.hpp
+++ tasks/all/moiseev_a_mult_mat/include/ops_mpi.hpp
@@ -3,6 +3,7 @@
 #include <utility>
 #include <vector>
 
+#include "boost/mpi/communicator.hpp"
 #include "core/task/include/task.hpp"
 
 namespace moiseev_a_mult_mat_mpi {
@@ -17,6 +18,18 @@ class MultMatMPI : public ppc::core::Task {
   bool PostProcessingImpl() override;
 
  private:
+  void DetermineGrid(int world_size, int& p, int& active_procs, int& block) const;
+
+  void DistributeMatrices(boost::mpi::communicator& world, int world_rank, int active_procs, int p, int block,
+                          std::vector<double>& a_block, std::vector<double>& b_block) const;
+
+  static void ComputeLocalBlock(boost::mpi::communicator& active, int my_row, int my_col, int p, int block,
+                                std::vector<double>& a_block, std::vector<double>& b_block,
+                                std::vector<double>& c_block);
+
+  void GatherResult(boost::mpi::communicator& world, int world_rank, int active_procs, int p, int block,
+                    const std::vector<double>& c_block);
+
   std::vector<double> matrix_a_, matrix_b_, matrix_c_;
   int matrix_size_{};
   int num_blocks_{};
