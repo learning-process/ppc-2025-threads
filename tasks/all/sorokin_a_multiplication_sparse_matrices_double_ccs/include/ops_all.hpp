b8ecae385546428805fa97b992f1dd485484aad4
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2025-05-16 21:20:15 +0300

    start

--- tasks/all/sorokin_a_multiplication_sparse_matrices_double_ccs/include/ops_all.hpp
+++ tasks/all/sorokin_a_multiplication_sparse_matrices_double_ccs/include/ops_all.hpp
@@ -38,5 +38,21 @@ void MultiplyCCS(boost::mpi::communicator &world, const std::vector<double> &a_v
                  const std::vector<double> &b_values, const std::vector<int> &b_row_indices, int k,
                  const std::vector<int> &b_col_ptr, std::vector<double> &c_values, std::vector<int> &c_row_indices,
                  int n, std::vector<int> &c_col_ptr);
-
+void DistributeBColumns(boost::mpi::communicator &world, int rank, int size, const std::vector<double> &b_values,
+                        const std::vector<int> &b_row_indices, const std::vector<int> &b_col_ptr, int n,
+                        int base_cols_per_proc, int remainder, std::vector<double> &local_b_values,
+                        std::vector<int> &local_b_row_indices, std::vector<int> &local_b_col_ptr);
+void CalculateLocalNNZ(const std::vector<int> &a_row_indices, const std::vector<int> &a_col_ptr,
+                       const std::vector<int> &local_b_row_indices, const std::vector<int> &local_b_col_ptr, int m,
+                       int num_local_cols, std::vector<int> &local_nnz);
+void ComputeLocalC(const std::vector<double> &a_values, const std::vector<int> &a_row_indices,
+                   const std::vector<int> &a_col_ptr, const std::vector<double> &local_b_values,
+                   const std::vector<int> &local_b_row_indices, const std::vector<int> &local_b_col_ptr, int m,
+                   int num_local_cols, const std::vector<int> &local_nnz, std::vector<double> &local_c_values,
+                   std::vector<int> &local_c_row_indices, std::vector<int> &local_c_col_ptr);
+void GatherResults(boost::mpi::communicator &world, int rank, int size, int n, int base_cols_per_proc, int remainder,
+                   int num_local_cols, int start_col, const std::vector<int> &local_nnz,
+                   const std::vector<double> &local_c_values, const std::vector<int> &local_c_row_indices,
+                   const std::vector<int> &local_c_col_ptr, std::vector<double> &c_values,
+                   std::vector<int> &c_row_indices, std::vector<int> &c_col_ptr);
 }  // namespace sorokin_a_multiplication_sparse_matrices_double_ccs_all
--- tasks/all/sorokin_a_multiplication_sparse_matrices_double_ccs/src/ops_all.cpp
+++ tasks/all/sorokin_a_multiplication_sparse_matrices_double_ccs/src/ops_all.cpp
@@ -1,33 +1,17 @@
 #include "all/sorokin_a_multiplication_sparse_matrices_double_ccs/include/ops_all.hpp"
 
 #include <algorithm>
+#include <boost/mpi/communicator.hpp>
 #include <cmath>
 #include <cstddef>
 #include <vector>
 
 namespace sorokin_a_multiplication_sparse_matrices_double_ccs_all {
 
-void MultiplyCCS(boost::mpi::communicator& world, const std::vector<double>& a_values,
-                 const std::vector<int>& a_row_indices, int m, const std::vector<int>& a_col_ptr,
-                 const std::vector<double>& b_values, const std::vector<int>& b_row_indices, int k,
-                 const std::vector<int>& b_col_ptr, std::vector<double>& c_values, std::vector<int>& c_row_indices,
-                 int n, std::vector<int>& c_col_ptr) {
-  const int rank = world.rank();
-  const int size = world.size();
-  world.barrier();
-
-  const int base_cols_per_proc = n / size;
-  const int remainder = n % size;
-  int start_col = 0;
-  int num_local_cols = 0;
-
-  start_col = rank * base_cols_per_proc + std::min(rank, remainder);
-  num_local_cols = base_cols_per_proc + (rank < remainder ? 1 : 0);
-
-  std::vector<double> local_b_values;
-  std::vector<int> local_b_row_indices;
-  std::vector<int> local_b_col_ptr(num_local_cols + 1);
-
+void DistributeBColumns(boost::mpi::communicator& world, int rank, int size, const std::vector<double>& b_values,
+                        const std::vector<int>& b_row_indices, const std::vector<int>& b_col_ptr, int n,
+                        int base_cols_per_proc, int remainder, std::vector<double>& local_b_values,
+                        std::vector<int>& local_b_row_indices, std::vector<int>& local_b_col_ptr) {
   if (rank == 0) {
     for (int p = 0; p < size; ++p) {
       const int p_start = (p * base_cols_per_proc) + std::min(p, remainder);
@@ -58,9 +42,11 @@ void MultiplyCCS(boost::mpi::communicator& world, const std::vector<double>& a_v
     world.recv(0, 1, local_b_row_indices);
     world.recv(0, 2, local_b_col_ptr);
   }
-  world.barrier();
+}
 
-  std::vector<int> local_nnz(num_local_cols, 0);
+void CalculateLocalNNZ(const std::vector<int>& a_row_indices, const std::vector<int>& a_col_ptr,
+                       const std::vector<int>& local_b_row_indices, const std::vector<int>& local_b_col_ptr, int m,
+                       int num_local_cols, std::vector<int>& local_nnz) {
 #pragma omp parallel for
   for (int j = 0; j < num_local_cols; ++j) {
     std::vector<bool> used(m, false);
@@ -75,14 +61,20 @@ void MultiplyCCS(boost::mpi::communicator& world, const std::vector<double>& a_v
       }
     }
   }
+}
 
-  std::vector<int> local_c_col_ptr(num_local_cols + 1, 0);
+void ComputeLocalC(const std::vector<double>& a_values, const std::vector<int>& a_row_indices,
+                   const std::vector<int>& a_col_ptr, const std::vector<double>& local_b_values,
+                   const std::vector<int>& local_b_row_indices, const std::vector<int>& local_b_col_ptr, int m,
+                   int num_local_cols, const std::vector<int>& local_nnz, std::vector<double>& local_c_values,
+                   std::vector<int>& local_c_row_indices, std::vector<int>& local_c_col_ptr) {
+  local_c_col_ptr.resize(num_local_cols + 1, 0);
   for (int j = 0; j < num_local_cols; ++j) {
     local_c_col_ptr[j + 1] = local_c_col_ptr[j] + local_nnz[j];
   }
 
-  std::vector<double> local_c_values(local_c_col_ptr.back());
-  std::vector<int> local_c_row_indices(local_c_col_ptr.back());
+  local_c_values.resize(local_c_col_ptr.back());
+  local_c_row_indices.resize(local_c_col_ptr.back());
 
 #pragma omp parallel for
   for (int j = 0; j < num_local_cols; ++j) {
@@ -109,13 +101,14 @@ void MultiplyCCS(boost::mpi::communicator& world, const std::vector<double>& a_v
         pos++;
       }
     }
-    local_nnz[j] = pos - local_c_col_ptr[j];
-    local_c_col_ptr[j + 1] = pos;
   }
+}
 
-  local_c_values.resize(local_c_col_ptr.back());
-  local_c_row_indices.resize(local_c_col_ptr.back());
-
+void GatherResults(boost::mpi::communicator& world, int rank, int size, int n, int base_cols_per_proc, int remainder,
+                   int num_local_cols, int start_col, const std::vector<int>& local_nnz,
+                   const std::vector<double>& local_c_values, const std::vector<int>& local_c_row_indices,
+                   const std::vector<int>& local_c_col_ptr, std::vector<double>& c_values,
+                   std::vector<int>& c_row_indices, std::vector<int>& c_col_ptr) {
   if (rank == 0) {
     c_col_ptr.resize(n + 1);
     std::vector<int> gather_nnz(n, 0);
@@ -179,6 +172,42 @@ void MultiplyCCS(boost::mpi::communicator& world, const std::vector<double>& a_v
       world.send(0, global_j, local_c_values.data() + start, count);
     }
   }
+}
+
+void MultiplyCCS(boost::mpi::communicator& world, const std::vector<double>& a_values,
+                 const std::vector<int>& a_row_indices, int m, const std::vector<int>& a_col_ptr,
+                 const std::vector<double>& b_values, const std::vector<int>& b_row_indices, int k,
+                 const std::vector<int>& b_col_ptr, std::vector<double>& c_values, std::vector<int>& c_row_indices,
+                 int n, std::vector<int>& c_col_ptr) {
+  const int rank = world.rank();
+  const int size = world.size();
+  world.barrier();
+
+  const int base_cols_per_proc = n / size;
+  const int remainder = n % size;
+  const int start_col = rank * base_cols_per_proc + std::min(rank, remainder);
+  const int num_local_cols = base_cols_per_proc + (rank < remainder ? 1 : 0);
+
+  std::vector<double> local_b_values;
+  std::vector<int> local_b_row_indices;
+  std::vector<int> local_b_col_ptr(num_local_cols + 1);
+
+  DistributeBColumns(world, rank, size, b_values, b_row_indices, b_col_ptr, n, base_cols_per_proc, remainder,
+                     local_b_values, local_b_row_indices, local_b_col_ptr);
+  world.barrier();
+
+  std::vector<int> local_nnz(num_local_cols, 0);
+  CalculateLocalNNZ(a_row_indices, a_col_ptr, local_b_row_indices, local_b_col_ptr, m, num_local_cols, local_nnz);
+
+  std::vector<double> local_c_values;
+  std::vector<int> local_c_row_indices;
+  std::vector<int> local_c_col_ptr;
+
+  ComputeLocalC(a_values, a_row_indices, a_col_ptr, local_b_values, local_b_row_indices, local_b_col_ptr, m,
+                num_local_cols, local_nnz, local_c_values, local_c_row_indices, local_c_col_ptr);
+
+  GatherResults(world, rank, size, n, base_cols_per_proc, remainder, num_local_cols, start_col, local_nnz,
+                local_c_values, local_c_row_indices, local_c_col_ptr, c_values, c_row_indices, c_col_ptr);
   world.barrier();
 }
 
