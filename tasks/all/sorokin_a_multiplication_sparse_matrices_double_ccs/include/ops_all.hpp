3552cb48a48b931b73ff68601b272b7371fdba8f
AndreySorokin7 <andrey_sorokin_nn@mail,ru>
2025-05-19 13:45:42 +0300

    fix

--- tasks/all/sorokin_a_multiplication_sparse_matrices_double_ccs/include/ops_all.hpp
+++ tasks/all/sorokin_a_multiplication_sparse_matrices_double_ccs/include/ops_all.hpp
@@ -46,10 +46,10 @@ void CalculateLocalNNZ(const std::vector<int> &a_row_indices, const std::vector<
                        const std::vector<int> &local_b_row_indices, const std::vector<int> &local_b_col_ptr, int m,
                        int num_local_cols, std::vector<int> &local_nnz);
 static void ComputeLocalC(const std::vector<double> &a_values, const std::vector<int> &a_row_indices,
-                   const std::vector<int> &a_col_ptr, const std::vector<double> &local_b_values,
-                   const std::vector<int> &local_b_row_indices, const std::vector<int> &local_b_col_ptr, int m,
-                   int num_local_cols, const std::vector<int> &local_nnz, std::vector<double> &local_c_values,
-                   std::vector<int> &local_c_row_indices, std::vector<int> &local_c_col_ptr);
+                          const std::vector<int> &a_col_ptr, const std::vector<double> &local_b_values,
+                          const std::vector<int> &local_b_row_indices, const std::vector<int> &local_b_col_ptr, int m,
+                          int num_local_cols, const std::vector<int> &local_nnz, std::vector<double> &local_c_values,
+                          std::vector<int> &local_c_row_indices, std::vector<int> &local_c_col_ptr);
 void GatherResults(boost::mpi::communicator &world, int rank, int size, int n, int base_cols_per_proc, int remainder,
                    int num_local_cols, int start_col, const std::vector<int> &local_nnz,
                    const std::vector<double> &local_c_values, const std::vector<int> &local_c_row_indices,
