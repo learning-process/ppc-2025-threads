8331d35d0d0f532909ed472582fbab558f585a76
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2025-05-20 14:28:01 +0300

    fix clang

--- tasks/all/kolodkin_g_multiplication_matrix_CRS/include/ops_all.hpp
+++ tasks/all/kolodkin_g_multiplication_matrix_CRS/include/ops_all.hpp
@@ -26,7 +26,7 @@ struct SparseMatrixCRS {
   std::vector<int> rowPtr;
   int numRows;
   int numCols;
-  SparseMatrixCRS() : values({}), colIndices({}), rowPtr({}), numRows(0), numCols(0){};
+  SparseMatrixCRS() : values({}), colIndices({}), rowPtr({}), numRows(0), numCols(0) {};
   SparseMatrixCRS(int rows, int cols) : numRows(rows), numCols(cols) { rowPtr.resize(rows + 1, 0); }
   void AddValue(int row, Complex value, int col);
   SparseMatrixCRS(const SparseMatrixCRS& other) = default;
