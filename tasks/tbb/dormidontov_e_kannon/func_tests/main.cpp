24b1ddb5a059c7c96911205c88ffa45f567dcc4a
Dormidontov Egor <113050823+EgorDormidontov@users.noreply.github.com>
2025-05-21 17:49:58 +0300

    tbb

---
+++ tasks/tbb/dormidontov_e_kannon/func_tests/main.cpp
@@ -0,0 +1,225 @@
+#include <gtest/gtest.h>
+
+#include <cstdint>
+#include <memory>
+#include <random>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+#include "tbb/dormidontov_e_kannon/include/ops_tbb.hpp"
+
+namespace dormidontov_e_kannon_tbb {
+matrix GenMatrix(size_t n) {
+  matrix mat(n * n);
+  std::mt19937 gen(1337);
+  std::uniform_real_distribution<double> distribution(-100, 100);
+
+  for (size_t i = 0; i < n * n; i++) {
+    mat[i] = distribution(gen);
+  }
+  return mat;
+}
+
+matrix NaiveMultipilication(const matrix& A, const matrix& B, size_t n) {
+  matrix C(n * n);
+  for (size_t i = 0; i < n; i++) {
+    for (size_t j = 0; j < n; j++) {
+      for (size_t k = 0; k < n; k++) {
+        C[idx(i, j, n)] += A[idx(i, k, n)] * B[idx(k, j, n)];
+      }
+    }
+  }
+  return C;
+}
+
+TEST(dormidontov_e_kannon_tbb, mat36x36) {
+  tbb::global_control global_limit(tbb::global_control::max_allowed_parallelism, 4);
+  size_t test_side_size = 36;
+  size_t test_num_blocks = 6;
+  auto A = GenMatrix(test_side_size);
+  auto B = GenMatrix(test_side_size);
+  matrix ans = NaiveMultipilication(A, B, test_side_size);
+  matrix C(test_side_size * test_side_size);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(B.data()));
+  task_data_seq->inputs_count.emplace_back(A.size());
+  task_data_seq->inputs_count.emplace_back(B.size());
+  task_data_seq->inputs_count.emplace_back(test_num_blocks);
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(C.data()));
+  task_data_seq->outputs_count.emplace_back(C.size());
+
+  dormidontov_e_kannon_tbb::TbbTask task_seq(task_data_seq);
+  ASSERT_TRUE(task_seq.Validation());
+  task_seq.PreProcessing();
+  task_seq.Run();
+  task_seq.PostProcessing();
+  for (size_t i = 0; i < test_side_size * test_side_size; i++) {
+    EXPECT_NEAR(ans[i], C[i], 1e-6);
+  }
+}
+
+TEST(dormidontov_e_kannon_tbb, mat16) {
+  tbb::global_control global_limit(tbb::global_control::max_allowed_parallelism, 4);
+  size_t test_side_size = 4;
+  size_t test_num_blocks = 2;
+  matrix A = {1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1};
+  matrix B = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
+  matrix ans = NaiveMultipilication(A, B, test_side_size);
+  matrix C(test_side_size * test_side_size, 0.0);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(B.data()));
+  task_data_seq->inputs_count.emplace_back(A.size());
+  task_data_seq->inputs_count.emplace_back(B.size());
+  task_data_seq->inputs_count.emplace_back(test_num_blocks);
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(C.data()));
+  task_data_seq->outputs_count.emplace_back(C.size());
+
+  dormidontov_e_kannon_tbb::TbbTask task_seq(task_data_seq);
+  ASSERT_TRUE(task_seq.Validation());
+  task_seq.PreProcessing();
+  task_seq.Run();
+  task_seq.PostProcessing();
+
+  for (size_t i = 0; i < test_side_size * test_side_size; i++) {
+    EXPECT_EQ(ans[i], C[i]);
+  }
+}
+
+TEST(dormidontov_e_kannon_tbb, wrong_matrix_size) {
+  tbb::global_control global_limit(tbb::global_control::max_allowed_parallelism, 4);
+  matrix A(4 * 4);
+  matrix B(3 * 3);
+  matrix C(2 * 2);
+  size_t test_num_blocks = 1;
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(B.data()));
+  task_data_seq->inputs_count.emplace_back(A.size());
+  task_data_seq->inputs_count.emplace_back(B.size());
+  task_data_seq->inputs_count.emplace_back(test_num_blocks);
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(C.data()));
+  task_data_seq->outputs_count.emplace_back(C.size());
+
+  dormidontov_e_kannon_tbb::TbbTask task_seq(task_data_seq);
+  ASSERT_FALSE(task_seq.Validation());
+}
+
+TEST(dormidontov_e_kannon_tbb, mat_36x36) {
+  tbb::global_control global_limit(tbb::global_control::max_allowed_parallelism, 4);
+  size_t test_side_size = 36;
+  size_t test_num_blocks = 6;
+  matrix A(test_side_size * test_side_size, 1.0);
+  matrix B(test_side_size * test_side_size, 1.0);
+  matrix C(test_side_size * test_side_size, 0.0);
+  matrix ans(test_side_size * test_side_size, test_side_size);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(B.data()));
+  task_data_seq->inputs_count.emplace_back(A.size());
+  task_data_seq->inputs_count.emplace_back(B.size());
+  task_data_seq->inputs_count.emplace_back(test_num_blocks);
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(C.data()));
+  task_data_seq->outputs_count.emplace_back(C.size());
+
+  dormidontov_e_kannon_tbb::TbbTask task_seq(task_data_seq);
+  ASSERT_TRUE(task_seq.Validation());
+  task_seq.PreProcessing();
+  task_seq.Run();
+  task_seq.PostProcessing();
+
+  for (size_t i = 0; i < test_side_size * test_side_size; i++) {
+    EXPECT_NEAR(ans[i], C[i], 1e-6);
+  }
+}
+
+TEST(dormidontov_e_kannon_tbb, wrong_block_size) {
+  tbb::global_control global_limit(tbb::global_control::max_allowed_parallelism, 4);
+  size_t test_side_size = 17;
+  size_t test_num_blocks = 8;
+  matrix A(test_side_size * test_side_size, 1.0);
+  matrix B(test_side_size * test_side_size, 1.0);
+  matrix C(test_side_size * test_side_size);
+  matrix ans(test_side_size * test_side_size, test_side_size);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(B.data()));
+  task_data_seq->inputs_count.emplace_back(A.size());
+  task_data_seq->inputs_count.emplace_back(B.size());
+  task_data_seq->inputs_count.emplace_back(test_num_blocks);
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(C.data()));
+  task_data_seq->outputs_count.emplace_back(C.size());
+
+  dormidontov_e_kannon_tbb::TbbTask task_seq(task_data_seq);
+  ASSERT_FALSE(task_seq.Validation());
+}
+
+TEST(dormidontov_e_kannon_tbb, mat27x27) {
+  tbb::global_control global_limit(tbb::global_control::max_allowed_parallelism, 4);
+  size_t test_side_size = 27;
+  size_t test_num_blocks = 3;
+  matrix A(test_side_size * test_side_size, 1);
+  matrix B(test_side_size * test_side_size, 1);
+  matrix C(test_side_size * test_side_size, 0);
+  matrix ans = NaiveMultipilication(A, B, test_side_size);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(B.data()));
+  task_data_seq->inputs_count.emplace_back(A.size());
+  task_data_seq->inputs_count.emplace_back(B.size());
+  task_data_seq->inputs_count.emplace_back(test_num_blocks);
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(C.data()));
+  task_data_seq->outputs_count.emplace_back(C.size());
+
+  dormidontov_e_kannon_tbb::TbbTask task_seq(task_data_seq);
+  ASSERT_TRUE(task_seq.Validation());
+  task_seq.PreProcessing();
+  task_seq.Run();
+  task_seq.PostProcessing();
+
+  for (size_t i = 0; i < test_side_size * test_side_size; i++) {
+    EXPECT_NEAR(ans[i], C[i], 1e-6);
+  }
+}
+
+TEST(dormidontov_e_kannon_tbb, I_mat) {
+  tbb::global_control global_limit(tbb::global_control::max_allowed_parallelism, 4);
+  size_t test_side_size = 36;
+  size_t test_num_blocks = 6;
+  matrix A(test_side_size * test_side_size, 1.0);
+  matrix B(test_side_size * test_side_size);
+  matrix C(test_side_size * test_side_size);
+
+  for (size_t i = 0; i < test_side_size; i++) {
+    B[idx(i, i, test_side_size)] = 1.0;
+  }
+
+  matrix ans = A;
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(B.data()));
+  task_data_seq->inputs_count.emplace_back(A.size());
+  task_data_seq->inputs_count.emplace_back(B.size());
+  task_data_seq->inputs_count.emplace_back(test_num_blocks);
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(C.data()));
+  task_data_seq->outputs_count.emplace_back(C.size());
+
+  dormidontov_e_kannon_tbb::TbbTask task_seq(task_data_seq);
+  ASSERT_TRUE(task_seq.Validation());
+  task_seq.PreProcessing();
+  task_seq.Run();
+  task_seq.PostProcessing();
+
+  for (size_t i = 0; i < test_side_size * test_side_size; i++) {
+    EXPECT_EQ(ans[i], C[i]);
+  }
+}
+}  // namespace dormidontov_e_kannon_tbb
---
+++ tasks/tbb/dormidontov_e_kannon/src/ops_tbb.cpp
@@ -0,0 +1,118 @@
+#include "tbb/dormidontov_e_kannon/include/ops_tbb.hpp"
+
+#include <oneapi/tbb/blocked_range.h>
+#include <oneapi/tbb/blocked_range2d.h>
+#include <oneapi/tbb/parallel_for.h>
+#include <tbb/tbb.h>
+
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+bool dormidontov_e_kannon_tbb::TbbTask::PreProcessingImpl() {
+  block_size_ = side_size_ / num_blocks_;
+
+  auto* a_ptr = reinterpret_cast<double*>(task_data->inputs[0]);
+  auto* b_ptr = reinterpret_cast<double*>(task_data->inputs[1]);
+  A_.assign(a_ptr, a_ptr + matrix_size_);
+  B_.assign(b_ptr, b_ptr + matrix_size_);
+  A_buffer_.assign(matrix_size_, 0);
+  B_buffer_.assign(matrix_size_, 0);
+  C_.assign(matrix_size_, 0);
+
+  return true;
+}
+
+bool dormidontov_e_kannon_tbb::TbbTask::ValidationImpl() {
+  matrix_size_ = static_cast<size_t>(task_data->inputs_count[0]);
+  side_size_ = static_cast<size_t>(std::sqrt(matrix_size_));
+  num_blocks_ = static_cast<size_t>(task_data->inputs_count[2]);
+
+  return task_data->inputs_count[0] == task_data->inputs_count[1] &&
+         task_data->outputs_count[0] == task_data->inputs_count[0] && side_size_ % num_blocks_ == 0;
+}
+
+void dormidontov_e_kannon_tbb::TbbTask::StartingShift() {
+  std::swap(A_buffer_, A_);
+  std::swap(B_buffer_, B_);
+
+  for (size_t block_i = 0; block_i < num_blocks_; ++block_i) {
+    for (size_t block_j = 0; block_j < num_blocks_; ++block_j) {
+      size_t row;
+      size_t col;
+      col = row = (block_j + block_i) % num_blocks_;
+      tbb::parallel_for(tbb::blocked_range2d<size_t>(block_i, block_i + block_size_, block_j, block_j + block_size_),
+                        [&](const tbb::blocked_range2d<size_t>& r) {
+                          for (size_t i = r.rows().begin(); i != r.rows().end(); i++) {
+                            for (size_t j = r.cols().begin(); j != r.cols().end(); j++) {
+                              A_[idx(idx(block_i, i, block_size_), idx(block_j, j, block_size_), side_size_)] =
+                                  A_buffer_[idx(idx(block_i, i, block_size_), idx(col, j, block_size_), side_size_)];
+                              B_[idx(idx(block_i, i, block_size_), idx(block_j, j, block_size_), side_size_)] =
+                                  B_buffer_[idx(idx(row, i, block_size_), idx(block_j, j, block_size_), side_size_)];
+                            }
+                          }
+                        });
+    }
+  }
+}
+
+void dormidontov_e_kannon_tbb::TbbTask::IterationShift() {
+  std::swap(A_buffer_, A_);
+  std::swap(B_buffer_, B_);
+
+  for (size_t block_i = 0; block_i < num_blocks_; ++block_i) {
+    for (size_t block_j = 0; block_j < num_blocks_; ++block_j) {
+      size_t row;
+      size_t col;
+      row = (block_i + 1) % num_blocks_;
+      col = (block_j + 1) % num_blocks_;
+      tbb::parallel_for(tbb::blocked_range2d<size_t>(block_i, block_i + block_size_, block_j, block_j + block_size_),
+                        [&](const tbb::blocked_range2d<size_t>& r) {
+                          for (size_t i = r.rows().begin(); i != r.rows().end(); i++) {
+                            for (size_t j = r.cols().begin(); j != r.cols().end(); j++) {
+                              A_[idx(idx(block_i, i, block_size_), idx(block_j, j, block_size_), side_size_)] =
+                                  A_buffer_[idx(idx(block_i, i, block_size_), idx(col, j, block_size_), side_size_)];
+                              B_[idx(idx(block_i, i, block_size_), idx(block_j, j, block_size_), side_size_)] =
+                                  B_buffer_[idx(idx(row, i, block_size_), idx(block_j, j, block_size_), side_size_)];
+                            }
+                          }
+                        });
+    }
+  }
+}
+
+void dormidontov_e_kannon_tbb::TbbTask::MultImpl() {
+  for (size_t block_i = 0; block_i < side_size_; block_i += block_size_) {
+    for (size_t block_j = 0; block_j < side_size_; block_j += block_size_) {
+      tbb::parallel_for(tbb::blocked_range2d<size_t>(block_i, block_i + block_size_, block_j, block_j + block_size_),
+                        [&](const tbb::blocked_range2d<size_t>& r) {
+                          for (size_t i = r.rows().begin(); i != r.rows().end(); i++) {
+                            for (size_t j = r.cols().begin(); j != r.cols().end(); j++) {
+                              for (size_t k = 0; k < block_size_; k++) {
+                                C_[idx(i, j, side_size_)] +=
+                                    A_[idx(i, block_j + k, side_size_)] * B_[idx(block_i + k, j, side_size_)];
+                              }
+                            }
+                          }
+                        });
+    }
+  }
+}
+
+bool dormidontov_e_kannon_tbb::TbbTask::RunImpl() {
+  oneapi::tbb::task_arena arena(1);
+  arena.execute([&]() {
+    StartingShift();
+
+    for (size_t iter = 0; iter < num_blocks_; ++iter) {
+      MultImpl();
+      IterationShift();
+    }
+  });
+  return true;
+}
+
+bool dormidontov_e_kannon_tbb::TbbTask::PostProcessingImpl() {
+  std::ranges::copy(C_, reinterpret_cast<double*>(task_data->outputs[0]));
+  return true;
+}
