efe56ca83802fcd4334203260d3414d0dd3f6f0e
Dormidontov Egor <113050823+EgorDormidontov@users.noreply.github.com>
2025-05-21 13:44:35 +0300

    omp

---
+++ tasks/omp/dormidontov_e_kannon/include/ops_omp.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cmath>
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace dormidontov_e_kannon_omp {
+
+inline size_t idx(size_t row, size_t column, size_t n) { return (row * n) + column; }
+
+using matrix = std::vector<double>;
+
+class OmpTask : public ppc::core::Task {
+ public:
+  explicit OmpTask(std::shared_ptr<ppc::core::TaskData> task_data) : Task(std::move(task_data)) {}
+
+  bool PreProcessingImpl() override;
+  bool ValidationImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+ private:
+  size_t matrix_size_;
+  size_t side_size_;
+  size_t block_size_;
+  size_t num_blocks_;
+  matrix A_;
+  matrix B_;
+  matrix C_;
+  matrix A_buffer_;
+  matrix B_buffer_;
+
+  void StartingShift();
+  void IterationShift();
+};
+}  // namespace dormidontov_e_kannon_omp
