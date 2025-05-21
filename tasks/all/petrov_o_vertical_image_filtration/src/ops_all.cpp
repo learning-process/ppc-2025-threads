b3b8ceee9d749085960d7a456d4cc37db41fed10
ValeraDanger <oleg.petrov.03@list.ru>
2025-05-21 19:24:34 +0300

    tidy

--- tasks/all/petrov_o_vertical_image_filtration/src/ops_all.cpp
+++ tasks/all/petrov_o_vertical_image_filtration/src/ops_all.cpp
@@ -69,7 +69,9 @@ void TaskAll::ApplyGaussianFilterTbb(std::vector<int> &local_output_ref, size_t 
           float sum = 0.0F;
           for (int ki = -1; ki <= 1; ++ki) {
             for (int kj = -1; kj <= 1; ++kj) {
+              // NOLINTNEXTLINE(bugprone-misplaced-widening-cast) - cast is correct, ki + 1 >= 0
               auto kernel_row = static_cast<size_t>(ki + 1);
+              // NOLINTNEXTLINE(bugprone-misplaced-widening-cast) - cast is correct, kj + 1 >= 0
               auto kernel_col = static_cast<size_t>(kj + 1);
               sum += static_cast<float>(input_[((i_in + ki) * width_) + (j_in + kj)]) *
                      gaussian_kernel_[(kernel_row * 3) + kernel_col];
