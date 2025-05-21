e458a885f45c34ee113183fba5fb78ee51ada3f9
Kharin Matvey <harin.matveix@ya.ru>
2025-05-21 18:08:44 +0300

    f

--- tasks/all/kharin_m_multidimensional_integral_calc/src/ops_all.cpp
+++ tasks/all/kharin_m_multidimensional_integral_calc/src/ops_all.cpp
@@ -80,8 +80,7 @@ bool kharin_m_multidimensional_integral_calc_all::TaskALL::PreProcessingImpl() {
     }
     boost::mpi::scatterv(world_, input_, send_counts, displacements, local_input_.data(),
                         static_cast<int>(local_input_.size()), 0);
-  }
-  else {
+  } else {
     boost::mpi::scatterv(world_, local_input_.data(), static_cast<int>(local_input_.size()), 0);
   }
   world_.barrier();
