f2c1416baacb01060421e280d262266c6ded4658
Nikita <nikita200604prochorov@mail.ru>
2025-05-23 02:04:33 +0300

    fix-7

--- tasks/seq/prokhorov_n_multidimensional_integrals_by_trapezoidal_method/func_tests/main.cpp
+++ tasks/seq/prokhorov_n_multidimensional_integrals_by_trapezoidal_method/func_tests/main.cpp
@@ -102,33 +102,3 @@ TEST(prokhorov_n_multidimensional_integrals_by_trapezoidal_method_seq, test_inte
 
   EXPECT_NEAR(result, expected, 1e-2);
 }
-
-TEST(prokhorov_n_multidimensional_integrals_by_trapezoidal_method_seq, test_integral_3d_xyz) {
-  std::vector<double> lower = {0.0, 0.0, 0.0};
-  std::vector<double> upper = {1.0, 1.0, 1.0};
-  std::vector<int> steps = {100, 100, 100};
-  double expected = 1.0 / 8.0;
-  double result = 0.0;
-
-  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(lower.data()));
-  task_data_seq->inputs_count.emplace_back(lower.size() * sizeof(double));
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(upper.data()));
-  task_data_seq->inputs_count.emplace_back(upper.size() * sizeof(double));
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(steps.data()));
-  task_data_seq->inputs_count.emplace_back(steps.size() * sizeof(int));
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(&result));
-  task_data_seq->outputs_count.emplace_back(sizeof(double));
-
-  prokhorov_n_multidimensional_integrals_by_trapezoidal_method_seq::TestTaskSequential test_task_sequential(
-      task_data_seq);
-
-  test_task_sequential.SetFunction([](const std::vector<double>& point) { return point[0] * point[1] * point[2]; });
-
-  ASSERT_EQ(test_task_sequential.Validation(), true);
-  test_task_sequential.PreProcessing();
-  test_task_sequential.Run();
-  test_task_sequential.PostProcessing();
-
-  EXPECT_NEAR(result, expected, 1e-5);
-}
