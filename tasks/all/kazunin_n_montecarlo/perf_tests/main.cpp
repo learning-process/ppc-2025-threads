26a0e68e57a85726249a48d9119829ffde3023f7
NikKazzzzzz <127418135+NikKazzzzzz@users.noreply.github.com>
2025-05-22 23:04:15 +0300

    Remove comments

--- tasks/all/kazunin_n_montecarlo/perf_tests/main.cpp
+++ tasks/all/kazunin_n_montecarlo/perf_tests/main.cpp
@@ -31,9 +31,8 @@ TEST(kazunin_n_montecarlo_all, test_pipeline_run) {
   const auto f = [](const std::array<double, n> &args) {
     return std::accumulate(args.begin(), args.end(), 1.0,
                            [](const double acc, const double coord) { return acc + (sin(coord) * cos(coord)); });
-  };  // ((sin(x)*cos(x)) + (sin(y)*cos(y)) + ...)
+  };
 
-  // Create task_data
   auto task_data = std::make_shared<ppc::core::TaskData>();
   if (world.rank() == 0) {
     task_data->inputs = {reinterpret_cast<uint8_t *>(&precision), reinterpret_cast<uint8_t *>(&limits)};
@@ -42,10 +41,8 @@ TEST(kazunin_n_montecarlo_all, test_pipeline_run) {
     task_data->outputs_count = {1};
   }
 
-  // Create Task
   auto test_task = std::make_shared<kazunin_n_montecarlo_all::MonteCarloAll<n, decltype(f)>>(task_data, f);
 
-  // Create Perf attributes
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
@@ -55,10 +52,8 @@ TEST(kazunin_n_montecarlo_all, test_pipeline_run) {
     return static_cast<double>(duration) * 1e-9;
   };
 
-  // Create and init perf results
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
-  // Create Perf analyzer
   auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task);
   perf_analyzer->PipelineRun(perf_attr, perf_results);
   if (world.rank() == 0) {
@@ -80,9 +75,8 @@ TEST(kazunin_n_montecarlo_all, test_task_run) {
   const auto f = [](const std::array<double, n> &args) {
     return std::accumulate(args.begin(), args.end(), 1.0,
                            [](const double acc, const double coord) { return acc + (sin(coord) * cos(coord)); });
-  };  // ((sin(x)*cos(x)) + (sin(y)*cos(y)) + ...)
+  };
 
-  // Create task_data
   auto task_data = std::make_shared<ppc::core::TaskData>();
   if (world.rank() == 0) {
     task_data->inputs = {reinterpret_cast<uint8_t *>(&precision), reinterpret_cast<uint8_t *>(&limits)};
@@ -91,10 +85,8 @@ TEST(kazunin_n_montecarlo_all, test_task_run) {
     task_data->outputs_count = {1};
   }
 
-  // Create Task
   auto test_task = std::make_shared<kazunin_n_montecarlo_all::MonteCarloAll<n, decltype(f)>>(task_data, f);
 
-  // Create Perf attributes
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
   const auto t0 = std::chrono::high_resolution_clock::now();
@@ -104,10 +96,8 @@ TEST(kazunin_n_montecarlo_all, test_task_run) {
     return static_cast<double>(duration) * 1e-9;
   };
 
-  // Create and init perf results
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
-  // Create Perf analyzer
   auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task);
   perf_analyzer->TaskRun(perf_attr, perf_results);
   if (world.rank() == 0) {
