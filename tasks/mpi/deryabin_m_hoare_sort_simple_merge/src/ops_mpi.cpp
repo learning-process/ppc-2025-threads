563b60de8dc85a2aead75f86f19b3b8c5234696c
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-22 22:36:35 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_hoare_sort_simple_merge/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_hoare_sort_simple_merge/src/ops_mpi.cpp
@@ -3,8 +3,8 @@
 #include <oneapi/tbb/task_group.h>
 
 #include <algorithm>
-#include <boost/mpi/collectives/broadcast.hpp>
 #include <bit>
+#include <boost/mpi/collectives/broadcast.hpp>
 #include <cmath>
 #include <core/util/include/util.hpp>
 #include <cstddef>
