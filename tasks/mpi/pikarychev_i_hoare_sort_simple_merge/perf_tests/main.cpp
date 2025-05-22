b7d02dfe2905110e33a86c807bd58259df27e7f1
flashkasi <ilyapikarychev@gmail.com>
2025-05-22 03:03:42 +0300

    test

--- tasks/mpi/pikarychev_i_hoare_sort_simple_merge/perf_tests/main.cpp
+++ tasks/mpi/pikarychev_i_hoare_sort_simple_merge/perf_tests/main.cpp
@@ -11,7 +11,8 @@
 #include <utility>
 #include <vector>
 
-#include "../include/ops_stl.hpp"
+#include "../include/ops_all.hpp"
+#include "boost/mpi/communicator.hpp"
 #include "core/perf/include/perf.hpp"
 #include "core/task/include/task.hpp"
 
@@ -24,7 +25,9 @@ std::vector<T> GenerateReverseSortedVector(size_t size) {
 }
 }  // namespace
 
-TEST(pikarychev_i_hoare_sort_simple_merge_stl, test_pipeline_run) {
+TEST(pikarychev_i_hoare_sort_simple_merge_all, test_pipeline_run) {
+  boost::mpi::communicator world;
+
   std::random_device dev;
   std::mt19937 gen(dev());
   std::uniform_int_distribution<> dist(-5000, 5000);
@@ -35,12 +38,14 @@ TEST(pikarychev_i_hoare_sort_simple_merge_stl, test_pipeline_run) {
   bool reverse = false;
 
   auto task_data = std::make_shared<ppc::core::TaskData>();
-  task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(&reverse));
-  task_data->inputs_count.emplace_back(in.size());
-  task_data->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  task_data->outputs_count.emplace_back(out.size());
-  auto task = pikarychev_i_hoare_sort_simple_merge::HoareSTL<int>(task_data);
+  if (world.rank() == 0) {
+    task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+    task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(&reverse));
+    task_data->inputs_count.emplace_back(in.size());
+    task_data->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+    task_data->outputs_count.emplace_back(out.size());
+  }
+  auto task = pikarychev_i_hoare_sort_simple_merge::HoareMPITBB<int>(task_data);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
@@ -53,12 +58,15 @@ TEST(pikarychev_i_hoare_sort_simple_merge_stl, test_pipeline_run) {
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
   auto perf_analyzer = std::make_shared<ppc::core::Perf>(std::make_shared<decltype(task)>(std::move(task)));
   perf_analyzer->PipelineRun(perf_attr, perf_results);
-  ppc::core::Perf::PrintPerfStatistic(perf_results);
-
-  EXPECT_TRUE(std::ranges::is_sorted(out, std::greater<>{}));
+  if (world.rank() == 0) {
+    ppc::core::Perf::PrintPerfStatistic(perf_results);
+    EXPECT_TRUE(std::ranges::is_sorted(out));
+  }
 }
 
-TEST(pikarychev_i_hoare_sort_simple_merge_stl, test_task_run) {
+TEST(pikarychev_i_hoare_sort_simple_merge_all, test_task_run) {
+  boost::mpi::communicator world;
+
   std::random_device dev;
   std::mt19937 gen(dev());
   std::uniform_int_distribution<> dist(-5000, 5000);
@@ -69,12 +77,14 @@ TEST(pikarychev_i_hoare_sort_simple_merge_stl, test_task_run) {
   bool reverse = false;
 
   auto task_data = std::make_shared<ppc::core::TaskData>();
-  task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(&reverse));
-  task_data->inputs_count.emplace_back(in.size());
-  task_data->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  task_data->outputs_count.emplace_back(out.size());
-  auto task = pikarychev_i_hoare_sort_simple_merge::HoareSTL<int>(task_data);
+  if (world.rank() == 0) {
+    task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+    task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(&reverse));
+    task_data->inputs_count.emplace_back(in.size());
+    task_data->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+    task_data->outputs_count.emplace_back(out.size());
+  }
+  auto task = pikarychev_i_hoare_sort_simple_merge::HoareMPITBB<int>(task_data);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
   perf_attr->num_running = 10;
@@ -87,7 +97,8 @@ TEST(pikarychev_i_hoare_sort_simple_merge_stl, test_task_run) {
   auto perf_results = std::make_shared<ppc::core::PerfResults>();
   auto perf_analyzer = std::make_shared<ppc::core::Perf>(std::make_shared<decltype(task)>(std::move(task)));
   perf_analyzer->TaskRun(perf_attr, perf_results);
-  ppc::core::Perf::PrintPerfStatistic(perf_results);
-
-  EXPECT_TRUE(std::ranges::is_sorted(out, std::greater<>{}));
+  if (world.rank() == 0) {
+    ppc::core::Perf::PrintPerfStatistic(perf_results);
+    EXPECT_TRUE(std::ranges::is_sorted(out));
+  }
 }
