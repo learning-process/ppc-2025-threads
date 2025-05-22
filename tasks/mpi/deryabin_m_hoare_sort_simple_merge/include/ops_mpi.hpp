b7047d946d9c5e1075ff214461ea5fe94685491c
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-22 19:44:14 +0300

    Update ops_mpi.hpp

--- tasks/mpi/deryabin_m_hoare_sort_simple_merge/include/ops_mpi.hpp
+++ tasks/mpi/deryabin_m_hoare_sort_simple_merge/include/ops_mpi.hpp
@@ -11,6 +11,7 @@
 
 namespace deryabin_m_hoare_sort_simple_merge_mpi {
 
+void HoaraSort(std::vector<double>& a, size_t first, size_t last);
 void HoaraSort(std::vector<double>& a, size_t first, size_t last, tbb::task_group& tg, size_t available_threads);
 void MergeTwoParts(std::vector<double>& a, size_t first, size_t last, tbb::task_group& tg, size_t available_threads);
 
