e49ff6cc380c343a4fc4c29fecd68d9dbd0bf3e7
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-22 18:04:00 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_hoare_sort_simple_merge/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_hoare_sort_simple_merge/src/ops_mpi.cpp
@@ -152,7 +152,7 @@ bool deryabin_m_hoare_sort_simple_merge_mpi::HoareSortTaskMPI::RunImpl() {
     size_t partner = static_cast<size_t>(world.rank()) ^ step;
     if ((static_cast<size_t>(world.rank()) & step) == 0) {
         world.send(world.rank() + 1, world.rank(), input_array_A_.data() + static_cast<unsigned short>(world.rank() * num_chunk_per_proc * min_chunk_size_) << (i + 1),
-                static_cast<unsigned short>(num_chunk_per_proc * min_chunk_size_) << i;  
+                 static_cast<unsigned short>(num_chunk_per_proc * min_chunk_size_) << i;  
     } else {
       world.recv(
           world.rank() - 1, world.rank() - 1,
