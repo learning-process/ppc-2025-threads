a55684e8033e062a9add4bb8c27a49ce1704c97b
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-23 21:26:53 +0300

    2

--- tasks/all/deryabin_m_hoare_sort_simple_merge/include/ops_all.hpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/include/ops_all.hpp
@@ -13,7 +13,8 @@
 namespace deryabin_m_hoare_sort_simple_merge_mpi {
 
 void HoaraSort(std::vector<double>& a, size_t first, size_t last);
-void HoaraSort(std::vector<double>& a, size_t first, size_t last, oneapi::tbb::task_group& tg, size_t available_threads);
+void HoaraSort(std::vector<double>& a, size_t first, size_t last, oneapi::tbb::task_group& tg,
+               size_t available_threads);
 void MergeTwoParts(std::vector<double>& a, size_t first, size_t last, oneapi::tbb::task_group& tg,
                    size_t available_threads);
 
