e6a8121484ce2eb4139b001f97e6450b94358269
Ivan Alputov <i.alputov@yadro.com>
2025-05-23 22:33:49 +0300

    perf dead

--- tasks/all/alputov_i_graham_scan/perf_tests/main.cpp
+++ tasks/all/alputov_i_graham_scan/perf_tests/main.cpp
@@ -125,3 +125,52 @@ TEST(alputov_i_graham_scan_all_perf, test_pipeline_run) {
     ASSERT_TRUE(hull_set.count({1001.0, 1001.0}));
   }
 }
+
+TEST(alputov_i_graham_scan_all_perf, test_task_run) {
+  int rank{};
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  std::vector<alputov_i_graham_scan_all::Point> input_points_struct = GeneratePerfData(kPerfPointCount);
+  std::vector<double> input_doubles = PointsToDoublesPerf(input_points_struct);
+
+  int actual_hull_size = 0;
+  std::vector<double> output_hull_doubles(input_doubles.size());
+
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  if (rank == 0) {
+    task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_doubles.data()));
+    task_data->inputs_count.emplace_back(input_doubles.size());
+    task_data->outputs.emplace_back(reinterpret_cast<uint8_t*>(&actual_hull_size));
+    task_data->outputs_count.emplace_back(1);
+    task_data->outputs.emplace_back(reinterpret_cast<uint8_t*>(output_hull_doubles.data()));
+    task_data->outputs_count.emplace_back(output_hull_doubles.size());
+  }
+
+  std::shared_ptr<alputov_i_graham_scan_all::TestTaskALL> task_obj(
+      new alputov_i_graham_scan_all::TestTaskALL(task_data), TaskALLDeleter());
+
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perf_attr->current_timer = [t0]() {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(task_obj);
+
+  perf_analyzer->TaskRun(perf_attr, perf_results);
+
+  if (rank == 0) {
+    ppc::core::Perf::PrintPerfStatistic(perf_results);
+    ASSERT_TRUE(VerifyHullBasic(input_points_struct, output_hull_doubles, actual_hull_size));
+    std::set<std::pair<double, double>> hull_set;
+    for (int i = 0; i < actual_hull_size; ++i) {
+      hull_set.insert({output_hull_doubles[2 * i], output_hull_doubles[(2 * i) + 1]});
+    }
+    ASSERT_TRUE(hull_set.count({-1001.0, -1001.0}));
+    ASSERT_TRUE(hull_set.count({1001.0, 1001.0}));
+  }
+}
