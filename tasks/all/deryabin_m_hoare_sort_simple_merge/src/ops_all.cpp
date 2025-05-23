cc58256380653b952230f6912b7a7a0e00b8c11f
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-23 21:26:10 +0300

    1

--- tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
@@ -59,13 +59,13 @@ void deryabin_m_hoare_sort_simple_merge_mpi::HoaraSort(std::vector<double>& a, s
   const size_t j = pj - a.data();
   const size_t i = pi - a.data();
   if (available_threads > 1) {
-    tg.run([&a, &first, &j, &tg, &available_threads]() { HoaraSort(a, first, j, &tg, available_threads >> 1); });
-    tg.run([&a, &i, &last, &tg, &available_threads]() {
-      HoaraSort(a, i + 1, last, &tg, available_threads - (available_threads >> 1));
+    tg.run([&a, &first, &j, &available_threads]() { HoaraSort(a, first, j, tg, available_threads >> 1); });
+    tg.run([&a, &i, &last, &available_threads]() {
+      HoaraSort(a, i + 1, last, tg, available_threads - (available_threads >> 1));
     });
   } else {
-    HoaraSort(a, first, j, &tg, 1);
-    HoaraSort(a, i + 1, last, &tg, 1);
+    HoaraSort(a, first, j, tg, 1);
+    HoaraSort(a, i + 1, last, tg, 1);
   }
 }
 
@@ -77,8 +77,8 @@ void deryabin_m_hoare_sort_simple_merge_mpi::MergeTwoParts(std::vector<double>& 
   const size_t size = last - first;
   const size_t mid = first + size / 2;
   if (available_threads > 1) {
-    tg.run([&, &first, &mid, &tg, &available_threads]() { MergeTwoParts(a, first, mid, tg, available_threads / 2); });
-    tg.run([&, &last, &mid, &tg, &available_threads]() {
+    tg.run([&, &first, &mid, &available_threads]() { MergeTwoParts(a, first, mid, tg, available_threads / 2); });
+    tg.run([&, &last, &mid, &available_threads]() {
       MergeTwoParts(a, mid, last, tg, available_threads - available_threads / 2);
     });
     tg.wait();
@@ -160,12 +160,12 @@ bool deryabin_m_hoare_sort_simple_merge_mpi::HoareSortTaskMPI::RunImpl() {
   if (world.rank() != world.size() - 1) {
     oneapi::tbb::task_group tg;
     HoaraSort(input_array_A_, static_cast<size_t>(world.rank()) * num_chunk_per_proc * min_chunk_size_,
-              ((static_cast<size_t>(world.rank()) + 1) * num_chunk_per_proc * min_chunk_size_) - 1, &tg, num_threads);
+              ((static_cast<size_t>(world.rank()) + 1) * num_chunk_per_proc * min_chunk_size_) - 1, tg, num_threads);
     tg.wait();
   } else {
     oneapi::tbb::task_group tg;
     HoaraSort(input_array_A_, static_cast<size_t>(world.rank()) * num_chunk_per_proc * min_chunk_size_, dimension_ - 1,
-              &tg, num_threads);
+              tg, num_threads);
     tg.wait();
   }
   return true;
