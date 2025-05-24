6ab13b6500c8e65c9e68e0de91d90aeaa33bb149
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-24 22:59:26 +0300

    Update ops_all.cpp

--- tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
@@ -59,11 +59,12 @@ void deryabin_m_hoare_sort_simple_merge_mpi::HoaraSort(std::vector<double>& a, s
   const size_t j = pj - a.data();
   const size_t i = pi - a.data();
   if (available_threads > 1) {
-    HoaraSort(a, first, j, tg, available_threads >> 1);
-    // tg.run([&a, &first, &j, &tg, &available_threads]() { HoaraSort(a, first, j, tg, available_threads >> 1); });
+    // HoaraSort(a, first, j, tg, available_threads >> 1);
+    tg.run([&a, &first, &j, &tg, &available_threads]() { HoaraSort(a, first, j, tg, available_threads >> 1); });
     tg.run([&a, &i, &last, &tg, &available_threads]() {
       HoaraSort(a, i + 1, last, tg, available_threads - (available_threads >> 1));
     });
+    tg.wait();
   } else {
     HoaraSort(a, first, j, tg, 1);
     HoaraSort(a, i + 1, last, tg, 1);
@@ -179,7 +180,6 @@ bool deryabin_m_hoare_sort_simple_merge_mpi::HoareSortTaskMPI::RunImpl() {
       }
     }
   }
-  tg.wait();
   return true;
 }
 
