357afd2818a3ee195f98b6b76c80d0304a8ce478
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-25 22:31:03 +0300

    Update ops_all.cpp

--- tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
@@ -17,8 +17,7 @@ void deryabin_m_hoare_sort_simple_merge_mpi::HoaraSort(std::vector<double>::iter
     return;
   }
   const auto mid = first + ((last - first) >> 1);
-  const double x = (*first < *mid) ? (*mid < *last ? *mid : std::max(*first, *last))
-                                   : (*first < *last ? *first : std::max(*mid, *last));
+  const double x = *mid;
   auto left = first;
   auto right = last;
   do {
@@ -31,11 +30,11 @@ void deryabin_m_hoare_sort_simple_merge_mpi::HoaraSort(std::vector<double>::iter
     std::iter_swap(left, right);
   } while (left < right);
   if (last - first >= 199) {
-    oneapi::tbb::parallel_invoke([&first, &left]() { HoaraSort(first, left); },
-                                 [&right, &last]() { HoaraSort(right + 1, last); });
+    oneapi::tbb::parallel_invoke([=]() { HoaraSort(first, right); },
+                                 [=]() { HoaraSort(left + 1, last); });
   } else {
-    HoaraSort(first, left);
-    HoaraSort(right + 1, last);
+    HoaraSort(first, right);
+    HoaraSort(left + 1, last);
   }
 }
 
