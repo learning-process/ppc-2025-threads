abff675ea8ac51f8f502afa2b9dafd0acce5b634
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-25 18:20:22 +0300

    Update ops_all.hpp

--- tasks/all/deryabin_m_hoare_sort_simple_merge/include/ops_all.hpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/include/ops_all.hpp
@@ -12,7 +12,7 @@
 
 namespace deryabin_m_hoare_sort_simple_merge_mpi {
 
-void HoaraSort(std::vector<double>::iterator& first, std::vector<double>::iterator& last);
+void HoaraSort(std::vector<double>::iterator first, std::vector<double>::iterator last);
 void MergeTwoParts(std::vector<double>& a, size_t first, size_t last);
 
 class HoareSortTaskSequential : public ppc::core::Task {
