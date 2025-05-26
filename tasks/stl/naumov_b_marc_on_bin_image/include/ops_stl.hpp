8c61ff5a2736588b97d25030d49bf442972a370c
BogdanNaumov <bogerbloger@yandex.ru>
2025-05-27 02:36:10 +0300

    f

--- tasks/stl/naumov_b_marc_on_bin_image/include/ops_stl.hpp
+++ tasks/stl/naumov_b_marc_on_bin_image/include/ops_stl.hpp
@@ -7,6 +7,10 @@
 
 namespace naumov_b_marc_on_bin_image_stl {
 
+std::vector<int> GenerateRandomBinaryMatrix(int rows, int cols, double probability = 0.5);
+std::vector<int> GenerateSparseBinaryMatrix(int rows, int cols, double probability = 0.1);
+std::vector<int> GenerateDenseBinaryMatrix(int rows, int cols, double probability = 0.9);
+
 class TestTaskSTL : public ppc::core::Task {
  public:
   explicit TestTaskSTL(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
@@ -16,8 +20,25 @@ class TestTaskSTL : public ppc::core::Task {
   bool PostProcessingImpl() override;
 
  private:
-  std::vector<int> input_, output_;
-  int rc_size_{};
+  void ProcessBlock(int start_row, int start_col, int block_rows, int block_cols);
+  void ProcessPixel(int row, int col);
+  void AssignNewLabel(int row, int col);
+  void AssignMinLabel(int row, int col, const std::vector<int> &neighbors);
+  void MergeLabels();
+
+  std::vector<int> FindAdjacentLabels(int row, int col);
+  void AssignLabel(int row, int col, int &current_label);
+  int FindRoot(int label);
+  void UnionLabels(int label1, int label2);
+  void CalculateBlockSize();
+
+  int rows_{};
+  int cols_{};
+  std::vector<int> input_image_;
+  std::vector<int> output_image_;
+  std::vector<int> label_parent_;
+  int block_size_ = 64;
+  int current_label_ = 0;
 };
 
 }  // namespace nesterov_a_test_task_stl
