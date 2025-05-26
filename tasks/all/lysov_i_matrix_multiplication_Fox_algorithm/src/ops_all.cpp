6c5d1426ee56b7b8a510e58d5d07df6aebd2a651
ivan.lysov.05@gmail.com <ivan.lysov.05@gmail.com>
2025-05-27 01:36:03 +0300

    fix clang tidy and clang format

--- tasks/all/lysov_i_matrix_multiplication_Fox_algorithm/src/ops_all.cpp
+++ tasks/all/lysov_i_matrix_multiplication_Fox_algorithm/src/ops_all.cpp
@@ -5,21 +5,16 @@
 #include <tbb/global_control.h>
 #include <tbb/parallel_for.h>
 #include <tbb/task_arena.h>
-#include "oneapi/tbb/parallel_for.h
+
 #include <algorithm>
 #include <boost/mpi/collectives.hpp>
 #include <boost/mpi/communicator.hpp>
+#include <boost/mpi/status.hpp>
 #include <cmath>
 #include <cstddef>
 #include <cstring>
+#include <ranges>
 #include <vector>
-#include "oneapi/tbb/parallel_for.h
-#include "boost/mpi/collectives/broadcast.hpp"
-#include "boost/mpi/collectives/scatter.hpp"
-#include "oneapi/tbb/global_control.h"
-#include "oneapi/tbb/task_arena.h"
-#include "boost/mpi/collectives/gather.hpp"
-
 int lysov_i_matrix_multiplication_fox_algorithm_mpi_tbb::ComputeProcessGrid(int world_size, std::size_t n) {
   int q = static_cast<int>(std::floor(std::sqrt(world_size)));
   while (q > 1 && (world_size % q != 0 || (n % q) != 0)) {
