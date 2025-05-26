a52466cc74ca038d2e706540d7dfbb22e62145fd
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-27 02:22:04 +0300

    Update ops_all.cpp

--- tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
@@ -26,7 +26,7 @@ void deryabin_m_hoare_sort_simple_merge_mpi::HoaraSort(std::vector<double>::iter
     while (right > first && *right > pivot_value) {
       right--;
     }
-    if (*left == *right) {
+    if (*left == *right && left < right) {
       if (*left < *(left + 1)) {
         left++;
       } else {
