37396b3140ea27c57ddfb47f4870e7ab35605aec
ofuuse <129729757+ofuuse@users.noreply.github.com>
2025-05-26 12:01:34 +0300

    Fix review

--- tasks/all/durynichev_d_integrals_simpson_method/func_tests/main.cpp
+++ tasks/all/durynichev_d_integrals_simpson_method/func_tests/main.cpp
@@ -157,3 +157,28 @@ TEST(durynichev_d_integrals_simpson_method_all, test_integral_2D_x2_plus_y2_reve
     EXPECT_NEAR(out[0], -(2.0 / 3.0), 1e-4);
   }
 }
+
+TEST(durynichev_d_integrals_simpson_method_all, test_integral_2D_x2_plus_y2_neg_bounds) {
+  boost::mpi::communicator world;
+
+  std::vector<double> in = {-1.0, 1.0, -1.0, 1.0, 100};
+  std::vector<double> out(1, 0.0);
+
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  if (world.rank() == 0) {
+    task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
+    task_data->inputs_count.emplace_back(in.size());
+    task_data->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+    task_data->outputs_count.emplace_back(out.size());
+  }
+
+  durynichev_d_integrals_simpson_method_all::SimpsonIntegralSTLMPI task(task_data);
+  ASSERT_TRUE(task.Validation());
+  task.PreProcessing();
+  task.Run();
+  task.PostProcessing();
+
+  if (world.rank() == 0) {
+    EXPECT_NEAR(out[0], 2 + (2.0 / 3.0), 1e-4);
+  }
+}
