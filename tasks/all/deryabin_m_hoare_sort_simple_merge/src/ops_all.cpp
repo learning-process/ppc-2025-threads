b33d4422ad379fddbe4270c418dcf8644bc4bd66
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-28 11:49:38 +0300

    Update ops_all.cpp

--- tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
@@ -182,8 +182,6 @@ bool deryabin_m_hoare_sort_simple_merge_mpi::HoareSortTaskMPI::RunImpl() {
   const auto world_size = world.size();
   if (world_size != 1) {
     const size_t iterations = static_cast<size_t>(std::bit_width(chunk_count_ - 1));
-    std::vector<double> send_recv_buffer;
-    send_recv_buffer.reserve(2 * chunk_size * (1ULL << (iterations - 1)));
     for (size_t i = 0; i < iterations; ++i) {
       const unsigned short step = 1ULL << i;
       const size_t block_size = chunk_size * step;
@@ -195,18 +193,15 @@ bool deryabin_m_hoare_sort_simple_merge_mpi::HoareSortTaskMPI::RunImpl() {
           if (world.rank() != 0) {
             start_idx += rest_;
           }
-          std::copy(input_array_A_.begin() + start_idx, input_array_A_.begin() + start_idx + block_size,
-                    send_recv_buffer.begin());
-          MPI_Ssend(send_recv_buffer.data(), block_size, MPI_DOUBLE, world.rank() + step, 0, world);
+          MPI_Ssend(input_array_A_.data() + start_idx, int(block_size), MPI_DOUBLE, world.rank() + step, 0, world);
         }
         if (!is_even || world.rank() == world_size - 1) {
           size_t start_idx = (static_cast<size_t>(world.rank() - 2 * step) + 1) * chunk_size;
           if (world.rank() - step != 0) {
             start_idx += rest_;
           }
-          MPI_Recv(send_recv_buffer.data(), block_size, MPI_DOUBLE, world.rank() - step, 0, world, MPI_STATUS_IGNORE);
-          std::copy(send_recv_buffer.begin(), send_recv_buffer.begin() + block_size,
-                    input_array_A_.begin() + start_idx);
+          MPI_Recv(input_array_A_.data() + start_idx, int(block_size), MPI_DOUBLE, world.rank() - step, 0, world,
+                   MPI_STATUS_IGNORE);
           MergeTwoParts(input_array_A_.begin() + start_idx, input_array_A_.begin() + start_idx + 2 * block_size);
         }
       }
