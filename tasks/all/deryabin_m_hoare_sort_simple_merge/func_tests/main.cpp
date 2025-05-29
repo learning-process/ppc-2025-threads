af6adf28b848eab630fd926491db7cf62494bcb4
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-29 22:12:26 +0300

    Update main.cpp

--- tasks/all/deryabin_m_hoare_sort_simple_merge/func_tests/main.cpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/func_tests/main.cpp
@@ -17,10 +17,10 @@ TEST(deryabin_m_hoare_sort_simple_merge_mpi, test_random_array) {
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<> distribution(-100, 100);
-  std::vector<double> input_array(800);
+  std::vector<double> input_array(10);
   std::ranges::generate(input_array.begin(), input_array.end(), [&] { return distribution(gen); });
   std::vector<std::vector<double>> in_array(1, input_array);
-  std::vector<double> output_array(800);
+  std::vector<double> output_array(10);
   std::vector<std::vector<double>> out_array(1, output_array);
   std::vector<double> true_solution(input_array);
   std::ranges::sort(true_solution.begin(), true_solution.end());
@@ -45,14 +45,14 @@ TEST(deryabin_m_hoare_sort_simple_merge_mpi, test_random_array) {
 
 TEST(deryabin_m_hoare_sort_simple_merge_mpi, test_double_reverse_array) {
   boost::mpi::communicator world;
-  std::vector<double> input_array(800);
+  std::vector<double> input_array(10);
   const auto half = input_array.size() / 2U;
   std::ranges::generate(input_array.begin(), input_array.end() - (long)half,
                         [value = half]() mutable { return value--; });
   std::ranges::generate(input_array.end() - (long)half, input_array.end(),
                         [value = half]() mutable { return value--; });
   std::vector<std::vector<double>> in_array(1, input_array);
-  std::vector<double> output_array(800);
+  std::vector<double> output_array(10);
   std::vector<std::vector<double>> out_array(1, output_array);
   std::vector<double> true_solution(input_array);
   std::ranges::sort(true_solution.begin(), true_solution.end());
@@ -80,10 +80,10 @@ TEST(deryabin_m_hoare_sort_simple_merge_mpi, test_negative_elements_array) {
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<> distribution(-100, -1);
-  std::vector<double> input_array(800);
+  std::vector<double> input_array(10);
   std::ranges::generate(input_array.begin(), input_array.end(), [&] { return distribution(gen); });
   std::vector<std::vector<double>> in_array(1, input_array);
-  std::vector<double> output_array(800);
+  std::vector<double> output_array(10);
   std::vector<std::vector<double>> out_array(1, output_array);
   std::vector<double> true_solution(input_array);
   std::ranges::sort(true_solution.begin(), true_solution.end());
@@ -110,11 +110,11 @@ TEST(deryabin_m_hoare_sort_simple_merge_mpi, test_shuffle_array) {
   boost::mpi::communicator world;
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::vector<double> input_array(800);
+  std::vector<double> input_array(10);
   std::ranges::generate(input_array.begin(), input_array.end(), [value = 0]() mutable { return value++; });
   std::shuffle(input_array.begin(), input_array.end(), gen);
   std::vector<std::vector<double>> in_array(1, input_array);
-  std::vector<double> output_array(800);
+  std::vector<double> output_array(10);
   std::vector<std::vector<double>> out_array(1, output_array);
   std::vector<double> true_solution(input_array);
   std::ranges::sort(true_solution.begin(), true_solution.end());
@@ -141,12 +141,12 @@ TEST(deryabin_m_hoare_sort_simple_merge_mpi, test_partially_sorted_array) {
   boost::mpi::communicator world;
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::vector<double> input_array(800);
+  std::vector<double> input_array(10);
   std::ranges::generate(input_array.begin(), input_array.end(), [value = 0]() mutable { return value++; });
   const auto half = input_array.size() / 2U;
   std::shuffle(input_array.begin() + (long)half, input_array.end(), gen);
   std::vector<std::vector<double>> in_array(1, input_array);
-  std::vector<double> output_array(800);
+  std::vector<double> output_array(10);
   std::vector<std::vector<double>> out_array(1, output_array);
   std::vector<double> true_solution(input_array);
   std::ranges::sort(true_solution.begin(), true_solution.end());
@@ -171,10 +171,10 @@ TEST(deryabin_m_hoare_sort_simple_merge_mpi, test_partially_sorted_array) {
 
 TEST(deryabin_m_hoare_sort_simple_merge_mpi, test_backward_sorted_array) {
   boost::mpi::communicator world;
-  std::vector<double> input_array(800);
+  std::vector<double> input_array(10);
   std::ranges::generate(input_array.begin(), input_array.end(), [value = 800]() mutable { return value--; });
   std::vector<std::vector<double>> in_array(1, input_array);
-  std::vector<double> output_array(800);
+  std::vector<double> output_array(10);
   std::vector<std::vector<double>> out_array(1, output_array);
   std::vector<double> true_solution(input_array);
   std::ranges::sort(true_solution.begin(), true_solution.end());
