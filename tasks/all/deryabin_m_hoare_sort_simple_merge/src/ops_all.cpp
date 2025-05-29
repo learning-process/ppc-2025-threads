465ac871a461b817e6bfb5bd29dd11f6d6b6d38f
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-29 15:24:46 +0300

    Update ops_all.cpp

--- tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
@@ -215,7 +215,8 @@ bool deryabin_m_hoare_sort_simple_merge_mpi::HoareSortTaskMPI::RunImpl() {
         }
         if (world_size % 2 != 0 && world.rank() == 0) {
           world.recv(world.rank() + step - 1, 0, &input_array_A_[start_idx], block_size);
-          MergeTwoParts(input_array_A_.begin() + start_idx, input_array_A_.begin() + start_idx + (1 + world.rank() / step) * block_size);
+          MergeTwoParts(input_array_A_.begin() + start_idx,
+                        input_array_A_.begin() + start_idx + (1 + world.rank() / step) * block_size);
         } else {
           world.recv(world.rank() + step, 0, &input_array_A_[start_idx], block_size);
           MergeTwoParts(input_array_A_.begin() + start_idx, input_array_A_.begin() + start_idx + 2 * block_size);
