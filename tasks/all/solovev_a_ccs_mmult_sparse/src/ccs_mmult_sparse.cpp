0464bf07945511553d5d8fd971221ae3514e7d89
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2025-05-29 20:47:47 +0300

    clang

--- tasks/all/solovev_a_ccs_mmult_sparse/src/ccs_mmult_sparse.cpp
+++ tasks/all/solovev_a_ccs_mmult_sparse/src/ccs_mmult_sparse.cpp
@@ -6,7 +6,7 @@
 #include <boost/mpi/collectives/broadcast.hpp>
 #include <boost/mpi/collectives/reduce.hpp>
 #include <boost/mpi/communicator.hpp>
-#include <boost/serialization/vector.hpp> // NOLINT
+#include <boost/serialization/vector.hpp>  // NOLINT
 #include <cmath>
 #include <complex>
 #include <functional>
