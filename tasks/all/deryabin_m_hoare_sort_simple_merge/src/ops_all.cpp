eb0931d4fbb99bbee5bb1da23fcfb9459e4ef605
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-31 00:28:00 +0300

    Update ops_all.cpp

--- tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
@@ -75,7 +75,7 @@ void deryabin_m_hoare_sort_simple_merge_mpi::HoaraSort(std::vector<double>::iter
 
 void deryabin_m_hoare_sort_simple_merge_mpi::MergeTwoParts(std::vector<double>::iterator first,
                                                            std::vector<double>::iterator last) {
-  if (last - first >= 2000) {
+  if (last - first >= 200) {
     const auto mid = first + ((last - first) >> 1);
     const auto left_end = std::upper_bound(first, mid - 1, *mid);
     const auto right_start = std::upper_bound(mid, last, *(mid - 1));
@@ -99,7 +99,7 @@ void deryabin_m_hoare_sort_simple_merge_mpi::MergeTwoParts(std::vector<double>::
 void deryabin_m_hoare_sort_simple_merge_mpi::MergeUnequalTwoParts(std::vector<double>::iterator first,
                                                                   std::vector<double>::iterator mid,
                                                                   std::vector<double>::iterator last) {
-  if (last - first >= 2000) {
+  if (last - first >= 200) {
     const auto left_end = std::upper_bound(first, mid - 1, *mid);
     const auto right_start = std::upper_bound(mid, last, *(mid - 1));
     const size_t overlap_len = std::min(std::distance(left_end, mid), std::distance(mid - 1, right_start));
