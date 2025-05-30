231507c0dcf34f254e83db5a27abea44ff87f657
Matthewsoul124 <132999776+Matthewsoul124@users.noreply.github.com>
2025-05-30 19:30:09 +0300

    fix 1

--- tasks/all/fyodorov_m_shell_sort_with_even_odd_batcher_merge/perf_tests/main.cpp
+++ tasks/all/fyodorov_m_shell_sort_with_even_odd_batcher_merge/perf_tests/main.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <boost/mpi/collectives/broadcast.hpp>
 #include <boost/mpi/communicator.hpp>
+#include <boost/serialization/vector.hpp>
 #include <chrono>
 #include <cstdint>
 #include <iostream>
