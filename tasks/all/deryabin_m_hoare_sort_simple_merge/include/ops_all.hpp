570329025b94cc654428f03dce7ee2e2081ee5a5
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-06-01 00:41:58 +0300

    Update ops_all.hpp

--- tasks/all/deryabin_m_hoare_sort_simple_merge/include/ops_all.hpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/include/ops_all.hpp
@@ -9,13 +9,8 @@
 #include "core/task/include/task.hpp"
 
 namespace deryabin_m_hoare_sort_simple_merge_mpi {
-
-void ProcessPartition(std::vector<double>::iterator left, std::vector<double>::iterator right, double pivot_value);
-double SelectPivot(std::vector<double>::iterator first, std::vector<double>::iterator mid,
-                   std::vector<double>::iterator last);
 void SeqHoaraSort(std::vector<double>::iterator first, std::vector<double>::iterator last);
 void HoaraSort(std::vector<double>::iterator first, std::vector<double>::iterator last);
-void MergeTwoParts(std::vector<double>::iterator first, std::vector<double>::iterator last);
 void MergeUnequalTwoParts(std::vector<double>::iterator first, std::vector<double>::iterator mid,
                           std::vector<double>::iterator last);
 
