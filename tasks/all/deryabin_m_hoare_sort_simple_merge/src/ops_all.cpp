150df406cc272b03b0ef3d194c597ccb8f049715
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-05-31 18:04:04 +0300

    Update ops_all.cpp

--- tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
@@ -261,10 +261,10 @@ bool deryabin_m_hoare_sort_simple_merge_mpi::HoareSortTaskMPI::RunImpl() {
           start_idx += rest_;
         }
         world.recv(recv_rank, 0, input_array_A_.data() + start_idx, block_size);
-        const auto end_iter =
+        const auto final_iter =
             special_odd_case ? input_array_A_.end() : input_array_A_.begin() + start_idx + block_size * 2 - rest_;
         MergeUnequalTwoParts(input_array_A_.begin() + start_idx, input_array_A_.begin() + start_idx + block_size,
-                             end_iter);
+                             final_iter);
       }
     }
   }
