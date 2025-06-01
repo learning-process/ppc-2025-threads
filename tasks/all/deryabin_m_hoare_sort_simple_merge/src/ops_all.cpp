a67bb24320281b3125b93c95b23419c47fecd508
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-06-01 15:43:53 +0300

    Update ops_all.cpp

--- tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
+++ tasks/all/deryabin_m_hoare_sort_simple_merge/src/ops_all.cpp
@@ -257,7 +257,7 @@ bool deryabin_m_hoare_sort_simple_merge_mpi::HoareSortTaskMPI::RunImpl() {
   for (size_t i = 0; i < iterations; ++i) {
     const size_t step = 1ULL << i;
     if ((chunk_count_ - world_rank) % step == 0 || world_rank == 0) {
-      const bool is_even = ((chunk_count_ & 1) ? ((chunk_count_ - world_rank + step - 1) / step) & 1 != 0U
+      const bool is_even = ((chunk_count_ & 1) ? (((chunk_count_ - world_rank + step - 1) / step) & 1) != 0U
                                                : !((chunk_count_ - world_rank - 1) / step & 1) == 0U) != 0U;
       if (is_even) {
         if (world_rank == 0) {
