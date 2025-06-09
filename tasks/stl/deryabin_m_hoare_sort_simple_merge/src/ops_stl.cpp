b533e3516e3ffd5bdaffc3fa74278363661f5579
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2025-06-09 04:59:08 +0300

    Update ops_stl.cpp

--- tasks/stl/deryabin_m_hoare_sort_simple_merge/src/ops_stl.cpp
+++ tasks/stl/deryabin_m_hoare_sort_simple_merge/src/ops_stl.cpp
@@ -83,7 +83,7 @@ bool deryabin_m_hoare_sort_simple_merge_stl::HoareSortTaskSTL::ValidationImpl() 
 }
 
 bool deryabin_m_hoare_sort_simple_merge_stl::HoareSortTaskSTL::RunImpl() {
-  const size_t num_threads = std::thread::hardware_concurrency();
+  const size_t num_threads = std::min(ppc::util::GetPPCNumThreads(), std::thread::hardware_concurrency);
   if (chunk_count_ < num_threads) {
     // Увеличиваем число кусочков до ближайшей степени двойки >= num_threads,
     // чтобы эффективно загрузить все доступные потоки
